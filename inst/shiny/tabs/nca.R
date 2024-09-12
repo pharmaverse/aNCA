f09579e617e2413f486bda3e5743e9cfc602cb8b
Gero1999 <gerardojoro@gmail.com>
2024-09-12 12:12:52 +0200

    duplicated code chunk for creation of mydata() in nca.R (already in data.R)

--- inst/shiny/tabs/nca.R
+++ inst/shiny/tabs/nca.R
@@ -111,46 +111,6 @@ observeEvent(input$settings_upload,{
 })
 
 
-# Update analyte selection input based on the data
-observeEvent(data(), updateSelectInput(session, inputId = 'analyte', 
-                                       label = 'Choose the analyte :', 
-                                       choices = unique(data()$ANALYTE)))
-
-# When an analyte is selected and the user clicks the "Submit" button, create the PKNCA data object
-mydata = reactiveVal(NULL)
-observeEvent(input$submit_analyte, {
-  
-  # Segregate the data into concentration and dose records
-  df_conc <- create_conc(data(), input$analyte, input$proftype)
-  df_dose <- create_dose(df_conc)
-  
-  # Define initially a inclusions/exclusions for lambda slope estimation (with no input)
-  df_conc$is.excluded.hl = FALSE
-  df_conc$is.included.hl = FALSE
-  df_conc$REASON = NA  # Exclusions will have preferential reason statements than inclusions
-  df_conc$exclude_half.life = FALSE
-  
-  # Make the PKNCA concentration and dose objects
-  myconc <- PKNCA::PKNCAconc(df_conc, 
-                             formula = AVAL ~ TIME | STUDYID + PCSPEC + ANALYTE + USUBJID/DOSNO,
-                             exclude_half.life='exclude_half.life',
-                             time.nominal = 'NFRLT')
-  
-  mydose <- PKNCA::PKNCAdose(data = df_dose, 
-                             formula = DOSEA ~ TIME | STUDYID + PCSPEC + ANALYTE + USUBJID + DOSNO, 
-                             route = ifelse(toupper(df_dose$IQROUTE) == 'EXTRAVASCULAR', 'extravascular', 'intravascular'), 
-                             time.nominal = 'NFRLT', 
-                             duration = 'ADOSEDUR')
-  
-  # Combine the PKNCA objects into the PKNCAdata object
-  mydata <- PKNCA::PKNCAdata(data.conc = myconc, 
-                             data.dose = mydose, 
-                             units = PKNCA::pknca_units_table(concu = myconc$data$AVALU[1], 
-                                                              doseu = myconc$data$DOSEU[1], 
-                                                              amountu = myconc$data$AVALU[1], 
-                                                              timeu = myconc$data$RRLTU[1]))
-  mydata(mydata)
-})
 
 
 # Display the PKNCA data object for the user (concentration records)
