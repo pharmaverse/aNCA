ed12e91268d336ef133a79a624006f3af9e8581b
Spinner <spinnerj@emea.roche.com>
2024-10-11 10:50:31 +0200

    migrate shiny functions to inst folder and update namespace

---
+++ R/shiny_functions.R
@@ -0,0 +1,9 @@
+source(system.file("shiny/functions/create_filter.R", package = "aNCA"))
+source(system.file("shiny/functions/mod_partialAUCInput.R", package = "aNCA"))
+
+# export the functions
+#' @export
+create_filter
+
+#' @export
+partialAUCInput
