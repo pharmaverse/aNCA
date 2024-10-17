4d580831238e1bbca035e3c4e7bd1a23def4a3bb
m-kolomanski <m.kolomanski@yahoo.com>
2024-10-17 14:30:09 +0200

    chore: removed duplicated imports

--- R/run_app.R
+++ R/run_app.R
@@ -6,21 +6,17 @@
 #'
 #' @importFrom dplyr mutate filter select group_by summarise pull arrange ungroup
 #' @importFrom dplyr rename_with across case_when left_join rename
-#' @importFrom DT DTOutput
-#' @importFrom DT renderDataTable datatable formatStyle styleEqual
+#' @importFrom DT DTOutput renderDataTable datatable formatStyle styleEqual
 #' @importFrom ggplot2 ggplot geom_errorbar geom_point geom_line labs aes facet_wrap
 #' @importFrom htmlwidgets JS
 #' @importFrom PKNCA PKNCAconc PKNCAdose PKNCAdata pk.nca PKNCA.options pknca_units_table
-#' @importFrom plotly plotlyOutput
-#' @importFrom plotly renderPlotly plotlyOutput plotly_build event_data
+#' @importFrom plotly plotlyOutput renderPlotly plotly_build event_data
 #' @importFrom rio export_list
 #' @importFrom rmarkdown render
 #' @importFrom shinyBS bsModal
 #' @importFrom shinyFiles shinyDirChoose
-#' @importFrom shinyjqui orderInput
 #' @importFrom shinyjqui orderInput updateOrderInput
-#' @importFrom shinyWidgets dropdown pickerInput
-#' @importFrom shinyWidgets pickerInput switchInput updatePickerInput
+#' @importFrom shinyWidgets dropdown pickerInput switchInput updatePickerInput
 #' @importFrom tools file_ext
 #' @importFrom utils read.csv write.csv
 #' @importFrom zip zipr
