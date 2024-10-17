06dbc3a4c1814f68aae4b756a225f135c3611dac
m-kolomanski <m.kolomanski@yahoo.com>
2024-10-17 14:24:06 +0200

    refactor: simplified the way shiny app is run from within the package

--- R/run_app.R
+++ R/run_app.R
@@ -1,6 +1,30 @@
 #' Run the Shiny app
+#' @import shiny
+#'
+#' List of functions imported for the shiny application.
+#' When adding new imports, please keep the alphabetical order, at lest for packages.
+#'
+#' @importFrom dplyr mutate filter select group_by summarise pull arrange ungroup
+#' @importFrom dplyr rename_with across case_when left_join rename
+#' @importFrom DT DTOutput
+#' @importFrom DT renderDataTable datatable formatStyle styleEqual
+#' @importFrom ggplot2 ggplot geom_errorbar geom_point geom_line labs aes facet_wrap
+#' @importFrom htmlwidgets JS
+#' @importFrom PKNCA PKNCAconc PKNCAdose PKNCAdata pk.nca PKNCA.options pknca_units_table
+#' @importFrom plotly plotlyOutput
+#' @importFrom plotly renderPlotly plotlyOutput plotly_build event_data
+#' @importFrom rio export_list
+#' @importFrom rmarkdown render
+#' @importFrom shinyBS bsModal
+#' @importFrom shinyFiles shinyDirChoose
+#' @importFrom shinyjqui orderInput
+#' @importFrom shinyjqui orderInput updateOrderInput
+#' @importFrom shinyWidgets dropdown pickerInput
+#' @importFrom shinyWidgets pickerInput switchInput updatePickerInput
+#' @importFrom tools file_ext
+#' @importFrom utils read.csv write.csv
+#' @importFrom zip zipr
 #' @export
-
 run_app <- function() {
-  shiny::shinyApp(ui = app_ui(), server = app_server)
+  shiny::runApp(system.file("shiny", package = "aNCA"))
 }
