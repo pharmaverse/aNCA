0557dd241d35fb00341acd99cc78dc7a0878559f
Gero1999 <gerardojoro@gmail.com>
2024-10-18 13:26:06 +0200

    ui: separate tabs

--- inst/shiny/modules/tab_tlg.R
+++ inst/shiny/modules/tab_tlg.R
@@ -1,32 +1,71 @@
 tab_tlg_ui <- function(id) {
   ns <- NS(id)
-
+  
   tabsetPanel(
     #' TODO(mateusz): please migrate this to outside style.css file
     #' TODO(mateusz): those styles are global, so they will inpact all tables in the application -
     #'                are you sure we want to do that?
-    tags$head(tags$style(HTML("#selected_tlg_table table tbody td:nth-child(1), 
-                               #selected_tlg_table table tbody td:nth-child(2),
-                               #selected_tlg_table table tbody td:nth-child(4) 
+    tags$head(tags$style(HTML("table tbody td:nth-child(1), 
+                               #selected_tlg_datatable tbody td:nth-child(2),
+                               .selected_tlg_datatable tbody td:nth-child(4) 
                                   {pointer-events: none;}")
-                         )
-              ),
+    )
+    ),
     tabPanel("Order details",
-      actionButton(ns("add_tlg"), "Add TLG"),
-      actionButton(ns("remove_tlg"), "Remove TLG"),
-      actionButton(ns("submit_tlg_order"), "Submit Order Details"),
-
-      DTOutput(ns("selected_tlg_table")),
-      actionButton(ns("submit_tlg_order"), "Submit Order Details")
+             actionButton(ns("add_tlg"), "Add TLG"),
+             actionButton(ns("remove_tlg"), "Remove TLG"),
+             actionButton(ns("submit_tlg_order"), "Submit Order Details"),
+             
+             DTOutput(ns("selected_tlg_table")),
+             actionButton(ns("submit_tlg_order"), "Submit Order Details")
     ),
     tabPanel("Tables",
-      radioButtons(inputId = ns("buttons_Tables"), label = "\n", choices = "")
+             fluidRow(
+               column(2,  # Left column for plot selection
+                      h3("Input from user to choose plot"),
+                      radioButtons(inputId = ns("buttons_Tables"), label = "\n", choices = "")
+               ),
+               column(6,  # Middle column for plot output
+                      h3("Output plots to display based on user's input"),
+                      plotOutput(ns("plot_Tables"))
+               ),
+               column(2,  # Right column for plot customization inputs
+                      h3("Inputs with selections linked to downloadable object (i.e, tlg_order())"),
+                      textInput(ns("footnote_Tables"), label = 'Footnote')
+               )
+             )
     ),
     tabPanel("Listings",
-      radioButtons(inputId = ns("buttons_Listings"), label = "\n", choices = "")
+             fluidRow(
+               column(2,  # Left column for plot selection
+                      h3("Input from user to choose plot"),
+                      radioButtons(inputId = ns("buttons_Listings"), label = "\n", choices = "")
+               ),
+               column(6,  # Middle column for plot output
+                      h3("Output plots to display based on user's input"),
+                      plotOutput(ns("plot_Listings"))
+               ),
+               column(2,  # Right column for plot customization inputs
+                      h3("Inputs with selections linked to downloadable object (i.e, tlg_order())"),
+                      textInput(ns("footnote_Listings"), label = 'Footnote')
+               )
+             )
     ),
     tabPanel("Graphs",
-      radioButtons(inputId = ns("buttons_Graphs"), label = "\n", choices = "")
+             fluidRow(
+               column(2,  # Left column for plot selection
+                      h3("Input from user to choose plot"),
+                      radioButtons(inputId = ns("buttons_Graphs"), label = "\n", choices = "")
+               ),
+               column(6,  # Middle column for plot output
+                      h3("Output plots to display based on user's input"),
+                      plotOutput(ns("plot_Graphs"))
+               ),
+               column(2,  # Right column for plot customization inputs
+                      h3("Inputs with selections linked to downloadable object (i.e, tlg_order())"),
+                      textInput(ns("footnote_Graphs"), label = 'Footnote')
+               )
+             )
     )
   )
 }
@@ -34,27 +73,27 @@ tab_tlg_ui <- function(id) {
 tab_tlg_server <- function(id, data) {
   moduleServer(id, function(input, output, session) {
     # SERVER LOGIC OF NAVBAR TLG TAB ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+    
     # Order details: Input definition for Tables, Listings and Graphs to generate in the other tabs
     # Tables:
     # Listings:
     # Graphs:
-
+    
     # TABSET: Order details =============================================
-
+    
     # Make available the CSV file with the TLG list and available links to NEST
     tlg_order <- reactiveVal(
       # TODO(mateusz): migrate this to shiny/data folder
       read.csv("www/data/TLG_order_details.csv") %>%
         mutate(PKid = paste0("<a href='", Catalog_Link, "' target='_blank'>", PKid, "</a>"))
     )
-
+    
     # Based on the TLG list conditions for data() define the preselected rows in $Selection
     observeEvent(list(tlg_order(), data()), {
-
+      
       # Take the column name to later test if it is present in the data
       column_of_conditions <- gsub("([=<>!].*)", "", tlg_order()$Condition)
-
+      
       # If there is any condition assess if it is true at least for one case
       new_tlg_order <- tlg_order() %>%
         mutate(
@@ -65,18 +104,18 @@ tab_tlg_server <- function(id, data) {
             TRUE ~ FALSE
           )
         )
-
+      
       # Return TLG list with preselections
       tlg_order(new_tlg_order)
     })
-
-
+    
+    
     # Render the TLG list for the user's inspection
     output$selected_tlg_table <- DT::renderDT({
-
+      
       # Render the editable DT table
       datatable(
-        elementId = "selected_tlg_table",
+        elementId = "selected_tlg_datatable",
         data = dplyr::filter(tlg_order(), Selection),
         editable = list(
           target = "cell",
@@ -110,7 +149,7 @@ tab_tlg_server <- function(id, data) {
               )
             )
           ),
-
+          
           # Aesthetics to define the row distribution in groups (Tables, Listing, Graphs)
           rowGroup = list(dataSrc = which(names(tlg_order()) %in% c("Type", "Dataset")))
         ),
@@ -123,18 +162,18 @@ tab_tlg_server <- function(id, data) {
           fontFamily = "Arial"
         )
     }, server = FALSE)
-
+    
     # Save table changes from the UI into the server
     observeEvent(input$selected_tlg_table_cell_edit, {
       info <- input$selected_tlg_table_cell_edit
-
+      
       # Update the reactive data frame with the new value
       new_tlg_order <- tlg_order()
       new_tlg_order[new_tlg_order$Selection, ][info$row, info$col] <- info$value
       tlg_order(new_tlg_order)
     })
-
-
+    
+    
     # Show modal when the add_tlg button is pressed
     observeEvent(input$add_tlg, {
       showModal(modalDialog(
@@ -147,7 +186,7 @@ tab_tlg_server <- function(id, data) {
         size = "l"
       ))
     })
-
+    
     # Render the DT table in the modal
     output$modal_tlg_table <- DT::renderDT({
       datatable(
@@ -169,7 +208,7 @@ tab_tlg_server <- function(id, data) {
             ),
             list(targets = 0, orderable = FALSE, className = "select-checkbox")
           ),
-
+          
           # Aesthetics to create a checkbox column for selected rows
           select = list(
             style = "multiple",
@@ -177,13 +216,13 @@ tab_tlg_server <- function(id, data) {
             selection = "none",
             server = FALSE
           ),
-
+          
           # Aesthetics to define the row distribution in groups (Tables, Listing, Graphs)
           rowGroup = list(dataSrc = which(names(tlg_order()) %in% c("Type", "Dataset")))
         )
       )
     })
-
+    
     # Update the Selection column when the confirm_add_tlg button is pressed
     observeEvent(input$confirm_add_tlg, {
       print("confirm_add_tlg")
@@ -197,23 +236,23 @@ tab_tlg_server <- function(id, data) {
       }
       removeModal()
     })
-
-
+    
+    
     # Update the Selection column when the confirm_add_tlg button is pressed
     observeEvent(input$remove_tlg, {
       selected_rows <- input$selected_tlg_table_rows_selected
-
+      
       if (length(selected_rows) > 0) {
         tlg_order_data <- tlg_order()
         tlg_order_data$Selection[tlg_order_data$Selection][selected_rows] <- FALSE
         tlg_order(tlg_order_data)
       }
     })
-
-
+    
+    
     # When the user submits the TLG order...
     observeEvent(input$submit_tlg_order, {
-
+      
       # Filter only the rows requested by the user
       tlg_order_filt <- tlg_order()[tlg_order()$Selection, ]
       
