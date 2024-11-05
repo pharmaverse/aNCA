0e286e0a0423d6370c002d9fe0dfa05a1b7229b0
m-kolomanski <m.kolomanski@yahoo.com>
2024-11-05 17:35:59 +0100

    feat: eval_range can handle sequences of singular numbers

--- R/filter_slopes.R
+++ R/filter_slopes.R
@@ -70,14 +70,3 @@ filter_slopes <- function(data, slopes, profiles) {
 
   data
 }
-
-#' Evaluates range notation. If provided notation is invalid, returns NA. In example,
-#' eval_range("1:5") returns c(1,2,3,4,5).
-#'
-#' @param x character string with range notation, eg. 1:5.
-#' @returns numeric vector with specified range of numbers, NA if notation is invalid
-#' @noRD
-.eval_range <- function(x) {
-  val_range <- try(eval(parse(text = x)), silent = TRUE)
-  if (inherits(val_range, "try-error")) NA else val_range
-}
