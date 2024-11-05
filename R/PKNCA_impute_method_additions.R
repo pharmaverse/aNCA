ec3e1094b8b73fe9072482c31acec4114ec0bbdb
Gero1999 <gerardojoro@gmail.com>
2024-11-05 17:08:10 +0100

    enhancement: include additional C0 imputation strategies

---
+++ R/PKNCA_impute_method_additions.R
@@ -0,0 +1,30 @@
+PKNCA_impute_method_start_logslope <- function(conc, time, start, end, ..., options = list()) {
+  ret <- data.frame(conc = conc, time = time)
+  mask_start <- time %in% start
+  if (!any(mask_start)) {
+    all_concs <- conc[time >= start  &  time <= end]
+    all_times <- time[time >= start  &  time <= end]
+    if (!all(is.na(all_concs))) {
+      c0 <- PKNCA::pk.calc.c0(all_concs, all_times, method = "logslope")
+      ret <- rbind(ret, data.frame(time = start, conc = c0))
+      ret <- ret[order(ret$time), ]
+    }
+  }
+  ret
+}
+
+
+PKNCA_impute_method_start_c1 <- function(conc, time, start, end, ..., options = list()) {
+  ret <- data.frame(conc = conc, time = time)
+  mask_start <- time %in% start
+  if (!any(mask_start)) {
+    all_concs <- conc[time >= start  &  time <= end]
+    all_times <- time[time >= start  &  time <= end]
+    if (!all(is.na(all_concs))) {
+      c1 <- all_concs[which.min(all_times, na.rm = T)]
+      ret <- rbind(ret, data.frame(time = start, conc = c1))
+      ret <- ret[order(ret$time), ]
+    }
+  }
+  ret
+}
