b22a8fddbc9e1d6600b2a3fdbfa7019771d29049
m-kolomanski <m.kolomanski@yahoo.com>
2024-11-06 14:46:26 +0100

    docs: updated docs for new logic, updated NAMESPACE

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,14 @@
 #' Evaluates range notation. If provided notation is invalid, returns NA. In example,
 #' eval_range("1:5") returns c(1,2,3,4,5).
-#' TODO: provide proper documentation.
 #'
 #' @param x character string with range notation, eg. 1:5.
 #' @returns numeric vector with specified range of numbers, NA if notation is invalid
+#'
+#' @examples
+#' # Basic usage
+#' .eval_range("1:5") # c(1, 2, 3, 4, 5)
+#' .eval_range("5,3:1,15") # c(5, 3, 2, 1, 15)
+#'
 #' @export
 .eval_range <- function(x) {
   val_range <- try(eval(parse(text = paste0("c(", x, ")"))), silent = TRUE)
@@ -12,10 +17,14 @@
 
 #' Compresses a numeric vector into the simples possible character string that, when evaluated,
 #' will create the same numeric vector.
-#' TODO: providep proper documentation
 #'
 #' @param range_vector numeric vector with numbers to compress into string
 #' @returns simplest possible character string representing provided vector
+#'
+#' @examples
+#' # Basic usage
+#' .compress_range(c(1, 2, 3, 4)) # "1:4"
+#' .compress_range(c(15, 1, 11, 4, 5, 10, 2, 12, 3)) # "1:5,10:12,15"
 #' @export
 .compress_range <- function(range_vector) {
   if (class(range_vector) != "numeric") range_vector <- suppressWarnings(as.numeric(range_vector))
