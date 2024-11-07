429a1e61408a5374e2e473490dea9b0602f0c154
Gero1999 <gerardojoro@gmail.com>
2024-11-07 14:38:39 +0100

    documentation: functions in R/PKNCA_impute_method_additions.R

--- R/PKNCA_impute_method_additions.R
+++ R/PKNCA_impute_method_additions.R
@@ -1,3 +1,22 @@
+#' This function imputes the start concentration using the log slope method.
+#'
+#' @param conc Numeric vector of concentrations.
+#' @param time Numeric vector of times corresponding to the concentrations.
+#' @param start Numeric value indicating the start/dose time.
+#' @param end Numeric value indicating the end time.
+#' @param ... Additional arguments (currently not used).
+#' @param options List of options (currently not used).
+#' 
+#' @return A data frame with imputed start concentration.
+#' @export
+#'
+#' @examples
+#' conc <- c(1, 2, 3, 4, 5)
+#' time <- c(0, 1, 2, 3, 4)
+#' start <- 0
+#' end <- 4
+#' PKNCA_impute_method_start_log(conc, time, start, end)
+
 PKNCA_impute_method_start_log <- function(conc, time, start, end, ..., options = list()) { # nolint
   ret <- data.frame(conc = conc, time = time)
   mask_start <- time %in% start
@@ -13,6 +32,24 @@ PKNCA_impute_method_start_log <- function(conc, time, start, end, ..., options =
   ret
 }
 
+#' This function imputes the start concentration using the first concentration after dose
+#'
+#' @param conc Numeric vector of concentrations.
+#' @param time Numeric vector of times corresponding to the concentrations.
+#' @param start Numeric value indicating the start/dose time.
+#' @param end Numeric value indicating the end time.
+#' @param ... Additional arguments (currently not used).
+#' @param options List of options (currently not used).
+#' 
+#' @return A data frame with imputed start concentration.
+#' @export
+#'
+#' @examples
+#' conc <- c(1, 2, 3, 4, 5)
+#' time <- c(0, 1, 2, 3, 4)
+#' start <- 0
+#' end <- 4
+#' PKNCA_impute_method_start_c1(conc, time, start, end)
 PKNCA_impute_method_start_c1 <- function(conc, time, start, end, ..., options = list()) { # nolint
   ret <- data.frame(conc = conc, time = time)
   mask_start <- time %in% start
