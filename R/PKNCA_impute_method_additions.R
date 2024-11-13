a622e61b9355948b697f8c7d7e80d9ea101cc02a
Gero1999 <gerardojoro@gmail.com>
2024-11-13 14:27:57 +0100

    bug: fixed impute_method_start_c1 unneded argument issue

--- R/PKNCA_impute_method_additions.R
+++ R/PKNCA_impute_method_additions.R
@@ -18,6 +18,7 @@
 #' PKNCA_impute_method_start_log(conc, time, start, end)
 
 PKNCA_impute_method_start_log <- function(conc, time, start, end, ..., options = list()) { # nolint
+
   ret <- data.frame(conc = conc, time = time)
   mask_start <- time %in% start
   if (!any(mask_start)) {
@@ -57,7 +58,7 @@ PKNCA_impute_method_start_c1 <- function(conc, time, start, end, ..., options = 
     all_concs <- conc[time >= start  &  time <= end]
     all_times <- time[time >= start  &  time <= end]
     if (!all(is.na(all_concs))) {
-      c1 <- all_concs[which.min(all_times, na.rm = TRUE)]
+      c1 <- all_concs[which.min(all_times)]
       ret <- rbind(ret, data.frame(time = start, conc = c1))
       ret <- ret[order(ret$time), ]
     }
