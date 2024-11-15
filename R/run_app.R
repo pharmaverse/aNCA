eac86681fccf691f8da28bb7b241bef492beaae5
Pavel Demin <paveldemin@me.com>
2024-11-15 11:53:02 +0000

    chore: require all packages listed in the NAMESPACE

--- R/run_app.R
+++ R/run_app.R
@@ -1,50 +1,38 @@
 #' Run the Shiny app
+#' Please do not add import roxygen comments here. This function
+#' should require every namespace that is documented in the NAMESPACE.
 #'
-#' List of functions imported for the shiny application.
-#' When adding new imports, please keep the alphabetical order, at lest for packages.
+#' Apart from the dependencies, this function should also load the package
+#' itself, in case it is run via `aNCA::run_app()`.
 #'
 #' @param ... Arguments passed to `shiny::runApp()`
 #'
-#' @import aNCA
-#' @import shiny
-#' @importFrom dplyr mutate filter select group_by summarise pull arrange ungroup
-#' @importFrom dplyr rename_with across case_when left_join rename
-#' @importFrom DT DTOutput renderDataTable datatable formatStyle styleEqual
-#' @importFrom ggplot2 ggplot geom_errorbar geom_point geom_line labs aes facet_wrap
-#' @importFrom htmlwidgets JS
-#' @importFrom logger log_trace log_debug log_info log_warn log_error log_fatal
-#' @importFrom PKNCA PKNCAconc PKNCAdose PKNCAdata pk.nca PKNCA.options pknca_units_table
-#' @importFrom plotly plotlyOutput renderPlotly plotly_build event_data
-#' @importFrom reactable reactable reactableOutput renderReactable colDef reactableTheme
-#' @importFrom reactable getReactableState
-#' @importFrom reactable.extras text_extra dropdown_extra
-#' @importFrom rio export_list
-#' @importFrom rmarkdown render
-#' @importFrom shinyBS bsModal
-#' @importFrom shinyFiles shinyDirChoose
-#' @importFrom shinyjqui orderInput updateOrderInput
-#' @importFrom shinyWidgets dropdown pickerInput switchInput updatePickerInput
-#' @importFrom tools file_ext
-#' @importFrom utils read.csv write.csv
-#' @importFrom zip zipr
 #' @export
 run_app <- function(...) {
   require("aNCA")
   require("dplyr")
   require("DT")
+  require("forcats")
   require("ggplot2")
+  require("grid")
+  require("haven")
   require("htmlwidgets")
   require("logger")
+  require("nestcolor")
   require("PKNCA")
   require("plotly")
-  require("reactable")
   require("reactable.extras")
+  require("reactable")
   require("rio")
   require("rmarkdown")
+  require("shiny")
   require("shinyBS")
   require("shinyFiles")
   require("shinyjqui")
   require("shinyWidgets")
+  require("stats")
+  require("tern")
+  require("tidyr")
   require("tools")
   require("utils")
   require("zip")
