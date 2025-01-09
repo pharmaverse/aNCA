c9a4f840e55ee8e76e45cf36bfe42195a376ed47
Spinner <spinnerj@emea.roche.com>
2025-01-09 12:22:37 +0100

    test: update get label test

--- tests/testthat/test-get_label.R
+++ tests/testthat/test-get_label.R
@@ -1,11 +1,16 @@
 describe("get_label", {
   it("gets correct label from LABELs dataframe", {
+    LABELS <- data.frame(
+      Variable = c("USUBJID", "AVAL"),
+      Label = c("Unique Subject Identifier", "Analysis Value"),
+      Dataset = c("ADPC", "ADPC")
+      )
 
-    get_label("USUBJID", "ADPC")
+    get_label("USUBJID", "ADPC")  
     get_label("HEIGHT", "ADPC")
-
+    
     expect_equal(get_label("USUBJID", "ADPC"), "Unique Subject Identifier")
     expect_equal(get_label("HEIGHT", "ADPC"), "No label available")
-
+    
   })
 })
