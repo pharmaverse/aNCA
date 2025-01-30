afdbc5f346f6f906e41affbea8dd2f530656e150
Gero1999 <gerardojoro@gmail.com>
2025-01-30 17:16:15 +0100

    test: add tests for l_pkconc function. Fails last one (char to format 0)

---
+++ tests/testthat/test-l_pkconc.R
@@ -0,0 +1,174 @@
+library(testthat)
+library(dplyr)
+
+# Sample data frame for testing
+adpc <- data.frame(
+  PARAM = c("A", "A", "B", "B"),
+  PCSPEC = c("Plasma", "Plasma", "Plasma", "Plasma"),
+  ROUTE = c("Oral", "Oral", "IV", "IV"),
+  TRT01A = c("Treatment1", "Treatment1", "Treatment2", "Treatment2"),
+  USUBJID = c("1", "2", "3", "4"),
+  AVISIT = c("Visit1", "Visit1", "Visit2", "Visit2"),
+  NFRLT = c(0, 1, 0, 1),
+  AFRLT = c(0, 1, 0, 1),
+  AVAL = c(0, 20, 30, NA),
+  AVALU = "mg/L",
+  stringsAsFactors = FALSE
+)
+
+# Define labels
+attr(adpc[["AFRLT"]], "label") <- "Actual time from first dose"
+attr(adpc[["NFRLT"]], "label") <- "Planned time from first dose"
+attr(adpc[["AVAL"]], "label") <- "Analysis value"
+attr(adpc[["PARAM"]], "label") <- "Analyte"
+attr(adpc[["TRT01A"]], "label") <- "Actual treatment"
+attr(adpc[["PCSPEC"]], "label") <- "Specimen"
+attr(adpc[["ROUTE"]], "label") <- "Administration"
+attr(adpc[["USUBJID"]], "label") <- "Unique Subject ID"
+attr(adpc[["AVISIT"]], "label") <- "Actual visit"
+
+describe("l_pkconc", {
+  it("creates listings for each unique combination of grouping variables", {
+    
+    
+    # For 1 case
+    listings_1l <- l_pkconc(adpc, listgroup_vars = c("PCSPEC"),
+                            grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                            displaying_vars = c("NFRLT", "AFRLT", "AVAL"))
+    expect_length(listings_1l, length(unique(adpc$PCSPEC)))
+    expect_named(listings_1l, expected = as.character(unique(adpc$PCSPEC)))
+    
+    # For 2 case
+    listings_2l <- l_pkconc(adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                            grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                            displaying_vars = c("NFRLT", "AFRLT", "AVAL"))
+    expect_length(listings_2l, length(unique(interaction(adpc$PARAM, adpc$PCSPEC, adpc$ROUTE))))
+    expect_named(listings_2l, expected = as.character(unique(interaction(adpc$PARAM, adpc$PCSPEC, adpc$ROUTE))))
+    
+    # For 4 case
+    listings_4l <- l_pkconc(adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE", "USUBJID"),
+                            grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                            displaying_vars = c("NFRLT", "AFRLT", "AVAL"))
+    
+    # Include lenghts and names expected
+    expect_length(listings_4l, length(unique(interaction(adpc$PARAM, adpc$PCSPEC, adpc$ROUTE, adpc$USUBJID))))
+    expect_named(listings_4l, expected = as.character(interaction(adpc$PARAM, adpc$PCSPEC, adpc$ROUTE, adpc$USUBJID)))
+    
+    # All in correct format (list) and each internal element internally inherits listing_df
+    expect_true(class(listings_1l) == "list" &&
+                  class(listings_2l) == "list" && 
+                  class(listings_4l) == "list")
+    expect_true(all(sapply(listings_1l, inherits, "listing_df")) &&    
+                  all(sapply(listings_2l, inherits, "listing_df")) && 
+                  all(sapply(listings_4l, inherits, "listing_df")))
+  })
+  
+  it("handles missing formatting_vars_table and uses a default built:", {
+    listings <- l_pkconc(adpc, 
+                         listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                         grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                         displaying_vars = c("NFRLT", "AFRLT", "AVAL"),
+                         formatting_vars_table = NULL)
+    
+    # Adds labels as column names & includes units when obvious and present  (i.e, AVALU for AVAL)
+    expect_equal(var_labels(listings[[1]]),
+                 expected = c(TRT01A = attr(adpc$TRT01A, "label"),
+                              USUBJID = attr(adpc$USUBJID, "label"),
+                              AVISIT = attr(adpc$AVISIT, "label"),
+                              NFRLT = attr(adpc$NFRLT, "label"), 
+                              AFRLT = attr(adpc$AFRLT, "label"), 
+                              AVAL = paste0(attr(adpc$AVAL, "label"), " (", unique(adpc$AVALU), ")"),
+                              AVALU = "AVALU"))
+  })
+  
+  it("handles missing title_table and creates a default", {
+    listings <- l_pkconc(adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                         grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                         displaying_vars = c("NFRLT", "AFRLT", "AVAL"),
+                         title_table = NULL)
+    
+    # Check if the title is in the correct format
+    expect_equal(attr(listings[[1]], "subtitles"),
+                 expected = "Analyte: A\nSpecimen: Plasma\nAdministration: Oral")
+    
+  })
+  
+  it("handles missing footnote_table (no footnote)", {
+    listings <- l_pkconc(adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                         grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                         displaying_vars = c("NFRLT", "AFRLT", "AVAL"),
+                         footnote_table = NULL)
+    expect_equal(attr(listings[[1]], "main_footer"), expected =  character())
+  })
+  
+  it("handles empty data frame by providing empty list", {
+    
+    # Define the input for the function
+    empty_adpc <- adpc[0, ]
+    
+    # Define the expected output
+    empty_list <- list()
+    names(empty_list) <- character()
+    
+    # Check if the output is as expected
+    expect_equal(l_pkconc(empty_adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                          grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                          displaying_vars = c("NFRLT", "AFRLT", "AVAL")),
+                 expected = empty_list)
+  })
+  
+  it("handles missing required columns", {
+    incomplete_adpc <- adpc %>% select(-AFRLT)
+    expect_error(l_pkconc(incomplete_adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                          grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                          displaying_vars = c("NFRLT", "AFRLT", "AVAL")),
+                 "Missing required columns: AFRLT")
+  })
+  
+  it("handles non-unique units per list group", {
+    non_unique_units_adpc <- adpc
+    non_unique_units_adpc$AVALU <- c("ng/mL", "mg/mL", "mg/L", "mg/L")
+    expect_warning(l_pkconc(non_unique_units_adpc,
+                            listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                            grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                            displaying_vars = c("NFRLT", "AFRLT", "AVAL")),
+                   "pkcl01, but not unique unit in A.Plasma.Oral for AVAL :ng/mL, mg/mL")
+  })
+  
+  it("handles custom formatting_vars_table", {
+    custom_formatting_vars_table <- data.frame(
+      Variable_name = c("TRT01A", "USUBJID", "AVISIT", "NFRLT", "AFRLT", "AVAL"),
+      Label = c("Treatment", "Subject ID", "Visit", "Nominal Time", "Actual Time", "Value"),
+      na_str = c("NA", "NA", "NA", "NA", "NA", "NA"),
+      cero_str = c("0", "0", "0", "0", "0", "BLQ"),
+      align = c("center", "center", "center", "center", "center", "center"),
+      format = c("NA", "NA", "NA", "NA", "NA", "missing"),
+      unit = c(NA, NA, NA, NA, NA, "AVALU"),
+      stringsAsFactors = FALSE
+    )
+    
+    listings <- l_pkconc(adpc, listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
+                         grouping_vars = c("TRT01A", "USUBJID", "AVISIT"),
+                         displaying_vars = c("NFRLT", "AFRLT", "AVAL"),
+                         formatting_vars_table = custom_formatting_vars_table)
+    
+    expect_length(listings, 4)
+    expect_true(all(sapply(listings, inherits, "listing")))
+    
+    # Check if 0s, NAs, and units are not present in the rlisting after formatting
+    for (listing in listings) {
+      data <- listing$data
+      expect_true(all(data$NFRLT != 0))
+      expect_true(all(data$AFRLT != 0))
+      expect_true(all(data$AVAL != 0))
+      expect_true(all(data$NFRLT != "NA"))
+      expect_true(all(data$AFRLT != "NA"))
+      expect_true(all(data$AVAL != "NA"))
+      expect_true(all(grepl("RRLTU", names(data))))
+      expect_true(all(grepl("AVALU", names(data))))
+    }
+    # Check if in the intial adpc rows with 0 and NA values in AVAL are substituted by BLQ and "missing"
+    expect_true(all(adpc$AVAL == c("BLQ", 20, 30, "missing")))
+    
+  })
+})
