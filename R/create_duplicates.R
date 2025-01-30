4ac943122e720b35e4bd7cba532c6eae6b0760cb
Spinner <spinnerj@emea.roche.com>
2025-01-30 17:06:27 +0100

    test: add testing for create_duplicates

--- R/create_duplicates.R
+++ R/create_duplicates.R
@@ -15,13 +15,21 @@
 #'
 #' @return A data frame with adjusted concentration data, including:
 #'   - Duplicated predose values assigned to the previous dose.
-#'   - Duplicated last values assigned to the next dose if predose values are missing.
+#'   - Duplicated last values assigned to the next dose if predose values are missing,
+#'   filtered so only samples within 24 hours of the next dose are included.
 #'   - Sorted by the grouping variables and relative time.
 #'
 #' @examples
 #' # Example usage
+#' conc_data <- data.frame(
+#'  USUBJID = c("001", "001", "001", "001", "001", "001"),
+#'  DOSNO = c(1, 1, 1, 2, 2, 2),
+#'  ARRLT = c(-1, 0, 1, 1, 2, 3),
+#'  AFRLT = c(0, 0.5, 1, 3, 4, 5),
+#'  NRRLT = c(0, 1, 2, 1, 2, 3),
+#'  NFRLT = c(0, 0.5, 1, 3, 4, 5),
 #' result <- create_duplicates(conc_data,
-#'                             groups = c("USUBJID", "ANALYTE", "PCSPEC"), dosno = "DOSNO")
+#'                             groups = c("USUBJID"), dosno = "DOSNO")
 #'
 #' @export
 create_duplicates <- function(conc_data,
@@ -36,7 +44,7 @@ create_duplicates <- function(conc_data,
     data %>%
       group_by(across(all_of(keys)))
   }
-  
+
   # Step 1: Identify the dosing times (ARRLT == 0)
   dose_times <- conc_data %>%
     mutate(dose_time = .data[[afrlt]] - .data[[arrlt]],
@@ -49,33 +57,41 @@ create_duplicates <- function(conc_data,
   
   # Step 2: Calculate dosing intervals
   dosing_intervals <- dose_times %>%
-    group_keys(groups) %>%
+    group_keys(setdiff(groups, dosno)) %>%
     mutate(
       interval_prev = dose_time - lag(dose_time),
-      interval_prev = replace_na(interval_prev, 0),
       nom_interval_prev = nom_dose_time - lag(nom_dose_time),
-      nom_interval_prev = replace_na(nom_interval_prev, 0),
+      interval_next = lead(dose_time) - dose_time,
+      nom_interval_next = lead(nom_dose_time) - nom_dose_time,
+      next_dose = interval_next + dose_time,
+      next_nom_dose = nom_interval_next + nom_dose_time,
+      across(c(interval_prev, nom_interval_prev, interval_next,
+               nom_interval_next, next_dose, next_nom_dose), 
+             ~ replace_na(., 0))
     ) %>%
     ungroup()
   
   
   # Step 3: Duplicate predose values to be clast for the previous dose
   predose_duplicates <- conc_data %>%
-    filter(.data[[arrlt]] <= 0, .data[[dosno]] > 1) %>%
+    filter(.data[[arrlt]] < 0, .data[[dosno]] > 1) %>%
     left_join(dosing_intervals, by = c(groups)) %>%
     mutate(
       !!dosno := .data[[dosno]] - 1,
       !!arrlt := .data[[arrlt]] + interval_prev,
       !!nrrlt := .data[[nrrlt]] + nom_interval_prev
     ) %>%
-    select(-interval_prev, -nom_interval_prev)
+    select(-interval_prev, -nom_interval_prev, -interval_next,
+           -nom_interval_next, -next_dose, -next_nom_dose,
+           -dose_time, -nom_dose_time)
   
   # Step 4: Identify missing predose values for the next dose
   missing_predose <- dose_times %>%
     anti_join(
       conc_data %>% filter(.data[[arrlt]] < 0),
       by = c(groups)
-    )
+    ) %>%
+    mutate(!!dosno := .data[[dosno]] - 1)
   
   # Step 5: Duplicate last value of the previous dose to become predose of next
   last_values <- conc_data %>%
@@ -86,10 +102,13 @@ create_duplicates <- function(conc_data,
     left_join(dosing_intervals, by = c(groups)) %>%
     mutate(
       !!dosno := .data[[dosno]] + 1,
-      !!arrlt := .data[[arrlt]] - interval_prev,
-      !!nrrlt := .data[[nrrlt]] - nom_interval_prev
+      !!arrlt := .data[[afrlt]] - next_dose,
+      !!nrrlt := .data[[nfrlt]] - next_nom_dose
     ) %>%
-    select(-interval_prev, -nom_interval_prev)
+    filter(arrlt >= -24) %>%
+    select(-interval_prev, -nom_interval_prev, -interval_next,
+           -nom_interval_next, -next_dose, -next_nom_dose,
+           -dose_time, -nom_dose_time)
   
   # Step 6: Combine all data
   conc_data <- conc_data %>%
