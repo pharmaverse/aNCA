1650274a4f14405a71e8cb81000376d59dd8010b
Gero1999 <gerardojoro@gmail.com>
2025-01-31 10:10:59 +0100

    fix: apply digits format definition pre-listing creation

--- R/pkcl01.R
+++ R/pkcl01.R
@@ -28,7 +28,7 @@ attr(adpc[["AVISIT"]], "label") <- "Actual visit"
 #' @param grouping_vars A character vector specifying the grouping variables for the listing.
 #' @param displaying_vars A character vector specifying the variables to display in the listing.
 #' @param formatting_vars_table A data frame specifying the formatting options for the variables.
-#' @param subtitle_lists A character string specifying the title of the listing table.
+#' @param subtitle_lists A character string specifying the subtitle of the listing table.
 #' @param footnote_table A character string specifying the footnote of the listing table.
 #'
 #' @return A list of listings, each corresponding to a unique combination of the grouping variables.
@@ -40,6 +40,16 @@ attr(adpc[["AVISIT"]], "label") <- "Actual visit"
 #'   - Creates a listing for each unique combination of the grouping variables.
 #'   - Adds units to the variable labels in the listing.
 #'
+#' The `formatting_vars_table` should be a data frame with the following columns:
+#'   - `var_name`: The name of the variable.
+#'   - `Label`: The label for the variable.
+#'   - `na_str`: The string to use for NA values.
+#'   - `cero_str`: The string to use for 0 values.
+#'   - `align`: The alignment for the variable (e.g., "center").
+#'   - `format_fun`: The formatting function to use ("round" or "signif").
+#'   - `digits`: The number of digits to use for numeric formatting.
+#'   - `unit`: The unit column for the variable (if applicable).
+#'
 #' @examples
 #' \dontrun{
 #'   # Example usage:
@@ -49,7 +59,6 @@ attr(adpc[["AVISIT"]], "label") <- "Actual visit"
 #' @import dplyr formatters rlistings
 #' @export
 #' @author Gerardo Rodriguez
-
 l_pkconc <- function(
     adpc = read.csv("inst/shiny/data/DummyRO_ADNCA.csv"),
     listgroup_vars = c("PARAM", "PCSPEC", "ROUTE"),
@@ -78,18 +87,21 @@ l_pkconc <- function(
   if (is.null(formatting_vars_table)) {
     # Create a dataframe with rows c(grouping_Vars, displaying_vars) and columns c("Variable name", "Label", "Digits")
     formatting_vars_table <-  data.frame(
-      Variable_name = c(grouping_vars, displaying_vars)
+      var_name = c(grouping_vars, displaying_vars)
     ) %>% 
       rowwise()  %>% 
       # Create a label column
-      mutate(Label = parse_annotation(adpc, paste0("!", Variable_name)),
+      mutate(Label = parse_annotation(adpc, paste0("!", var_name)),
              na_str = "NA",
-             cero_str = ifelse(Variable_name == "AVAL", "BLQ", "0"),
+             cero_str = ifelse(var_name == "AVAL", "BLQ", "0"),
              align = "center",
              # ToDo: Mateusz // Check formatters::list_valid_format_labels() for input list
-             format = "NA",
-             unit = case_when(Variable_name == "AVAL" & "AVALU" %in% names(adpc) ~ "AVALU",
-                              Variable_name %in% c("NFRLT", "AFRLT") & "RRLTU" %in% names(adpc) ~ "RRLTU",
+             format_fun =  ifelse(is.numeric(adpc[[var_name]]) && 
+                                    !is.integer(adpc[[var_name]]), "round", NA),
+             digits = ifelse(is.numeric(adpc[[var_name]]) && 
+                               !is.integer(adpc[[var_name]]), 3, NA),
+             unit = case_when(var_name == "AVAL" & "AVALU" %in% names(adpc) ~ "AVALU",
+                              var_name %in% c("NFRLT", "AFRLT") & "RRLTU" %in% names(adpc) ~ "RRLTU",
                               TRUE ~ NA
              )
       ) %>% 
@@ -99,29 +111,46 @@ l_pkconc <- function(
   # Create the proper object from the UI table for col_formatting
   formatting_vars_list <- lapply(1:nrow(formatting_vars_table), function(i){
     row <- formatting_vars_table[i,]
-    fmt_config(format = if (row$format == "NA") NULL else row$format,
-               na_str = row$na_str,
+    fmt_config(na_str = row$na_str,
+               format = NULL,
                align = row$align
     )
   }) %>% 
-    setNames(nm = formatting_vars_table$Variable_name)
+    setNames(nm = formatting_vars_table$var_name)
   
   # Create a special object to map 0 string values to each variable
   format_cero <- formatting_vars_table %>% 
-    select(Variable_name, cero_str) %>% 
-    pull(cero_str, Variable_name)
+    select(var_name, cero_str) %>% 
+    pull(cero_str, var_name)
+  
+  # Create a special pair of objects to map number of digits to each variable
+  format_digits <- formatting_vars_table %>% 
+    select(var_name, digits) %>% 
+    filter(!is.na(format_fun) & !is.na(format_digits)) %>% 
+    pull(digits, var_name)
+  
+  format_funs <- formatting_vars_table %>% 
+    select(var_name, format_fun) %>% 
+    filter(!is.na(format_fun) & !is.na(format_digits)) %>% 
+    pull(format_fun, var_name)
   
   # Create a special object to map unit columns to each variable
   format_unit <- formatting_vars_table %>% 
-    select(Variable_name, unit) %>% 
+    select(var_name, unit) %>% 
     filter(!is.na(unit)) %>% 
-    pull(unit, Variable_name)
+    pull(unit, var_name)
   
   # Group the data based on the listgroup_vars
   adpc_grouped <- adpc %>% 
     mutate(across(all_of(listgroup_vars), as.character)) %>% 
     rowwise() %>% 
-    dplyr::mutate(id_list = interaction(!!!syms(listgroup_vars))) %>% 
+    dplyr::mutate(id_list = interaction(!!!syms(listgroup_vars))) %>%
+    
+    # Format number of digits
+    mutate(across(all_of(names(format_digits)), ~
+                    do.call(format_funs[cur_column()], 
+                            list(x = ., digits = format_digits[cur_column()]))
+    )) %>% 
     
     # Format 0 values as defined by format_cero
     mutate(across(all_of(names(format_cero)), ~
@@ -133,23 +162,23 @@ l_pkconc <- function(
   
   # Split the lists based on the listgroup_vars
   lapply(unique(adpc_grouped[["id_list"]]), \(id_val) {
-
+    
     data <- adpc_grouped %>% dplyr::filter(id_list ==  id_val)
     list_data = data %>% select(any_of(c(grouping_vars, displaying_vars, unname(format_unit))))
-
+    
     # For those variables with units specified include them in their labels for the table
     for (var_with_unit in names(format_unit)) {
       unit_val <- unique(data[[format_unit[var_with_unit]]])
       if (length(unit_val) != 1) {
         warning(paste0("pkcl01, but not unique unit in ", id_val, " for ",
-                        var_with_unit, " :", paste(unit_val, collapse = ", ")))
+                       var_with_unit, " :", paste(unit_val, collapse = ", ")))
       }
       attr(list_data[[var_with_unit]], "label") <- paste0(attr(list_data[[var_with_unit]], "label"),
                                                           " (",
                                                           paste0(unit_val, collapse = ","), 
                                                           ")")
     }
-
+    
     rl <- rlistings::as_listing(
       df = list_data,
       key_cols = grouping_vars,
@@ -161,7 +190,7 @@ l_pkconc <- function(
                                      text = footnote_table),
       col_formatting = formatting_vars_list
     )
-
+    
   })  %>% 
     
     setNames(unique(adpc_grouped[["id_list"]]))
