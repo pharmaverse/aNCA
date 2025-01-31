21eb81c296e313d86bbffebbd82080a8d7287fff
m-kolomanski <m.kolomanski@yahoo.com>
2025-01-31 12:42:59 +0100

    refactor: simplify global imports

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -1,16 +1,4 @@
-source("modules/tab_data.R")
-#Load labels
+lapply(list.files("modules", pattern = "\\.R$", full.names = TRUE), source)
+lapply(list.files("functions", pattern = "\\.R$", full.names = TRUE), source)
+
 LABELS <- read.csv(system.file("shiny/data/adnca_labels.csv", package = "aNCA"))
-
-source("modules/slope_selector.R")
-
-source("modules/units_table.R")
-
-source("functions/partial_auc_input.R")
-
-source("modules/tlg_plot.R")
-
-source("modules/tab_visuals.R")
-
-source("functions/mapping_selectize_inputs.R")
-source("functions/generate_col_defs.R")
