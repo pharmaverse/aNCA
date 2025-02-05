b62a75aa07c40fb7b0c48de3edc1bd815301229d
Gero1999 <gerardojoro@gmail.com>
2025-02-05 15:50:27 +0100

    refactor: lintr code cleaning

--- inst/shiny/functions/partial_auc_input.R
+++ inst/shiny/functions/partial_auc_input.R
@@ -11,10 +11,12 @@ partial_auc_input <- function(id, ns, min_sel_value = 0, max_sel_value = NULL) {
   fluidRow(
     id = id,
     column(
-      width = 6, numericInput(ns(paste0("timeInputMin_", id)), "Min:", min = 0, value = min_sel_value)
+      width = 6, numericInput(ns(paste0("timeInputMin_", id)),
+                              "Min:", min = 0, value = min_sel_value)
     ),
     column(
-      width = 6, numericInput(ns(paste0("timeInputMax_", id)), "Max:", min = 0, value = max_sel_value)
+      width = 6, numericInput(ns(paste0("timeInputMax_", id)),
+                              "Max:", min = 0, value = max_sel_value)
     )
   )
 }
