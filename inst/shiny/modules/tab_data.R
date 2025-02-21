0197472692b0ff9147778c2e5a3396c3c52a2875
Gero1999 <gerardojoro@gmail.com>
2025-02-21 11:34:09 +0100

    refactor: tab_data.R after change in filters for ADNCA()

--- inst/shiny/modules/tab_data.R
+++ inst/shiny/modules/tab_data.R
@@ -8,73 +8,72 @@ source(system.file("/shiny/modules/column_mapping.R", package = "aNCA"))
 
 tab_data_ui <- function(id) {
   ns <- NS(id)
-
+  
   navset_pill(
     id = ns("data_navset"),
     nav_panel("Raw Data Upload",
-      card(
-        "Upload your PK dataset in .csv format",
-        # Local upload option
-        fileInput(
-          ns("local_upload"),
-          width = "60%",
-          label = NULL,
-          placeholder = ".csv",
-          buttonLabel = list(icon("folder"), "Upload File..."),
-          accept = c(".csv", ".rds")
-        )
-      ),
-      card(
-        div(
-          class = "card-container",
-          h3("Filters"),
-          p("Click the 'Add Filters' button to add filters to your data.
+              card(
+                "Upload your PK dataset in .csv or .rds format",
+                # Local upload option
+                fileInput(
+                  ns("local_upload"),
+                  width = "60%",
+                  label = NULL,
+                  placeholder = ".csv / .rds",
+                  buttonLabel = list(icon("folder"), "Upload File..."),
+                  accept = c(".csv", ".rds")
+                )
+              ),
+              card(
+                div(
+                  class = "card-container",
+                  h3("Filters"),
+                  p("Click the 'Add Filters' button to add filters to your data.
             Be sure to click 'Submit' in order to apply the changes.\n
           Any filters added here will be applied across the whole analysis."),
-          actionButton(ns("add_filter"), "Add Filter"),
-          tags$div(id = ns("filters")),
-          div(
-            class = "filters-submit-button",
-            input_task_button(ns("submit_filters"), "Submit Filters")
-          )
-        )
-      ),
-      reactableOutput(ns("filecontents"))
+                  actionButton(ns("add_filter"), "Add Filter"),
+                  tags$div(id = ns("filters")),
+                  div(
+                    class = "filters-submit-button",
+                    input_task_button(ns("submit_filters"), "Submit Filters")
+                  )
+                )
+              ),
+              reactableOutput(ns("filecontents"))
     ),
     nav_panel("Mapping and Filters",
-      layout_columns(
-        card(
-          column_mapping_ui(ns("column_mapping"))
-        )
-      )
+              layout_columns(
+                card(
+                  column_mapping_ui(ns("column_mapping"))
+                )
+              )
     ),
     nav_panel("Review Data",
-      id = ns("data_navset-review"),
-      uiOutput(ns("reviewDataContent")),
-      tags$script(HTML("
+              id = ns("data_navset-review"),
+              uiOutput(ns("reviewDataContent")),
+              tags$script(HTML("
       $(document).ready(function(){
       $('[data-toggle=\"tooltip\"]').tooltip();
       });
                     ")
-      )
+              )
     )
   )
-
+  
 }
 
 tab_data_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
-
-    # DATA LOADING -----------------------------------------------------------------
-    # Load the dummy ADNCA example for the user as default
+    
+    # Reactive values
     ADNCA <- reactiveVal(
       read.csv(
         system.file("shiny/data/Dummy_complex_data.csv", package = "aNCA"),
         na.strings = c("", "NA")
       )
     )
-
+    
     # Load data provided by user
     observeEvent(input$local_upload, {
       new_adnca <- switch(
@@ -83,10 +82,13 @@ tab_data_server <- function(id) {
         rds = readRDS(input$local_upload$datapath),
         validate("Invalid file type. Only accepted are .csv and .rds")
       )
-
       ADNCA(new_adnca)
     })
 
+    ADNCA_filt <- reactiveVal(NULL)
+    filters <- reactiveValues()
+
+    # Render raw data table
     output$filecontents <- renderReactable({
       req(ADNCA_filt())
       reactable(
@@ -104,9 +106,24 @@ tab_data_server <- function(id) {
       )
     })
 
-    # Column Mapping ----
+    # Handle user-provided filters
+    observeEvent(input$add_filter, {
+      filter_id <- paste0("filter_", input$add_filter)
+      insertUI(
+        selector = paste0("#", session$ns("filters")),
+        ui = input_filter_ui(session$ns(filter_id), colnames(ADNCA()))
+      )
+      filters[[filter_id]] <- input_filter_server(filter_id)
+    })
 
-    # Define the manual units for concentration, dose, and time in a format recognized by PKNCA
+    # Apply filters to data
+    observeEvent(list(input$submit_filters, ADNCA()), {
+      applied_filters <- lapply(reactiveValuesToList(filters), \(x) x())
+      filtered_data <- apply_filters(ADNCA(), applied_filters)
+      ADNCA_filt(filtered_data)
+    }, ignoreInit = FALSE)
+    
+    # Column Mapping
     manual_units <- list(
       concentration = c("mg/L", "µg/mL", "ng/mL", "pg/mL",
                         "mol/L", "mmol/L", "µmol/L", "nmol/L",
@@ -118,12 +135,10 @@ tab_data_server <- function(id) {
       time = c("sec", "min", "hr", "day", "week", "month", "year")
     )
 
-    # Define the callback function to change the tab
     change_to_review_tab <- function() {
       updateTabsetPanel(session, "data_navset", selected = "Review Data")
     }
 
-    # Call the column mapping module
     column_mapping <- column_mapping_server(
       id = "column_mapping",
       data = ADNCA,
@@ -131,65 +146,15 @@ tab_data_server <- function(id) {
       on_submit = change_to_review_tab
     )
 
-    output$reviewDataContent <- renderUI({
-      if (!is.null(processed_data()) && nrow(processed_data()) > 0) {
-        tagList(
-          "This is the data set that will be used for the analysis.
-          If you would like to make any changes please return to the 'Mapping and Filters' tab.",
-          reactableOutput(ns("data_processed"))
-        )
-      } else {
-        div(
-          "Please map your data in the 'Mapping and Filters' section before reviewing it."
-        )
-      }
-    })
-
-    # Reactive value for the processed dataset
     processed_data <- column_mapping$processed_data
-
-    # Global variable to store grouping variables
     grouping_variables <- column_mapping$grouping_variables
-
-    # Handle user-provided filters
-    filters <- reactiveValues()
-
-    observeEvent(input$add_filter, {
-      # Create a unique ID for each filter
-      filter_id <- paste0("filter_", input$add_filter)
-
-      # Insert a new filter UI
-      insertUI(
-        selector = paste0("#", session$ns("filters")),
-        ui = input_filter_ui(session$ns(filter_id), colnames(ADNCA()))
-      )
-
-      filters[[filter_id]] <- input_filter_server(filter_id)
-    })
-
-    # Create reactive value with applied filters
-    data <- reactiveVal(NULL)
-    ADNCA_filt <- reactiveVal(NULL)
-    observeEvent(list(input$submit_filters, ADNCA()), {
-      # Extract filters from reactive values
-      applied_filters <- lapply(reactiveValuesToList(filters), \(x) x())
-
-      # Filter and overwrite data
-      filtered_data <- apply_filters(
-        ADNCA(), applied_filters
-      )
-      ADNCA_filt(filtered_data)
-    }, ignoreInit = FALSE)
-
-    # Update the data table object with the filtered data
+    
+    # Render processed data table
     output$data_processed <- renderReactable({
-      req(data())
-
-      # Generate column definitions
-      col_defs <- generate_col_defs(data())
-
+      req(processed_data())
+      col_defs <- generate_col_defs(processed_data())
       reactable(
-        data(),
+        processed_data(),
         columns = col_defs,
         searchable = TRUE,
         sortable = TRUE,
@@ -204,8 +169,23 @@ tab_data_server <- function(id) {
       )
     })
 
+    # Render review data content
+    output$reviewDataContent <- renderUI({
+      if (!is.null(processed_data()) && nrow(processed_data()) > 0) {
+        tagList(
+          "This is the data set that will be used for the analysis.
+          If you would like to make any changes please return to the 'Mapping and Filters' tab.",
+          reactableOutput(ns("data_processed"))
+        )
+      } else {
+        div(
+          "Please map your data in the 'Mapping and Filters' section before reviewing it."
+        )
+      }
+    })
+
     list(
-      data = data,
+      data = processed_data,
       grouping_variables = grouping_variables
     )
   })
