42681d5f270063f6ab74012ec019bd77f0af01f3
Gero1999 <gerardojoro@gmail.com>
2025-03-14 11:21:02 +0100

    feat: incorporate save_settings module to tab_nca.R

--- inst/shiny/modules/tab_nca.R
+++ inst/shiny/modules/tab_nca.R
@@ -14,7 +14,8 @@ tab_nca_ui <- function(id) {
         "Setup", fluid = TRUE,
         navset_pill_list(
           nav_panel("NCA settings", nca_setup_ui(ns("nca_settings"))),
-          nav_panel("Slope Selector", slope_selector_ui(ns("slope_selector")))
+          nav_panel("Slope Selector", slope_selector_ui(ns("slope_selector"))),
+          nav_panel("Save setts", save_settings_ui(ns("save_settings")))
         )
       ),
       nav_panel(
@@ -276,74 +277,7 @@ tab_nca_server <- function(id, data, grouping_vars) {
     # NCA SETTINGS ----
     # TODO: move this section to a new module
     # Save the project settings
-    output$settings_save <- downloadHandler(
-      filename = function() {
-        paste(mydata()$conc$data$STUDYID[1], "NCA_settings.csv", sep = "_")
-      },
-      content = function(file) {
-
-        # Get the data settings from the NCA results (data run)
-        myconc <- res_nca()$data$conc
-
-        # Create a settings file that the user can download/upload
-        #for establishing the same configuration
-        setts_lambda <- myconc$data %>%
-          # Identify the points that the user has manually selected for the half-life calculation
-          mutate(
-            TYPE = case_when(is.excluded.hl ~ "Exclusion", is.included.hl ~ "Selection", TRUE ~ NA)
-          ) %>%
-          filter(is.excluded.hl | is.included.hl)  %>%
-          select(any_of(c(
-            unname(unlist(myconc$columns$groups)),
-            "IX",
-            myconc$columns$time,
-            myconc$columns$concentration,
-            "TYPE",
-            "REASON"
-          )))
-
-        # Make sure that there is at least one row so the settings can be considered
-        if (nrow(setts_lambda) == 0) {
-          setts_lambda <- setts_lambda %>%
-            add_row()
-        }
-
-        # Consider the intervals defined by the user for the AUC calculation
-        input_names_aucmin <- grep("^timeInputMin_", names(input), value = TRUE)
-        input_names_aucmax <- grep("^timeInputMax_", names(input), value = TRUE)
-        auc_mins <- unlist(lapply(input_names_aucmin, function(name) input[[name]]))
-        auc_maxs <- unlist(lapply(input_names_aucmax, function(name) input[[name]]))
-
-        # Include the rule settings as additional columns
-        setts <- setts_lambda %>%
-          mutate(
-            ANALYTE %in% input$select_analyte,
-            doses_selected = ifelse(
-              !is.null(input$select_dosno),
-              paste0(input$select_dosno, collapse = ","),
-              unique(mydata()$conc$data$DOSNO)
-            ),
-            method = input$method,
-            adj.r.squared_threshold = ifelse(
-              input$rule_adj_r_squared, input$adj.r.squared_threshold, NA
-            ),
-            aucpext.obs_threshold = ifelse(
-              input$rule_aucpext_obs, input$aucpext.obs_threshold, NA
-            ),
-            aucpext.pred_threshold = ifelse(
-              input$rule_aucpext_pred, input$aucpext.pred_threshold, NA
-            ),
-            span.ratio_threshold = ifelse(
-              input$rule_span_ratio, input$span.ratio_threshold, NA
-            ),
-            auc_mins = if (is.null(auc_mins)) NA else paste(auc_mins, collapse = ","),
-            auc_maxs = if (is.null(auc_maxs)) NA else paste(auc_maxs, collapse = ",")
-          )
-
-        write.csv(setts, file, row.names = FALSE)
-      },
-      contentType = "text/csv"
-    )
+    save_settings_server("save_settings", mydata, res_nca)
 
     # ADDITIONAL ANALYSIS ----
     additional_analysis_server("non_nca", mydata, grouping_vars)
