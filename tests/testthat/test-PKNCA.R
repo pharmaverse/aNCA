30eef3603c0ae40a0ceebb3a9bbefa057da72543
Jana Spinner <spinnerjana@gmail.com>
2025-03-28 08:50:12 +0100

    docs and lint

--- tests/testthat/test-PKNCA.R
+++ tests/testthat/test-PKNCA.R
@@ -91,16 +91,16 @@ describe("PKNCA_create_data_object", {
 
 # Test PKNCA_update_data_object
 describe("PKNCA_update_data_object", {
-  
+
   method <- "lin up log down"
   params <- c("cmax", "tmax", "auclast", "aucinf.obs")
   analytes <- unique(simple_data$ANALYTE)
   dosnos <- unique(simple_data$DOSNO)
   pcspecs <- unique(simple_data$PCSPEC)
   default_units <- pknca_data$units
-  
+
   ma_data <- PKNCA_create_data_object(multiple_data)
-  
+
   it("returns a PKNCAdata object", {
     updated_data <- PKNCA_update_data_object(
       adnca_data = pknca_data,
@@ -113,7 +113,7 @@ describe("PKNCA_update_data_object", {
     )
     expect_s3_class(updated_data, "PKNCAdata")
   })
-  
+
   it("includes only selected analytes, dosnos, and pcspecs in intervals", {
     updated_data <- PKNCA_update_data_object(
       adnca_data = ma_data,
@@ -129,7 +129,7 @@ describe("PKNCA_update_data_object", {
     expect_true(all(intervals$DOSNO == 1))
     expect_true(all(intervals$PCSPEC == "Plasma"))
   })
-  
+
   it("updates units for each analyte", {
     updated_data <- PKNCA_update_data_object(
       adnca_data = pknca_data,
@@ -143,7 +143,7 @@ describe("PKNCA_update_data_object", {
     unit_analytes <- unique(updated_data$units$ANALYTE)
     expect_setequal(unit_analytes, analytes)
   })
-  
+
   it("sets NCA options correctly", {
     updated_data <- PKNCA_update_data_object(
       adnca_data = pknca_data,
@@ -172,7 +172,7 @@ describe("PKNCA_update_data_object", {
     expect_true("impute" %in% names(updated_data))
     expect_true(all(is.na(updated_data$impute)))
   })
-  
+
   it("ensures units table has separate rows per analyte", {
     updated_data <- PKNCA_update_data_object(
       adnca_data = pknca_data,
@@ -183,25 +183,25 @@ describe("PKNCA_update_data_object", {
       params = c("cmax", "tmax", "auclast", "aucinf.obs"),
       should_impute_c0 = TRUE
     )
-    
+
     # Get units table and analyte column
     units_table <- updated_data$units
     analyte_col <- updated_data$conc$columns$groups$group_analyte
-    
+
     # Extract number of unique analytes and parameters
     analytes <- unique(units_table[[analyte_col]])
     params <- unique(units_table$PPTESTCD)
-    
+
     # Expect a row for every analyte-param combination
     expected_nrows <- length(analytes) * length(params)
     expect_equal(nrow(units_table), expected_nrows)
-    
+
     # Sanity: make sure all analyte-param pairs exist
     pair_check <- tidyr::crossing(
       ANALYTE = analytes,
       PPTESTCD = params
     )
-    
+
     expect_true(all(
       dplyr::semi_join(pair_check, units_table, by = c("ANALYTE", "PPTESTCD")) %>%
         nrow() == nrow(pair_check)
