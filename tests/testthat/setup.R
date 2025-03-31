e4f954699bf1f49b254e3c0372eb1b908f14d64a
Jana Spinner <spinnerjana@gmail.com>
2025-03-31 15:27:12 +0200

    test: update testing names

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,18 +1,18 @@
 # Import dataset from testthat/data folder
 
-data <- read.csv(testthat::test_path("data", "adnca_dummy_sm_dataset.csv"))
+DUMMY_DATA_FIXTURE <- read.csv(testthat::test_path("data", "adnca_dummy_sm_dataset.csv"))
 
 # Create PKNCAdata object
-pknca_data <- PKNCA_create_data_object(data %>% filter(PCSPEC == "Plasma"))
+PKNCA_DATA_FIXTURE <- PKNCA_create_data_object(DUMMY_DATA_FIXTURE %>% filter(PCSPEC == "Plasma"))
 # Set intervals
-pknca_data$intervals <- format_pkncadata_intervals(
-  pknca_data$conc, pknca_data$dose,
+PKNCA_DATA_FIXTURE$intervals <- format_pkncadata_intervals(
+  PKNCA_DATA_FIXTURE$conc, PKNCA_DATA_FIXTURE$dose,
   params = c("aucinf.obs", "aucint.last", "auclast",
              "cmax", "half.life", "tmax",
              "lambda.z", "lambda.z.n.points",
              "r.squared", "adj.r.squared", "lambda.z.time.first")
 )
-pknca_data <- create_start_impute(pknca_data)
+PKNCA_DATA_FIXTURE <- create_start_impute(PKNCA_DATA_FIXTURE)
 
 # Create NCA results
-res_nca <- PKNCA_calculate_nca(pknca_data)
+PKNCA_RESULTS_FIXTURE <- PKNCA_calculate_nca(PKNCA_DATA_FIXTURE)
