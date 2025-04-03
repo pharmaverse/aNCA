76009bcfac54c4f256343979c9e6c282131755fb
Jana Spinner <spinnerjana@gmail.com>
2025-04-03 19:14:24 +0200

    refactor: parameter selection in table with defaults

--- inst/shiny/modules/tab_nca/nca_setup.R
+++ inst/shiny/modules/tab_nca/nca_setup.R
@@ -51,18 +51,8 @@ nca_setup_ui <- function(id) {
         ),
         accordion_panel(
           title = "Parameter Selection",
-          selectizeInput(
-            inputId = ns("nca_params"),
-            label = "NCA parameters to calculate:",
-            choices = pull(pknca_cdisc_terms, PKNCA, input_names),
-            multiple = TRUE,
-            selected = c("cmax", "tmax", "half.life", "cl.obs", "auclast",
-                         "aucinf.pred", "aucinf.obs", "aucinf.obs.dn",
-                         "adj.r.squared", "lambda.z", "lambda.z.n.points",
-                         "cav", "cl.all", "cl.obs",
-                         "clast", "tlast")
-          ),
-          reactableOutput(ns("nca_parameters"))
+          reactableOutput(ns("nca_parameters")),
+          verbatimTextOutput(ns("selected_params_text"))
         ),
         accordion_panel(
           title = "Data Imputation",
@@ -396,6 +386,58 @@ nca_setup_server <- function(id, data, adnca_data) { # nolint : TODO: complexity
         selected = unique(data()$PCSPEC)[1]
       )
     })
+    
+    DEFAULT_PARAMS <- c("aucinf.obs", "aucinf.obs.dn",
+      "aucint.last",
+      "auclast", "auclast.dn",
+      "cmax", "cmax.dn",
+      "clast.obs", "clast.obs.dn",
+      "tlast", "tmax",
+      "half.life", "cl.obs", "vss.obs", "vz.obs",
+      "mrt.last", "mrt.obs",
+      "lambda.z",
+      "lambda.z.n.points", "r.squared",
+      "adj.r.squared", "lambda.z.time.first",
+      "aucpext.obs", "aucpext.pred")
+    
+    output$nca_parameters <- renderReactable({
+      #remove parameters that are currently unavailable in PKNCA
+      params_data <- pknca_cdisc_terms %>%
+        filter(!PPTESTCD %in% c("FAB", "FREL"))
+      
+      default_row_indices <- which(params_data$PKNCA %in% DEFAULT_PARAMS)
+      
+      reactable(
+        params_data %>%
+          select(CAT, TYPE, PPTESTCD, PPTEST),
+        groupBy = c("TYPE"),
+        pagination = FALSE,
+        filterable = TRUE,
+        compact = TRUE,
+        onClick = "select",
+        height = "49vh",
+        selection = "multiple",
+        defaultSelected = default_row_indices
+      )
+    })
+    
+    nca_params <- reactive({
+      selected_rows <- getReactableState("nca_parameters", "selected")
+      if (is.null(selected_rows) || length(selected_rows) == 0) return(NULL)
+      
+      params_data <- pknca_cdisc_terms %>%
+        filter(!PPTESTCD %in% c("FAB", "FREL"))
+      selected_terms <- params_data[selected_rows, , drop = FALSE]
+      
+      # Return PKNCA column names
+      selected_terms$PKNCA
+    })
+    
+    output$selected_params_text <- renderPrint({
+      req(nca_params())
+      cat("Selected PKNCA parameters:\n")
+      print(nca_params())
+    })
 
     # Reactive value to store the AUC data table
     auc_data <- reactiveVal(
@@ -423,19 +465,6 @@ nca_setup_server <- function(id, data, adnca_data) { # nolint : TODO: complexity
     }) %>%
       shiny::bindEvent(refresh_reactable())
     
-    output$nca_parameters <- renderReactable({
-      reactable(
-        pknca_cdisc_terms %>%
-          select(PPTESTCD, PPTEST, type),
-        groupBy = "type",
-        pagination = FALSE,
-        filterable = TRUE,
-        compact = TRUE,
-        onClick = "select",
-        height = "49vh",
-        selection = "multiple"  # Enable multiple selection
-      )
-    })
 
     # Add a blank row on button click
     observeEvent(input$addRow, {
@@ -462,9 +491,9 @@ nca_setup_server <- function(id, data, adnca_data) { # nolint : TODO: complexity
 
     # Updating Checkbox and Numeric Inputs
     observeEvent(list(input$rule_adj_r_squared, input$rule_aucpext_obs,
-                      input$rule_aucpext_pred, input$nca_params), {
+                      input$rule_aucpext_pred, nca_params()), {
 
-                   nca_params <- input$nca_params
+                   nca_params <- nca_params()
                    if (input$rule_adj_r_squared) nca_params <- c(nca_params, "adj.r.squared")
                    if (input$rule_aucpext_obs) nca_params <- c(nca_params, "aucpext.obs")
                    if (input$rule_aucpext_pred) nca_params <- c(nca_params, "aucpext.pred")
@@ -481,7 +510,7 @@ nca_setup_server <- function(id, data, adnca_data) { # nolint : TODO: complexity
         adnca_data(),
         auc_data(),
         input$method,
-        input$nca_params,
+        nca_params(),
         input$should_impute_c0,
         input$select_analyte,
         input$select_dosno,
@@ -526,7 +555,7 @@ nca_setup_server <- function(id, data, adnca_data) { # nolint : TODO: complexity
         selected_analytes = input$select_analyte,
         selected_dosno = input$select_dosno,
         selected_pcspec = input$select_pcspec,
-        params = input$nca_params,
+        params = nca_params(),
         should_impute_c0 = input$should_impute_c0
       )
       log_success("PKNCA data object updated.")
