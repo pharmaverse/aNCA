4d82f2288c4e71970e2c5186e24bbdd5262222a7
jimrothstein <jimrothstein@gmail.com>
2025-04-07 15:55:57 -0700

    287 add tests: remove unnecessary tests

--- tests/testthat/test-label_operators.R
+++ tests/testthat/test-label_operators.R
@@ -5,11 +5,14 @@ LABELS <- data.frame(
 )
 
 describe("get_labels", {
-  it("return label of heading if it exists in label file", {
+
+  # Test for successful label retrieval
+  it("returns label of a heading if it exists in the label file", {
     expect_equal(get_label(LABELS, "USUBJID", "ADPC"), "Unique Subject Identifier")
-    expect_error(get_label())
-    expect_error(get_label(LABELS, "USUBJID"))
-    expect_no_error(get_label(LABELS, "USUBJID", "ADP")) 
+  })
+
+  # Test for fallback behavior when no label is available
+  it("returns 'No label available' if the label does not exist", {
     expect_equal(get_label(LABELS, "USUBJID", "ADP"), "No label available")
   })
 })
