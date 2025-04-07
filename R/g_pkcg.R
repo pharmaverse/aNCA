8a4025a92f1f04f71f89c6dc3ab8adaa74913902
Kobana <kobanak@exbp.roche.com>
2025-04-07 09:49:35 +0200

    feat: creation of combined PK plots, update of tlg.yaml, creation of a single file to store all graph functions

--- R/g_pkcg.R
+++ R/g_pkcg.R
@@ -1,22 +1,22 @@
-#' Wrapper around aNCA::pkcg02() function. Calls the function with `LIN` scale argument.
+#' Wrapper around aNCA::pkcg01() function. Calls the function with `LIN` scale argument.
 #' @param data Data to be passed into the plotting function.
 #' @param ...  Any other parameters to be passed into the plotting function.
-#' @returns ggplot2 object for pkcg02.
+#' @returns ggplot2 object for pkcg01.
 #' @export
-g_pkconc_lin <- function(data, ...) {
-  pkcg02(adpc = data, scale = "LIN", ...)
+g_pkconc_ind_lin <- function(data, ...) {
+  pkcg01(adpc = data, scale = "LIN", ...)
 }
 
-#' Wrapper around aNCA::pkcg02() function. Calls the function with `LOG` scale argument.
+#' Wrapper around aNCA::pkcg01() function. Calls the function with `LOG` scale argument.
 #' @param data Data to be passed into the plotting function.
 #' @param ...  Any other parameters to be passed into the plotting function.
-#' @returns ggplot2 object for pkcg02.
+#' @returns ggplot2 object for pkcg01.
 #' @export
-g_pkconc_log <- function(data, ...) {
-  pkcg02(adpc = data, scale = "LOG", ...)
+g_pkconc_ind_log <- function(data, ...) {
+  pkcg01(adpc = data, scale = "LOG", ...)
 }
 
-#' Generate PK Concentration-Time Profile Plot by Cohort
+#' Generate PK Concentration-Time Profile Plots
 #'
 #' This function generates a list of ggplots for PK concentration-time profiles.
 #'
@@ -54,9 +54,9 @@ g_pkconc_log <- function(data, ...) {
 #'   attr(adpc[["AFRLT"]], "label") <- "Actual time from first dose"
 #'   attr(adpc[["AVAL"]], "label") <- "Analysis val"
 #'
-#'   plots_lin <- pckg02(adpc = adpc, xmax = 1)
-#'   plots_log <- pckg02(adpc = adpc, color_var = "USUBJID", scale = "LOG")
-#'   plots_sbs <- pckg02(
+#'   plots_lin <- pckg01(adpc = adpc, xmax = 1)
+#'   plots_log <- pckg01(adpc = adpc, color_var = "USUBJID", scale = "LOG")
+#'   plots_sbs <- pckg01(
 #'     adpc = adpc,
 #'     color_var = "USUBJID",
 #'     xbreaks_var = "NFRLT",
@@ -68,7 +68,7 @@ g_pkconc_log <- function(data, ...) {
 #'
 #' @export
 #' @author Gerardo Rodriguez
-pkcg02 <-  function(
+pkcg01 <- function(
   adpc = data(),
   xvar = "AFRLT",
   yvar = "AVAL",
@@ -82,19 +82,19 @@ pkcg02 <-  function(
   xmax = NA,
   ymin = NA,
   ymax = NA,
-  xlab = paste0(xvar, " [", unique(adpc[[xvar_unit]]), "]"),
-  ylab = paste0(yvar, " [", unique(adpc[[yvar_unit]]), "]"),
+  xlab = paste0("!", xvar, " [$", xvar_unit, "]"),
+  ylab = paste0("!", yvar, " [$", yvar_unit, "]"),
   title = NULL,
   subtitle = NULL,
   footnote = NULL,
   # Inputs to split-by/seggregate plots
-  plotgroup_vars = c("ROUTE", "PCSPEC", "PARAM", "TRT01A"),
+  plotgroup_vars = c("ROUTE", "PCSPEC", "PARAM", "USUBJID"),
   # TODO(mateusz): Refactor when label attributes are implemented.
   plotgroup_names = list(
     "ROUTE" = "Route",
     "PCSPEC" = "Specimen",
     "PARAM" = "Analyte",
-    "TRT01A" = "Treatment"
+    "USUBJID" = "Subject ID"
   ),
 
   # Specific inputs (needs metadata specification),
@@ -111,7 +111,7 @@ pkcg02 <-  function(
   col_labels <- purrr::map(adpc, ~ attr(.x, "label"))
 
   adpc_grouped <- adpc %>%
-    mutate(across(c(all_of(plotgroup_vars), "USUBJID"), as.character)) %>%
+    mutate(across(all_of(plotgroup_vars), as.character)) %>%
     rowwise() %>%
     dplyr::mutate(id_plot = interaction(!!!syms(plotgroup_vars)))
 
@@ -133,11 +133,12 @@ pkcg02 <-  function(
     id_var = "USUBJID",
     add_baseline_hline = FALSE,
     yvar_baseline = yvar,
-    plotting_choices = "all_in_one"
-  )
+    plotting_choices = "separate_by_obs"
+  )[[1]]
 
   # Provide limits and additional potential future aesthetic customizations
   plot <- plot +
+    aes(color = NULL) +
     theme(
       plot.title = element_text(family = "sans", size = 14, color = "black"),
       plot.subtitle = element_text(family = "sans", size = 11, color = "black")
@@ -198,7 +199,6 @@ pkcg02 <-  function(
       )
   }
 
-
   # Create the list of plots for each unique group
   lapply(unique(adpc_grouped[["id_plot"]]), \(id_val) {
     plot_data <- adpc_grouped %>% dplyr::filter(id_plot ==  id_val)
@@ -229,6 +229,238 @@ pkcg02 <-  function(
             unique(plot_data[, plotgroup_vars]),
             sep = "",  collapse = ", "
           )
+        )
+      } else {
+        parse_annotation(plot_data, subtitle)
+      }
+    }
+
+    title_text <- paste0(title, "<br>", "<sup>", subtitle, "</sup>")
+    title_margin <- (0.5 * length(unlist(strsplit(title_text, "\n|<br>"))))
+
+    #' magic numbers for footnote position and margin, work in app up to 4 lines
+    footnote <- {
+      if (is.null(footnote)) {
+        ""
+      } else {
+        parse_annotation(plot_data, footnote)
+      }
+    }
+    footnote_y <- 0.1 + (0.05 * length(unlist(strsplit(footnote, "\n|<br>"))))
+
+    plotly_plot <- plot %+%
+      plot_data %+%
+      theme(
+        # add margin to make space for subtitle and footnote #
+        plot.margin = margin(
+          title_margin,
+          0,
+          footnote_y * 5,
+          0,
+          "cm"
+        )
+      ) %>%
+      ggplotly(
+        tooltip = c("x", "y"),
+        dynamicTicks = TRUE,
+        #' NOTE: might require some fine tuning down the line, looks fine now
+        height = 500 + (footnote_y * 25) + title_margin * 50
+      ) %>%
+      layout(
+        yaxis = list(autorange = FALSE),
+        xaxis = list(autorange = FALSE),
+        # title and subtitle #
+        title = list(text = title_text),
+        # footnote #
+        annotations = list(
+          x = 0,
+          y =  -footnote_y,
+          text = footnote,
+          showarrow = FALSE,
+          yref = "paper",
+          xref = "paper",
+          align = "left",
+          parse = TRUE
+        )
+      )
+
+    if (scale == "LOG") {
+      plotly_plot <- plotly_plot %>%
+        layout(yaxis = list(type = "log"))
+    }
+
+    plotly_plot
+  }) |>
+    setNames(unique(adpc[["id_plot"]]))
+}
+#' Wrapper around aNCA::pkcg02() function. Calls the function with `LIN` scale argument.
+#' @param data Data to be passed into the plotting function.
+#' @param ...  Any other parameters to be passed into the plotting function.
+#' @returns ggplot2 object for pkcg02.
+#' @export
+g_pkconc_lin <- function(data, ...) {
+  pkcg02(adpc = data, scale = "LIN", ...)
+}
+
+#' Wrapper around aNCA::pkcg02() function. Calls the function with `LOG` scale argument.
+#' @param data Data to be passed into the plotting function.
+#' @param ...  Any other parameters to be passed into the plotting function.
+#' @returns ggplot2 object for pkcg02.
+#' @export
+g_pkconc_log <- function(data, ...) {
+  pkcg02(adpc = data, scale = "LOG", ...)
+}
+
+#' Generate PK Concentration-Time Profile Plot by Cohort
+#' @export
+#' @author Kezia Kobana
+pkcg02 <- function(
+  adpc = data(),
+  xvar = "AFRLT",
+  yvar = "AVAL",
+  xvar_unit = "RRLTU",
+  yvar_unit = "AVALU",
+  color_var = NULL,
+  color_var_label = NULL,
+  xbreaks_var = "NFRLT",
+  xbreaks_mindist = 0.5,
+  xmin = NA,
+  xmax = NA,
+  ymin = NA,
+  ymax = NA,
+  xlab = paste0(xvar, " [", unique(adpc[[xvar_unit]])[1], "]"),
+  ylab = paste0(yvar, " [", unique(adpc[[yvar_unit]])[1], "]"),
+  title = NULL,
+  subtitle = NULL,
+  footnote = NULL,
+  plotgroup_vars = c("ROUTE", "PCSPEC", "PARAM", "TRT01A"),
+  plotgroup_names = list(
+    "ROUTE" = "Route",
+    "PCSPEC" = "Specimen",
+    "PARAM" = "Analyte",
+    "TRT01A" = "Treatment"
+  ),
+  scale = c("LIN", "LOG", "SBS")[1],
+  studyid = "STUDYID",
+  trt_var = "TRT01A"
+) {
+
+  xmin <- as.numeric(xmin)
+  xmax <- as.numeric(xmax)
+  ymin <- as.numeric(ymin)
+  ymax <- as.numeric(ymax)
+
+  col_labels <- purrr::map(adpc, ~ attr(.x, "label"))
+
+  adpc_grouped <- adpc %>%
+    dplyr::mutate(across(c(all_of(plotgroup_vars), "USUBJID"), as.character)) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(id_plot = interaction(!!!syms(plotgroup_vars)))
+
+  adpc_grouped <- purrr::map2_dfc(adpc_grouped, names(adpc_grouped), ~ {
+    attr(.x, "label") <- col_labels[[.y]]
+    .x
+  })
+
+  plot_data <- adpc_grouped %>% dplyr::filter(id_plot == id_plot[1])
+
+  plot <- tern::g_ipp(
+    df = plot_data,
+    xvar = xvar,
+    yvar = yvar,
+    xlab = parse_annotation(plot_data, xlab),
+    ylab = parse_annotation(plot_data, ylab),
+    id_var = "USUBJID",
+    add_baseline_hline = FALSE,
+    yvar_baseline = yvar,
+    plotting_choices = "all_in_one"
+  )
+
+  plot <- plot +
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(family = "sans", size = 14, color = "black"),
+      plot.subtitle = ggplot2::element_text(family = "sans", size = 11, color = "black")
+    ) +
+    ggplot2::coord_cartesian(xlim = c(xmin, xmax), ylim = c(ymin, ymax)) +
+    ggplot2::scale_x_continuous(
+      guide = ggplot2::guide_axis(n.dodge = 1),
+      breaks = filter_breaks(
+        plot_data[[xbreaks_var]],
+        min_cm_distance = xbreaks_mindist,
+        plot = plot
+      ),
+      labels = \(x) ifelse(x %% 1 == 0, as.character(as.integer(x)), as.character(x))
+    )
+
+  if (!is.null(color_var)) {
+    plot <- plot +
+      ggplot2::aes(color = !!sym(color_var)) +
+      ggplot2::theme(legend.position = "none")
+  }
+
+  if (!is.null(color_var_label) && length(color_var) > 1) {
+    plot <- plot +
+      ggplot2::labs(color = color_var_label) +
+      ggplot2::theme(legend.position = "bottom")
+  }
+
+  if (scale == "LOG") {
+    plot <- plot +
+      ggplot2::labs(y = paste0("Log 10 - ", plot$labels$y))
+  }
+
+  if (scale == "SBS") {
+    adpc_grouped <- rbind(adpc_grouped, adpc_grouped) %>%
+      dplyr::mutate(
+        view = c(
+          rep("Linear view", nrow(adpc_grouped)),
+          rep("Semilogarithmic view (Log10)", nrow(adpc_grouped))
+        ),
+        !!sym(yvar) := ifelse(
+          !!sym(yvar) < 1e-3 & view == "Semilogarithmic view (Log10)", yes = 1e-3, no = !!sym(yvar)
+        )
+      )
+
+    plot <- plot %+% dplyr::filter(adpc_grouped, id_plot == unique(id_plot)[1]) +
+      ggplot2::facet_wrap(~view, scales = "free_y") +
+      ggh4x::scale_y_facet(
+        view == "Semilogarithmic view (Log10)",
+        trans = "log10",
+        breaks = scales::breaks_log(),
+        labels = scales::label_log()
+      )
+  }
+
+  lapply(unique(adpc_grouped[["id_plot"]]), \(id_val) {
+    plot_data <- adpc_grouped %>% dplyr::filter(id_plot ==  id_val)
+
+    title <- {
+      if (is.null(title)) {
+        paste0(
+          "Plot of PK Concentration-Time Profile ",
+          dplyr::case_when(
+            scale == "LIN" ~ "linear",
+            scale == "LOG" ~ "logarithmic",
+            TRUE ~ "linear and logarithmic"
+          ),
+          " scale, by Cohort: ", unique(plot_data[[studyid]])
+        )
+      } else {
+        parse_annotation(plot_data, title)
+      }
+    }
+
+    subtitle <- {
+      if (is.null(subtitle)) {
+        paste0(
+          #"Treatment Group: ", unique(plot_data[[trt_var]]), " (N=", nrow(plot_data), ")<br>",
+          # TODO(mateusz): Refactor when label attributes are implemented.
+          paste(
+            unlist(unname(plotgroup_names[plotgroup_vars])), ": ",
+            unique(plot_data[, plotgroup_vars]),
+            sep = "",  collapse = ", "
+          ),
+          " (N=", length(unique(plot_data$USUBJID)), ")"
         )
       } else {
         parse_annotation(plot_data, subtitle)
@@ -297,5 +529,4 @@ pkcg02 <-  function(
     plotly_plot
   }) |>
     setNames(unique(adpc[["id_plot"]]))
-
 }
--- tests/testthat/test-g_pkcg.R
+++ tests/testthat/test-g_pkcg.R
@@ -22,6 +22,7 @@ adpc_single <- dplyr::filter(adpc, USUBJID == "11101")
   purrr::walk(p_list, \(plot) expect_identical(class(plot), c("plotly", "htmlwidget")))
 }
 
+#' pkgc01 checks :
 describe("pkcg01", {
   it("generates valid plot list with default settings", {
     p_list <- pkcg01(adpc)
@@ -39,7 +40,6 @@ describe("pkcg01", {
     )
 
     .expect_plotlist(p_list, 4)
-
     p_json <- .get_plotly_json(p_list[[1]])
     expect_equal(p_json$layout$xaxis$title$text, "Test custom xlab")
     expect_equal(p_json$layout$yaxis$title$text, "Test custom ylab")
@@ -58,7 +58,6 @@ describe("pkcg01", {
     )
 
     .expect_plotlist(p_list, 4)
-
     p_json <- .get_plotly_json(p_list[[1]])
     expect_equal(p_json$layout$xaxis$title$text, "Test 11101")
     expect_equal(p_json$layout$yaxis$title$text, "Test Subject ID: 11101")
@@ -103,6 +102,89 @@ describe("g_pkconc_ind_lin", {
 describe("g_pkconc_ind_log", {
   it("generates plot with log scale", {
     p_list <- g_pkconc_ind_log(adpc_single)
+    p_json <- .get_plotly_json(p_list[[1]])
+    expect_equal(p_json$layout$yaxis$type, "log")
+  })
+})
+
+#' pkgc02 checks :
+describe("pkcg02", {
+  it("generates valid plot list with default settings", {
+    p_list <- pkcg02(adpc)
+    .expect_plotlist(p_list, 4)
+  })
+
+  it("generates plot with custom labels", {
+    p_list <- pkcg02(
+      adpc,
+      "xlab" = "Test custom xlab",
+      "ylab" = "Test custom ylab",
+      "title" = "Test custom title",
+      "subtitle" = "Test custom subtitle",
+      "footnote" = "Test custom footnote"
+    )
+
+    .expect_plotlist(p_list, 4)
+    p_json <- .get_plotly_json(p_list[[1]])
+    expect_equal(p_json$layout$xaxis$title$text, "Test custom xlab")
+    expect_equal(p_json$layout$yaxis$title$text, "Test custom ylab")
+    expect_equal(p_json$layout$title$text, "Test custom title<br><sup>Test custom subtitle</sup>")
+    expect_equal(p_json$layout$annotations[1, ]$text, "Test custom footnote")
+  })
+
+  it("generates plot with custom labels that include special syntax", {
+    p_list <- pkcg02(
+      adpc_single,
+      "xlab" = "Test $USUBJID",
+      "ylab" = "Test !USUBJID: $USUBJID",
+      "title" = "Test !DOSEU: $DOSEU",
+      "subtitle" = "Test $DOSEU\n$DOSEU",
+      "footnote" = "Test !USUBJID"
+    )
+
+    .expect_plotlist(p_list, 4)
+    p_json <- .get_plotly_json(p_list[[1]])
+    expect_equal(p_json$layout$xaxis$title$text, "Test 11101")
+    expect_equal(p_json$layout$yaxis$title$text, "Test Subject ID: 11101")
+    expect_equal(p_json$layout$title$text, "Test Dose unit: mg<br><sup>Test mg<br>mg</sup>")
+    expect_equal(p_json$layout$annotations[1, ]$text, "Test Subject ID")
+  })
+
+  it("generates plot list in respect to grouping variables", {
+    p_list <- pkcg02(adpc, plotgroup_vars = c("ROUTE", "PCSPEC", "PARAM"))
+    .expect_plotlist(p_list, 4)
+  })
+
+  it("generates plots in respect to provided limits", {
+    p_list <- pkcg02(
+      adpc,
+      xmin = 10,
+      xmax = 150,
+      ymin = 1,
+      ymax = 3
+    )
+
+    .expect_plotlist(p_list, 4)
+    p_json <- .get_plotly_json(p_list[[1]])
+    #' NOTE:
+    #'     #' Actual range expectations differ from values provided in function call since plotly
+    #' adds some margin.
+    #' expect_equal(p_json$layout$xaxis$range, c(3, 157))
+    expect_equal(p_json$layout$yaxis$range, c(0.9, 3.1))
+  })
+})
+
+describe("g_pkconc_lin", {
+  it("generates plot with linear scale", {
+    p_list <- g_pkconc_lin(adpc)
+    p_json <- .get_plotly_json(p_list[[1]])
+    expect_equal(p_json$layout$yaxis$type, "linear")
+  })
+})
+
+describe("g_pkconc_log", {
+  it("generates plot with log scale", {
+    p_list <- g_pkconc_log(adpc)
     p_json <- .get_plotly_json(p_list[[1]])
     expect_equal(p_json$layout$yaxis$type, "log")
   })
