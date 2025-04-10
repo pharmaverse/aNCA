8599039997232a26b14aca5799a105fc3059ad71
Jana Spinner <spinnerjana@gmail.com>
2025-04-10 17:13:57 +0200

    update testing

--- R/PKNCA.R
+++ R/PKNCA.R
@@ -66,10 +66,12 @@ PKNCA_create_data_object <- function(adnca_data) { # nolint: object_name_linter
   matrix_column <- "PCSPEC"
   std_route_column <- "std_route"
 
-  #Filter out flagged duplicates
-  adnca_data <- adnca_data %>%
-    filter(DFLAG == FALSE) %>%
-    select(-DFLAG)
+  #Filter out flagged duplicates if DFLAG column available
+  if ("DFLAG" %in% colnames(adnca_data)) {
+    adnca_data <- adnca_data %>%
+      filter(DFLAG == FALSE) %>%
+      select(-DFLAG)
+  }
   
   # Create concentration data
   df_conc <- format_pkncaconc_data(
