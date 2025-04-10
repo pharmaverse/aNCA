d04b7209460a28faad219bdae545d1de1671163c
Gero1999 <gerardojoro@gmail.com>
2025-04-10 18:04:21 +0200

    fix: consider unit in calculate_summary_stats & standardize

--- R/calculate_summary_stats.R
+++ R/calculate_summary_stats.R
@@ -6,6 +6,8 @@
 #'                     Non Compartmental Analysis using PKNCA package
 #' @param input_groups A character vector specifying the columns to group by.
 #'                     Here. the hierarchical order matters
+#' @param unit_col     A character string specifying the column name for units.
+#'                     Default is "PPSTRESU".
 #' @returns A data frame with summary statistics for each group and parameter.
 #' @details The function calculates the following statistics for numeric variables:
 #' \itemize{
@@ -40,10 +42,16 @@ calculate_summary_stats <- function(data, input_groups = "DOSNO") {
 
   # Calculate summary statistics, using all value rows
   # (note: this will give more weight to subjects with more valid records)
-
   summary_stats <- data %>%
+    mutate(conv_factor = PPSTRES / PPORRES) %>%
     group_by(across(all_of(c(input_groups, "PPTESTCD")))) %>%
     unique() %>%
+    mutate(
+      ModeUnit = names(sort(table(PPSTRESU), decreasing = TRUE))[1],
+      ModeConv_factor = as.numeric(names(sort(table(conv_factor), decreasing = TRUE))[1]),
+      PPSTRES = PPORRES * ModeConv_factor,
+      PPTESTCD = paste0(PPTESTCD, "[", ModeUnit, "]")
+    ) %>%
     summarise(
       Geomean = exp(mean(log(PPSTRES), na.rm = TRUE)),
       Geocv = (sd(PPSTRES, na.rm = TRUE) / exp(mean(log(PPSTRES), na.rm = TRUE))) * 100,
@@ -53,9 +61,9 @@ calculate_summary_stats <- function(data, input_groups = "DOSNO") {
       Max = ifelse(all(is.na(PPSTRES)), NA, max(PPSTRES, na.rm = TRUE)),
       Median = median(PPSTRES, na.rm = TRUE),
       Count.missing = sum(is.na(PPSTRES)),
-      Count.total = n()
+      Count.total = n(),
+      .groups = "drop"
     ) %>%
-    ungroup() %>%
     mutate(across(where(is.numeric), \(x) round(x, 3))) %>%
     pivot_longer(
       cols = c(Geomean, Geocv, Mean, SD, Min, Max, Median, Count.missing, Count.total),
@@ -66,20 +74,6 @@ calculate_summary_stats <- function(data, input_groups = "DOSNO") {
       names_from = PPTESTCD,
       values_from = Value
     )
-
-  # Include units for all column names
-  pttestcd_with_units <- data %>%
-    select(PPTESTCD, PPSTRESU) %>%
-    mutate(PPSTRESU = ifelse(PPSTRESU != "", paste0("[", PPSTRESU, "]"), "")) %>%
-    unique() %>%
-    pull(PPSTRESU, PPTESTCD)
-
-  summary_stats <- summary_stats %>%
-    rename_with(~ifelse(
-      gsub("_.*", "", .x) %in% names(pttestcd_with_units),
-      paste0(.x, pttestcd_with_units[gsub("_.*", "", .x)]),
-      .x
-    ))
 
   return(summary_stats)
 
