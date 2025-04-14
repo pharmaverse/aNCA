4c82b07b400b01d01c7ac9d1d0534c42ea8f7205
Gero1999 <gerardojoro@gmail.com>
2025-04-14 09:50:34 +0200

    test: vary values/results in sample data & test for labels & AUC ints

--- tests/testthat/test-pivot_wider_pknca_results.R
+++ tests/testthat/test-pivot_wider_pknca_results.R
@@ -19,17 +19,20 @@ conc_data <- data.frame(
   ARRLT = rep(1:5, 4),
   NFRLT = rep(1:10, 2),
   NRRLT = rep(1:5, 4),
-  AVAL = rep(c(10, 8, 6, 4, 2), 4) # Adjusted values for 5 points
+  AVAL = c(c(10, 8, 6, 4, 2),
+           c(10, 8, 6, 4, 2),
+           c(100, 80, 60, 40, 20),
+           c(100, 80, 60, 40, 20)) # Adjusted values for 5 points
 )
 
 dose_data <- data.frame(
   ID = rep(1:2, each = 2),
-  DOSNO = rep(1:2, each = 2),
+  DOSNO = rep(1:2, 2),
   AFRLT = rep(c(0, 5), 2), # Aligned with conc_data AFRLT start and end times
   ARRLT = rep(0, 4),
   NFRLT = rep(c(0, 5), 2), # Aligned with conc_data NFRLT start and end times
   NRRLT = rep(0, 4),
-  DOSE = rep(100, 4)
+  DOSE = c(rep(10, 2), rep(100, 2))
 )
 
 # Perform NCA analysis
@@ -39,7 +42,8 @@ main_intervals <- data.frame(
   half.life = TRUE,
   cmax = TRUE,
   aucint.last = FALSE,
-  type_interval = "main"
+  type_interval = "main",
+  DOSNO = c(1, 2)
 )
 auc_intervals <- data.frame(
   start = c(0, 5),
@@ -47,7 +51,8 @@ auc_intervals <- data.frame(
   half.life = FALSE,
   cmax = FALSE,
   aucint.last = TRUE,
-  type_interval = "manual"
+  type_interval = "manual",
+  DOSNO = c(1, 2)
 )
 myres <- PKNCA::pk.nca(
   PKNCA::PKNCAdata(
@@ -57,7 +62,7 @@ myres <- PKNCA::pk.nca(
       main_intervals,
       auc_intervals
     ),
-    options = list(keep_interval_cols = c("type_interval")),
+    options = list(keep_interval_cols = c("DOSNO", "type_interval")),
     units = PKNCA::pknca_units_table(
       concu = "ng/mL", doseu = "mg/kg", amountu = "mg", timeu = "hr"
     )
@@ -84,7 +89,6 @@ myres$result <- myres$result %>%
 describe("pivot_wider_pknca_results", {
 
   it("reshapes PKNCA results correctly", {
-
     # Apply pivot_wider_pknca_results
     result <- pivot_wider_pknca_results(myres)
 
@@ -92,25 +96,80 @@ describe("pivot_wider_pknca_results", {
     expect_s3_class(result, "data.frame")
 
     # Check that the result contains expected columns
-    expected_columns <- c("ID", "cmax", "AUClast", "AUCinf_obs", "Exclude")
+    expected_columns <- c(
+      "ID", "start", "end", "DOSNO", "AFRLT", "ARRLT", "NFRLT", "NRRLT", "DOSE",
+      "route", "duration", "cmax[ng/mL]", "tmax[hr]", "tlast[hr]", "lambda.z[1/hr]",
+      "r.squared[unitless]", "adj.r.squared[unitless]", "lambda.z.time.first[hr]",
+      "lambda.z.n.points[count]", "clast.pred[ng/mL]", "half.life[hr]",
+      "span.ratio[fraction]", "Exclude"
+    )
     expect_true(all(expected_columns %in% colnames(result)))
 
-    # Check that the result contains expected values
-    expect_true(all(result$cmax > 0))
-    expect_true(all(result$AUClast > 0))
+    # Check the labels for the parameters are in the result
+    expected_labels <- 
+
+    # Check that the result contains expected values for a few key columns
+    options <- result  %>%
+      select(ID, DOSNO) %>%
+      distinct()
+    for (i in 1:nrow(options)) {
+      id <- options$ID[i]
+      dosno <- options$DOSNO[i]
+
+      # Make a loop of the different id and dosno to check if the results are the expected
+      sub_conc_data <- conc_data  %>%
+        filter(ID == id, DOSNO == dosno)
+
+      actual_result <- result %>%
+        filter(ID == id, DOSNO == dosno) %>%
+        formatters::var_labels_remove()
+
+      expect_equal(actual_result[["cmax[ng/mL]"]], max(sub_conc_data$AVAL))
+      expect_equal(actual_result[["tmax[hr]"]], sub_conc_data$ARRLT[which.max(sub_conc_data$AVAL)])
+      expect_equal(actual_result[["lambda.z[1/hr]"]],
+                  PKNCA::pk.calc.half.life(sub_conc_data$AVAL,
+                                            sub_conc_data$AFRLT)$lambda.z,
+                                            tolerance = 1e-3)
+    }
   })
 
-  it("handles empty input gracefully", {
-    # Create empty input
-    empty_myres <- list(
-      data = list(conc = list(data = data.frame())),
-      result = data.frame()
+  it("handles manual AUC intervals correctly", {
+    # Check if manual AUC intervals are included
+    result <- pivot_wider_pknca_results(myres)
+    manual_auc_columns <- grep("^AUCINT_", colnames(result), value = TRUE)
+    expect_true(length(manual_auc_columns) > 0)
+  })
+
+  it("rounds numeric values to three decimals", {
+    result <- pivot_wider_pknca_results(myres)
+    numeric_columns <- sapply(result, is.numeric)
+    rounded_values <- result[, numeric_columns]
+    expect_true(all(apply(rounded_values, 2, function(x) all(abs(x - round(x, 3)) < 1e-6))))
+  })
+
+  it("adds appropriate labels to columns", {
+    result <- pivot_wider_pknca_results(myres)
+    expect_true(!is.null(attr(result$`cmax[ng/mL]`, "label")))
+    expect_equal(attr(result$`cmax[ng/mL]`, "label"), "Maximum concentration [ng/mL]")
+  })
+
+  it("validates variable labels in the result", {
+    result <- pivot_wider_pknca_results(myres)
+    labels <- var_labels(result)
+
+    expected_labels <- c(
+      ID = NA, start = NA, end = NA, DOSNO = NA, AFRLT = NA, ARRLT = NA,
+      NFRLT = NA, NRRLT = NA, DOSE = NA, route = NA, duration = NA,
+      `cmax[ng/mL]` = "cmax", `tmax[hr]` = "tmax", `tlast[hr]` = "tlast",
+      `lambda.z[1/hr]` = "lambda.z", `r.squared[unitless]` = "r.squared",
+      `adj.r.squared[unitless]` = "adj.r.squared",
+      `lambda.z.time.first[hr]` = "lambda.z.time.first",
+      `lambda.z.n.points[count]` = "lambda.z.n.points",
+      `clast.pred[ng/mL]` = "clast.pred", `half.life[hr]` = "half.life",
+      `span.ratio[fraction]` = "span.ratio", Exclude = NA
     )
 
-    # Apply pivot_wider_pknca_results
-    result <- pivot_wider_pknca_results(empty_myres)
-
-    # Check that the result is an empty data frame
-    expect_equal(nrow(result), 0)
+    expect_equal(labels, expected_labels)
   })
+
 })
