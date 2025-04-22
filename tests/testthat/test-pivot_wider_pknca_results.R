d9de931a8713ce84ed739c803246601bf865af9b
Gero1999 <gerardojoro@gmail.com>
2025-04-22 19:09:25 +0200

    fix: tests with new AUCINTs and consider NAs

--- tests/testthat/test-pivot_wider_pknca_results.R
+++ tests/testthat/test-pivot_wider_pknca_results.R
@@ -7,10 +7,10 @@ pwres <- pivot_wider_pknca_results(TEST_PKNCA_RES)
 #' for each parameter based on the original `TEST_PKNCA_RES` object.
 #'
 #' @param TEST_PKNCA_RES The original PKNCA results object.
-#' @param result The reshaped results from `pivot_wider_pknca_results`.
+#' @param pwres The pivoted results from `pivot_wider_pknca_results`.
 #' @return A logical value indicating whether all checks passed.
 #' @export
-.validate_pknca_params <- function(TEST_PKNCA_RES, result) {
+.validate_pknca_params <- function(TEST_PKNCA_RES, pwres) {
 
   # Extract unique parameter names with units
   names_params <- TEST_PKNCA_RES$result %>%
@@ -34,26 +34,25 @@ pwres <- pivot_wider_pknca_results(TEST_PKNCA_RES)
       original_data <- TEST_PKNCA_RES$result %>%
         filter(start == auc_start,
                end == auc_end)
-      reshaped_res <- result %>%
-        filter(start == auc_start,
-               end == auc_end)
+      reshaped_data <- pwres %>%
+        filter(!is.na(!!sym(param_col)))
     } else {
       original_data <- TEST_PKNCA_RES$result
-      reshaped_res <- result
+      reshaped_data <- pwres
     }
 
     original_vals <- original_data %>%
       formatters::var_labels_remove() %>%
       filter(PPTESTCD == pptestcd) %>%
       pull(PPSTRES)
-    result_vals <- reshaped_res %>%
+    result_vals <- reshaped_data %>%
       formatters::var_labels_remove() %>%
       pull(param_col)
 
     have_similar_vals <- all(round(result_vals, 3) %in% round(original_vals, 3))
     have_same_length <- length(result_vals) == length(original_vals)
     if (!(have_similar_vals && have_same_length)) {
-      stop(paste("Mismatch in values for parameter:", pptestcd))
+      stop(paste("Mismatch in values for parameter:", param_col))
     }
   }
 
@@ -65,29 +64,29 @@ pwres <- pivot_wider_pknca_results(TEST_PKNCA_RES)
 describe("pivot_wider_pknca_results", {
 
   it("produces a data.frame with expected format when only reshaping main intervals", {
-    pwres_only_main <- TEST_PKNCA_RES
-    pwres_only_main$result <- TEST_PKNCA_RES$result  %>%
+    res_only_main <- TEST_PKNCA_RES
+    res_only_main$result <- TEST_PKNCA_RES$result  %>%
       filter(type_interval == "main")
-    result_only_main <- pivot_wider_pknca_results(pwres_only_main)
-    expect_s3_class(result, "data.frame")
+    pwres_only_main <- pivot_wider_pknca_results(res_only_main)
+    expect_s3_class(pwres_only_main, "data.frame")
 
     # Check that the result contains expected columns
     expected_columns <- c(
       "USUBJID", "start", "end", "DOSNO", "AFRLT", "ARRLT",
-      "NFRLT", "NRRLT", "ADOSE", "route", "duration",
+      "NFRLT", "NRRLT", "ADOSE", "ROUTE", "ADOSEDUR",
       "CMAX[ng/mL]", "TMAX[hr]", "TLST[hr]", "LAMZ[1/hr]",
       "R2", "R2ADJ", "LAMZLL[hr]",
       "LAMZNPT[count]", "CLSTP[ng/mL]", "LAMZHL[hr]",
       "LAMZSPN", "LAMZIX",
       "LAMZMTD", "Exclude"
     )
-    expect_true(all(expected_columns %in% colnames(result_only_main)))
+    expect_true(all(expected_columns %in% colnames(pwres_only_main)))
 
-    # Check number of rows is the expected
-    expect_true(nrow(result_only_main) == nrow(pwres_only_main$data$dose$data))
+    # Check number of rows is the expected (each dose has 1 row in the pivoted data)
+    expect_true(nrow(pwres_only_main) == nrow(res_only_main$data$dose$data))
 
-    # Check values
-    expect_true(.validate_pknca_params(pwres_only_main, result_only_main))
+    # Check parameter values match the ones in the PKNCA results object
+    expect_true(.validate_pknca_params(res_only_main, pwres_only_main))
   })
 
   it("reshapes PKNCA results correctly when also considering AUC intervals", {
@@ -97,7 +96,7 @@ describe("pivot_wider_pknca_results", {
     # Check that the result contains expected columns
     expected_columns <- c(
       "USUBJID", "start", "end", "DOSNO", "AFRLT", "ARRLT",
-      "NFRLT", "NRRLT", "ADOSE", "route", "duration",
+      "NFRLT", "NRRLT", "ADOSE", "ROUTE", "ADOSEDUR",
       "CMAX[ng/mL]", "TMAX[hr]", "TLST[hr]", "LAMZ[1/hr]",
       "R2", "R2ADJ", "LAMZLL[hr]",
       "LAMZNPT[count]", "CLSTP[ng/mL]", "LAMZHL[hr]",
@@ -114,7 +113,7 @@ describe("pivot_wider_pknca_results", {
       "CMAX[ng/mL]", "TMAX[hr]", "TLST[hr]", "LAMZ[1/hr]",
       "R2", "R2ADJ", "LAMZLL[hr]",
       "LAMZNPT[count]", "CLSTP[ng/mL]", "LAMZHL[hr]",
-      "LAMZSPN", "AUCINT_0-5", "AUCINT_5-10"
+      "LAMZSPN", "AUCINT_0-2", "AUCINT_2-4"
     )
     rounded_values <- pwres[, expected_num_param_cols]
     expect_true(all(apply(rounded_values, 2,
@@ -124,14 +123,14 @@ describe("pivot_wider_pknca_results", {
   it("adds appropriate labels to columns", {
     labels <- formatters::var_labels(pwres)
     expected_labels <- c(
-      ID = NA, start = NA, end = NA, DOSNO = NA, AFRLT = NA, ARRLT = NA,
-      NFRLT = NA, NRRLT = NA, DOSE = NA, route = NA, duration = NA,
+      USUBJID = NA, PARAM = NA, start = NA, end = NA, DOSNO = NA, AFRLT = NA, ARRLT = NA,
+      NFRLT = NA, NRRLT = NA, ROUTE = NA, ADOSE = NA, DRUG = NA, ADOSEDUR = NA,
       `CMAX[ng/mL]` = "Max Conc", `TMAX[hr]` = "Time of CMAX",
       `TLST[hr]` = "Time of Last Nonzero Conc", `LAMZ[1/hr]` = "Lambda z",
       `R2` = "R Squared", `R2ADJ` = "R Squared Adjusted",
       `LAMZLL[hr]` = "Lambda z Lower Limit", `LAMZNPT[count]` = "Number of Points for Lambda z",
       `CLSTP[ng/mL]` = "Clast pred", `LAMZHL[hr]` = "Half-Life Lambda z",
-      `LAMZSPN` = "Lambda z Span", `AUCINT_0-5` = NA, `AUCINT_5-10` = NA,
+      `LAMZSPN` = "Lambda z Span", `AUCINT_0-2` = NA, `AUCINT_2-4` = NA,
       `LAMZIX` = NA, `LAMZMTD` = NA, `Exclude` = NA
     )
     expect_equal(labels, expected_labels)
@@ -142,18 +141,18 @@ describe("pivot_wider_pknca_results", {
     pwres_with_exclude <- TEST_PKNCA_RES
     pwres_with_exclude$result <- pwres_with_exclude$result %>%
       mutate(
-        exclude = ifelse(ID == 1 & DOSNO == 1, "Reason 1; Reason 2", NA_character_)
+        exclude = ifelse(USUBJID == 1 & DOSNO == 1, "Reason 1; Reason 2", NA_character_)
       )
 
     # Apply pivot_wider_pknca_results
     result <- pivot_wider_pknca_results(pwres_with_exclude)
 
     # Check that the Exclude column combines and deduplicates exclude values
-    exclude_values <- result %>% filter(ID == 1 & DOSNO == 1) %>% pull(Exclude)
+    exclude_values <- result %>% filter(USUBJID == 1 & DOSNO == 1) %>% pull(Exclude)
     expect_equal(exclude_values, "Reason 1, Reason 2")
 
     # Check that rows without exclude values have NA in the Exclude column
-    exclude_values_na <- result %>% filter(ID == 2 & DOSNO == 2) %>% pull(Exclude)
+    exclude_values_na <- result %>% filter(USUBJID == 2 & DOSNO == 2) %>% pull(Exclude)
     expect_true(is.na(exclude_values_na))
   })
 })
