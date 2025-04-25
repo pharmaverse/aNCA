9accc18ca59af20ab6799415c28d41aea84e3428
jimrothstein <jimrothstein@gmail.com>
2025-04-25 11:57:24 -0700

    lint

--- tests/testthat/test-label_operators.R
+++ tests/testthat/test-label_operators.R
@@ -1,18 +1,18 @@
 LABELS <- data.frame(
-   Variable = c("USUBJID", "AVAL"),
-   Label = c("Unique Subject Identifier", "Analysis Value"),
-   Dataset = c("ADPC", "ADPC")
+  Variable = c("USUBJID", "AVAL"),
+  Label = c("Unique Subject Identifier", "Analysis Value"),
+  Dataset = c("ADPC", "ADPC")
 )
 
-  #  I added next line (data <- )
-  #data <- data.frame(USUBJID = 1:3, AVAL = c(10, 20, 30))
-  #data <- apply_labels(data, LABELS, c("ADPC", "ADPC"))
-  #print(base::attr(data$A, "label"))
-   
+#  I added next line (data <- )
+#data <- data.frame(USUBJID = 1:3, AVAL = c(10, 20, 30))
+#data <- apply_labels(data, LABELS, c("ADPC", "ADPC"))
+#print(base::attr(data$A, "label"))
+
 #------------------------  apply_labels
 
 describe("apply_labels", {
-  
+
   # Test for successful label application
   it("applies labels to the data frame", {
     data <- data.frame(USUBJID = 1:3, AVAL = c(10, 20, 30))
@@ -21,9 +21,9 @@ describe("apply_labels", {
     expect_equal(base::attr(labeled_data$AVAL, "label"), "Analysis Value")
   })
 
-  # Here, apply to column that is a factor 
+  # Here, apply to column that is a factor
   it("applies labels to the data frame", {
-    data <- data.frame(USUBJID = 1:3, AVAL = c(10, 20, 30), F=factor(letters[1:3]))
+    data <- data.frame(USUBJID = 1:3, AVAL = c(10, 20, 30), F = factor(letters[1:3]))
     labeled_data <- apply_labels(data, LABELS, c("ADPC", "ADPC"))
     expect_equal(base::attr(labeled_data$USUBJID, "label"), "Unique Subject Identifier")
     expect_equal(base::attr(labeled_data$AVAL, "label"), "Analysis Value")
@@ -41,6 +41,8 @@ describe("apply_labels", {
   })
 })
 
+remotes::install_github("r-lib/lintr")
+
 # ------------------------  as_factor_preserve_label
 vec <- c("A", "B", "C")
 attr(vec, "label") <- "Example Label"
@@ -49,22 +51,22 @@ describe("as_factor_preserve_label", {
 
   it("returns object of class factor", {
     expect_equal("factor", class(as_factor_preserve_label(vec)))
-     })
-    
+  })
+
   it("returns ... ", {
     old_label = base::attr(vec, "label")
     new_label = base::attr(as_factor_preserve_label(vec), "label")
     expect_equal(old_label, new_label)
 
-    })
   })
+})
 
 # ------------------------  has_label
 vec <- c("A", "B", "C")
 attr(vec, "label") <- "Example Label"
 
 describe("has_label", {
-  it("returns TRUE if has label; FALSE otherwise",{
+  it("returns TRUE if has label; FALSE otherwise", {
   expect_true(has_label(vec))
   })
 })
@@ -95,4 +97,3 @@ describe("get_label", {
     expect_equal(get_label(LABELS, "USUBJID", "ADP"), "No label available")
   })
 })
-
