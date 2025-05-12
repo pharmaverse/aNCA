45c699c35247f72a78a15b38b901a79e00739e52
m-kolomanski <m.kolomanski@yahoo.com>
2025-05-12 10:40:13 +0200

    refactor: refactored `read_pk` to be more concise and extension-friendly

--- R/read_pk.R
+++ R/read_pk.R
@@ -2,8 +2,7 @@
 #'
 #' @details
 #' Currently supported file formats include:
-#' - `csv`
-#' - `rds`
+#' \itemize{`r paste0("\\item \\code{", names(readers), "}", collapse = "\n")`}
 #'
 #' @param path Character string with path to the dataset file.
 #' @returns A data.frame object with loaded data.
@@ -17,44 +16,11 @@
 read_pk <- function(path) {
   if (!file.exists(path)) stop("File does not exist: ", path)
 
-  switch(
-    file_ext(path),
-    csv = read.csv(path, na = c("", "NA")),
-    rds = readRDS(path),
-    xlsx = {
-      if (!requireNamespace("openxlsx2", silently = TRUE))
-        stop(
-          "Handling .xlsx files requires `openxlsx2` package, please install it with ",
-          "`install.packages('openxlsx2')`"
-        )
-      openxlsx2::read_xlsx(path)
-    },
-    sas7bdat = {
-      if (!requireNamespace("haven", silently = TRUE))
-        stop(
-          "Handling .sas7bdat files requires `haven` package, please install it with ",
-          "`install.packages('haven')`"
-        )
-      haven::read_sas(path)
-    },
-    xpt = {
-      if (!requireNamespace("haven", silently = TRUE))
-        stop(
-          "Handling .xpt files requires `haven` package, please install it with ",
-          "`install.packages('haven')`"
-        )
-      haven::read_xpt(path)
-    },
-    parquet = {
-      if (!requireNamespace("arrow", silently = TRUE))
-        stop(
-          "Handling .parquet files requires `arrow` package, please install it with ",
-          "`install.packages('arrow')`"
-        )
-      arrow::read_parquet(path)
-    },
-    stop("Invalid file type. Accepted formats are .csv, .rds, .xlsx.")
-  ) |>
+  format <- tools::file_ext(path)
+  if (!format %in% names(readers))
+    stop("Invalid file type. Accepted formats are ", paste(names(readers), collapse = ", "))
+
+  readers[[format]](path) |>
     validate_pk()
 }
 
@@ -79,3 +45,43 @@ validate_pk <- function(pk_data) {
 
   pk_data
 }
+
+#' List of readers responsible for loading files with specific file extensions.
+#' @noRd
+#' @keywords internal
+readers <- list(
+  csv = \(path) read.csv(path, na = c("", "NA")),
+  rds = \(path) readRDS(path),
+  xlsx = \(path) {
+    if (!requireNamespace("openxlsx2", silently = TRUE))
+      stop(
+        "Handling .xlsx files requires `openxlsx2` package, please install it with ",
+        "`install.packages('openxlsx2')`"
+      )
+    openxlsx2::read_xlsx(path)
+  },
+  sas7bdat = \(path) {
+    if (!requireNamespace("haven", silently = TRUE))
+      stop(
+        "Handling .sas7bdat files requires `haven` package, please install it with ",
+        "`install.packages('haven')`"
+      )
+    haven::read_sas(path)
+  },
+  xpt = \(path) {
+    if (!requireNamespace("haven", silently = TRUE))
+      stop(
+        "Handling .xpt files requires `haven` package, please install it with ",
+        "`install.packages('haven')`"
+      )
+    haven::read_xpt(path)
+  },
+  parquet = \(path) {
+    if (!requireNamespace("arrow", silently = TRUE))
+      stop(
+        "Handling .parquet files requires `arrow` package, please install it with ",
+        "`install.packages('arrow')`"
+      )
+    arrow::read_parquet(path)
+  }
+)
