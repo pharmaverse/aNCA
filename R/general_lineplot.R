774a48a8d7b83275b63e8e6bd8e3d3c132d28894
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-05-14 11:38:17 +0000

    spelling

--- R/general_lineplot.R
+++ R/general_lineplot.R
@@ -17,7 +17,7 @@
 #'                          Options are "Log" or other values.
 #' @param cycle             A character string or numeric value specifying the cycle to filter by
 #'                          when `time_scale` is "By Cycle". Default is NULL.
-#' @param show_threshold    A boolean specifying wether to show a threshold line or not.
+#' @param show_threshold    A boolean specifying whether to show a threshold line or not.
 #'                          Default is FALSE.
 #' @param threshold_value   A numeric value to set the y value of the threshold line.
 #'                          Default is 0.
