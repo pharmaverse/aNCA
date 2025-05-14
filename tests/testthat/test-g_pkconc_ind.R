d19174dd24d7851972717678e9753369813ba55f
Gero1999 <gerardojoro@gmail.com>
2025-05-14 13:02:14 +0200

    test: refactor and rearrange by type all tests, using more vdiffr

--- tests/testthat/test-g_pkconc_ind.R
+++ tests/testthat/test-g_pkconc_ind.R
@@ -1,192 +1,124 @@
-adpc <- read.csv(system.file("shiny/data/Dummy_complex_data.csv", package = "aNCA")) %>%
-  filter(
-    USUBJID %in% c("XX01-11101", "XX01-11102", "XX01-11103"),
-    PCSPEC == "SERUM", PARAM == "Analyte01"
-  )
+adpc <- FIXTURE_CONC_DATA %>%
+  filter(USUBJID %in% unique(USUBJID)[1:3])
 attr(adpc$USUBJID, "label") <- "Subject ID"
+attr(adpc$AVAL, "label") <- "Analysis value"
 attr(adpc$DOSEU, "label") <- "Dose unit"
-adpc_single <- dplyr::filter(adpc, USUBJID == "XX01-11101")
-
-#' Converts plotly object to JSON
-#' @param p plotly object
-#' @returns a list with plotly object data
-.get_plotly_json <- function(p) {
-  jsonlite::fromJSON(plotly::plotly_json(p, FALSE))
-}
-
-#' Checks basic expectations for plot list:
-#' - is a list
-#' - length is as expected
-#' - each element is a plotly object
-#' @param p_list plot list
-#' @param list_length expected length of the list
-.expect_plotlist <- function(p_list, list_length) {
-  expect_type(p_list, "list")
-  expect_equal(length(p_list), list_length)
-  purrr::walk(p_list, \(plot) expect_identical(class(plot), c("plotly", "htmlwidget")))
-}
+adpc_single <- dplyr::filter(adpc, USUBJID == USUBJID[1])
 
 describe("pkcg01", {
-  it("generates valid plot list with default settings", {
-    simple_plots <- pkcg01(adpc, plotly = FALSE)
-    expect_equal(length(simple_plots), 3)
+  it("generates valid ggplots with LIN scale", {
+    plots_lin <- pkcg01(adpc, scale = "LIN", plotly = FALSE)
+    expect_equal(length(plots_lin), 3)
     
-    simple_plot1 <- simple_plots[[1]]
-    simple_plot2 <- simple_plots[[2]]
-    simple_plot3 <- simple_plots[[3]]
-
-    vdiffr::expect_doppelganger("simple_plot1", simple_plot1)
-    vdiffr::expect_doppelganger("simple_plot2", simple_plot2)
-    vdiffr::expect_doppelganger("simple_plot3", simple_plot3)
+    vdiffr::expect_doppelganger("lin_plot1", plots_lin[[1]])
+    vdiffr::expect_doppelganger("lin_plot2", plots_lin[[2]])
+    vdiffr::expect_doppelganger("lin_plot3", plots_lin[[3]])
+  })
+  
+  it("generates plotly plots with LIN scale", {
+    plotlys_lin <- pkcg01(adpc, scale = "LIN", plotly = TRUE)
+    expect_equal(length(plotlys_lin), 3)
+    expect_true(inherits(plotlys_lin[[1]], "plotly"))
+  })
+  
+  it("generates valid ggplots with LOG scale", {
+    plots_log <- pkcg01(adpc, scale = "LOG", plotly = FALSE)
+    expect_equal(length(plots_log), 3)
+    
+    vdiffr::expect_doppelganger("log_plot1", plots_log[[1]])
+    vdiffr::expect_doppelganger("log_plot2", plots_log[[2]])
+    vdiffr::expect_doppelganger("log_plot3", plots_log[[3]])
   })
 
-  it("generates plot with custom labels", {
-    p_list <- pkcg01(
-      adpc_single,
-      "xlab" = "Test custom xlab",
-      "ylab" = "Test custom ylab",
-      "title" = "Test custom title",
-      "subtitle" = "Test custom subtitle",
-      "footnote" = "Test custom footnote"
+  it("generates plotly plots with LOG scale", {
+    plotlys_log <- pkcg01(adpc, scale = "LOG", plotly = TRUE)
+    expect_equal(length(plotlys_log), 3)
+    expect_true(inherits(plotlys_log[[1]], "plotly"))
+  })
+  
+  it("generates valid ggplots with SBS scale", {
+    plots_sbs <- pkcg01(adpc, scale = "SBS", plotly = FALSE)
+    expect_equal(length(plots_sbs), 3)
+    
+    vdiffr::expect_doppelganger("sbs_plot1", plots_sbs[[1]])
+    vdiffr::expect_doppelganger("sbs_plot2", plots_sbs[[2]])
+    vdiffr::expect_doppelganger("sbs_plot3", plots_sbs[[3]])
+  })
+
+  it("generates plotly plots with SBS scale", {
+    plotlys_sbs <- pkcg01(adpc, scale = "SBS", plotly = TRUE)
+    expect_equal(length(plotlys_sbs), 3)
+    expect_true(inherits(plotlys_sbs[[1]], "plotly"))
+  })
+  
+  it("generates plots with custom labels for LIN scale", {
+    plots_lin <- pkcg01(
+      adpc,
+      scale = "LIN",
+      xlab = "Custom X Label",
+      ylab = "Custom Y Label",
+      title = "Custom Title",
+      subtitle = "Custom Subtitle",
+      footnote = "Custom Footnote",
+      plotly = FALSE
     )
-
-    .expect_plotlist(p_list, 4)
-
-    p_json <- .get_plotly_json(p_list[[1]])
-    expect_equal(p_json$layout$xaxis$title$text, "Test custom xlab")
-    expect_equal(p_json$layout$yaxis$title$text, "Test custom ylab")
-    expect_equal(p_json$layout$title$text, "Test custom title<br><sup>Test custom subtitle</sup>")
-    expect_equal(p_json$layout$annotations[1, ]$text, "Test custom footnote")
+    plot <- plots_lin[[1]]
+    
+    expect_equal(plot$labels$x, "Custom X Label")
+    expect_equal(plot$labels$y, "Custom Y Label")
+    expect_equal(plot$labels$title, "Custom Title")
+    expect_equal(plot$labels$subtitle, "Custom Subtitle")
+    expect_equal(plot$labels$caption, "Custom Footnote")
   })
 
-  it("generates plot with custom labels that include special syntax", {
-    p_list <- pkcg01(
-      adpc_single,
-      "xlab" = "Test $USUBJID",
-      "ylab" = "Test !USUBJID: $USUBJID",
-      "title" = "Test !DOSEU: $DOSEU",
-      "subtitle" = "Test $DOSEU\n$DOSEU",
-      "footnote" = "Test !USUBJID"
-    )
-
-    .expect_plotlist(p_list, 4)
-
-    p_json <- .get_plotly_json(p_list[[1]])
-    expect_equal(p_json$layout$xaxis$title$text, "Test XX01-11101")
-    expect_equal(p_json$layout$yaxis$title$text, "Test Subject ID: XX01-11101")
-    expect_equal(p_json$layout$title$text, "Test Dose unit: mg<br><sup>Test mg<br>mg</sup>")
-    expect_equal(p_json$layout$annotations[1, ]$text, "Test Subject ID")
-  })
-
-  it("generates plot list in respect to grouping variables", {
-    p_list <- pkcg01(adpc, plotgroup_vars = c("ROUTE", "PCSPEC", "PARAM"))
-    .expect_plotlist(p_list, 4)
-  })
-
-  it("generates plots in respect to provided limits", {
-    p_list <- pkcg01(
-      adpc_single,
-      xmin = 10,
-      xmax = 150,
-      ymin = 1,
-      ymax = 3
-    )
-
-    .expect_plotlist(p_list, 4)
-
-    p_json <- .get_plotly_json(p_list[[1]])
-
-    #' NOTE:
-    #' Actual range expectations differ from values provided in function call since plotly
-    #' adds some margin.
-    expect_equal(p_json$layout$xaxis$range, c(3, 157))
-    expect_equal(p_json$layout$yaxis$range, c(0.9, 3.1))
-  })
-
-  it("generates plots with color_var and color_var_label", {
-    adpc <- adpc_single
-
-    p_list <- pkcg01(
-      adpc = adpc,
+  it("generates plots with custom multiple colors for LIN scale", {
+    plots_lin_colors <- pkcg01(
+      adpc,
+      scale = "LIN",
+      xlab = "Custom X Label",
+      ylab = "Custom Y Label",
+      title = "Custom Title",
+      subtitle = "Custom Subtitle",
+      footnote = "Custom Footnote",
+      plotly = FALSE,
       color_var = "DOSNO",
-      color_var_label = "Dose Number",
-      xvar = "AFRLT",
-      yvar = "AVAL",
-      xlab = "Time (hours)",
-      ylab = "Concentration (ng/mL)",
-      title = "PK Concentration-Time Profile",
-      subtitle = "By Treatment Group",
-      footnote = "Generated for testing purposes"
+      color = c("red", "blue", "green")
     )
-
-    expect_type(p_list, "list")
-    expect_s3_class(p_list[[1]], "plotly")
-
-    p_json <- .get_plotly_json(p_list[[1]])
-
-    # Check that the color legend is present and labeled correctly
-    expect_equal(p_json$data$legendgroup, as.character(1:5))
-    expect_true(all(p_json$data$showlegend))
-    expect_equal(p_json$layout$legend$title$text, "Dose Number")
+    plot_lin_colors <- plots_lin[[2]]
+    vdiffr::expect_doppelganger("lin_plot2_custom_colors", plot_lin_colors)
   })
 
-  it("generates plots with side-by-side (SBS) scale", {
-    suppressWarnings(
-      p_list <- pkcg01(
-        adpc_single,
-        scale = "SBS",
-        title = "Test SBS title",
-        subtitle = "Test SBS subtitle",
-        footnote = "Test SBS footnote"
-      )
-    )
-
-    .expect_plotlist(p_list, 4)
-
-    p_json <- .get_plotly_json(p_list[[1]])
-
-    expect_equal(p_json$layout$title$text, "Test SBS title<br><sup>Test SBS subtitle</sup>")
-    #expect_equal(p_json$layout$annotations[1, ]$text, "Test SBS xlab")
-    # Check for multiple x and y axes (facet_wrap)
-    expect_true(!is.null(p_json$layout$xaxis2))
-    expect_true(!is.null(p_json$layout$yaxis2))
-  })
-
-  it("throws an error for SBS plots when ggh4x is not available", {
+  it("returns error if missing ggh4x package for SBS scale", {
     # Temporarily mock requireNamespace to simulate ggh4x not being available
-    mock_requireNamespace <- function(...) FALSE
-    
-    original_requireNamespace <- requireNamespace
-    requireNamespace <- function(pkg, ...) {
-      if (pkg == "ggh4x") return(FALSE)
-      original_requireNamespace(pkg, ...)
-    }
-    on.exit(requireNamespace <- original_requireNamespace) # Restore after test
-    expect_error(
-      pkcg01(
-        adpc_single,
-        scale = "SBS",
-        title = "Test SBS title",
-        subtitle = "Test SBS subtitle",
-        footnote = "Test SBS footnote"
-      ),
-      regexp = "Side-by-side view requires `ggh4x` package, please install it with "
+    with_mocked_bindings(
+      {
+        expect_error(
+          pkcg01(adpc, scale = "SBS", plotly = FALSE),
+          "Side-by-side view requires `ggh4x` package, please install it with"
+        )
+      },
+      `requireNamespace` = function(pkg, quietly = TRUE) {
+        if (pkg == "ggh4x") return(FALSE)
+        base::requireNamespace(pkg, quietly = quietly)
+      }, .package = "aNCA"
     )
   })
 })
 
 describe("g_pkconc_ind_lin", {
   it("generates plot with linear scale", {
-    p_list <- g_pkconc_ind_lin(adpc_single)
-    p_json <- .get_plotly_json(p_list[[1]])
-    expect_equal(p_json$layout$yaxis$type, "linear")
+    plots_lin <- g_pkconc_ind_lin(adpc, plotly = FALSE)
+    plot <- plots_lin[[1]]
+    expect_equal(plot$labels$y, "Analysis value [mg/L]")
+    vdiffr::expect_doppelganger("g_pkconc_ind_lin_plot", plot)
   })
 })
 
 describe("g_pkconc_ind_log", {
   it("generates plot with log scale", {
-    p_list <- g_pkconc_ind_log(adpc_single)
-    p_json <- .get_plotly_json(p_list[[1]])
-    expect_equal(p_json$layout$yaxis$type, "log")
+    plots_log <- g_pkconc_ind_log(adpc, plotly = FALSE)
+    plot <- plots_log[[1]]
+    expect_equal(plot$labels$y, "Log 10 - Analysis value [mg/L]")
+    vdiffr::expect_doppelganger("g_pkconc_ind_log_plot", plot)
   })
 })
