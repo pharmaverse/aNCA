8f645248b22b63912763e95dfe0a0ac80f657d4d
Kobana <kobanak@exbp.roche.com>
2025-05-28 08:10:30 +0200

    fix : setup.R  correction"

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -339,12 +339,11 @@ DUMMY_DATA_FIXTURE <- read.csv(testthat::test_path("data", "adnca_dummy_sm_datas
 PKNCA_DATA_FIXTURE <- PKNCA_create_data_object(DUMMY_DATA_FIXTURE %>% filter(PCSPEC == "Plasma"))
 # Set intervals
 PKNCA_DATA_FIXTURE$intervals <- format_pkncadata_intervals(
-  PKNCA_DATA_FIXTURE$conc, PKNCA_DATAs_FIXTURE$dose,
+  PKNCA_DATA_FIXTURE$conc, PKNCA_DATA_FIXTURE$dose,
   params = c("aucinf.obs", "aucint.last", "auclast",
              "cmax", "half.life", "tmax",
              "lambda.z", "lambda.z.n.points",
              "r.squared", "adj.r.squared", "lambda.z.time.first")
-
 )
 PKNCA_DATA_FIXTURE <- create_start_impute(PKNCA_DATA_FIXTURE)
 # Create NCA results
