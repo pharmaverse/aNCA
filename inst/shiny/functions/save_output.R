b76efa13712185a796c9f18be03c64d79bd11f8a
Gero1999 <gerardojoro@gmail.com>
2025-05-28 18:57:57 +0200

    add: create shiny fun to save outputs

---
+++ inst/shiny/functions/save_output.R
@@ -0,0 +1,21 @@
+#' Save aNCA Result to the Output Directory
+#'
+#' @param output Output object, can be a data frame, plot or a list of them.
+#' @param output_path Path to the output directory (should exist or be creatable).
+#' @return Invisibly returns the file path written.
+save_output <- function(output, output_path) {
+  # Create output directory if it doesn't exist
+  output_path <- paste0("output/", output_path)
+  dir.create(dirname(output_path), showWarnings = FALSE, recursive = TRUE)
+  browser()
+  if (class(output) == "data.frame") {
+    write.csv(output, file = output_path, row.names = FALSE)
+  } else if (inherits(output, "ggplot")) {
+    ggsave(output_path, plot = output, width = 10, height = 6)
+  } else if (inherits(output, "list") && inherits(output[[1]], "ggplot")) {
+    for (name in names(output)) {
+      file_name <- paste0(output_path, "_", name)
+      ggsave(file_name, plot = output[[name]], width = 10, height = 6)
+    }
+  }
+}
