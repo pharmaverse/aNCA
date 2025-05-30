756f316ec02ecd9b59b09b0f2b2127786a52a639
m-kolomanski <m.kolomanski@yahoo.com>
2025-05-30 09:06:21 +0200

    testing CI

--- R/exclude_nca_extra.R
+++ R/exclude_nca_extra.R
@@ -36,56 +36,33 @@
 #'
 #' @export
 
-exclude_nca_by_param <- function(parameter, min_thr = NULL, max_thr = NULL, affected_parameters) {
 
+exclude_nca_by_param <- function(parameter, min_thr = NULL, max_thr = NULL) {
   # Determine if thresholds are defined and if so check they are single numeric objects
-  thr_def <- validate_thresholds(min_thr, max_thr)
-  if (missing(affected_parameters)) affected_parameters <- parameter
-
+  is_min_thr <- is_single_numeric(min_thr, "min_thr")
+  is_max_thr <- is_single_numeric(max_thr, "max_thr")
+  if (is_min_thr && is_max_thr && min_thr > max_thr)
+    stop("if both defined min_thr must be less than max_thr")
   function(x, ...) {
     ret <- rep(NA_character_, nrow(x))
-    idx_param <- which(x$PPTESTCD %in% parameter)
-    idx_to_flag <- which(x$PPTESTCD %in% affected_parameters)
-
-    if (length(idx_param) == 0 || length(idx_to_flag) == 0) {
-      # Do nothing, it wasn't calculated
-    } else if (length(idx_param) > 1) {
+    idx_param <- which(x$PPTESTCD == parameter)
+    if (length(idx_param) == 0 || is.na(x$PPORRES[idx_param])) return(ret)
+    if (length(idx_param) > 1)
       stop("Should not see more than one ", parameter, " (please report this as a bug)")
-    } else if (!is.na(x$PPORRES[idx_param])) {
-      current_value <- x$PPORRES[idx_param]
-      pretty_name <- translate_terms(parameter, "PKNCA", "PPTEST")
-
-      if (thr_def$is_min_thr && current_value < min_thr) {
-        ret[idx_to_flag] <- sprintf("%s < %g", pretty_name, min_thr)
-      }
-      if (thr_def$is_max_thr && current_value > max_thr) {
-        ret[idx_to_flag] <- sprintf("%s > %g", pretty_name, max_thr)
-      }
+    current_value <- x$PPORRES[idx_param]
+    pretty_name <- translate_terms(parameter, "PKNCA", "PPTEST")
+    if (is_min_thr && current_value < min_thr) {
+      ret[idx_param] <- sprintf("%s < %g", pretty_name, min_thr)
+    } else if (is_max_thr && current_value > max_thr) {
+      ret[idx_param] <- sprintf("%s > %g", pretty_name, max_thr)
     }
     ret
   }
 }
-
 # Helper function to validate if a value is a single numeric and check if it is defined
-validate_is_single_numeric <- function(value, name) {
-  is_val <- any(!is.null(value) & !is.na(value) & !missing(value))
-  if (is_val && (length(value) != 1 || !is.numeric(value))) {
+is_single_numeric <- function(value, name) {
+  if (any(!is.null(value) & !is.na(value) & !missing(value))) return(FALSE)
+  if (length(value) != 1 || !is.numeric(value))
     stop(sprintf("when defined %s must be a single numeric value", name))
-  }
-  is_val
+  TRUE
 }
-
-# Updated validate_thresholds function to use validate_is_single_numeric
-validate_thresholds <- function(min_thr, max_thr) {
-  is_min_thr <- validate_is_single_numeric(min_thr, "min_thr")
-  is_max_thr <- validate_is_single_numeric(max_thr, "max_thr")
-
-  if (is_min_thr && is_max_thr && min_thr > max_thr) {
-    stop("if both defined min_thr must be less than max_thr")
-  }
-  list(
-    is_min_thr = is_min_thr,
-    is_max_thr = is_max_thr
-  )
-}
-
