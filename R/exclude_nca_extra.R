aaf61e3079a59620fba7fd7b4edf4c52ddfc6ab2
Gero1999 <gerardojoro@gmail.com>
2025-05-30 09:42:38 +0200

    refactor: simplify condition logic & default arg
    Co-authored-by: Mateusz Kołomański <63905560+m-kolomanski@users.noreply.github.com>

--- R/exclude_nca_extra.R
+++ R/exclude_nca_extra.R
@@ -66,26 +66,45 @@ exclude_nca_by_param <- function(parameter, min_thr = NULL, max_thr = NULL, affe
   }
 }
 
+
+exclude_nca_by_param <- function( # nolint
+  parameter,
+  min_thr = NULL,
+  max_thr = NULL,
+  affected_parameters = parameter
+) {
+  # Determine if thresholds are defined and if so check they are single numeric objects
+  is_min_thr <- is_single_numeric(min_thr, "min_thr")
+  is_max_thr <- is_single_numeric(max_thr, "max_thr")
+  if (is_min_thr && is_max_thr && min_thr > max_thr)
+    stop("if both defined min_thr must be less than max_thr")
+  function(x, ...) {
+    ret <- rep(NA_character_, nrow(x))
+    idx_param <- which(x$PPTESTCD == parameter)
+    idx_aff_params <- which(x$PPTESTCD %in% affected_parameters)
+
+    if (length(idx_param) > 1)
+      stop("Should not see more than one ", parameter, " (please report this as a bug)")
+
+    if (length(idx_param) == 1 && !is.na(x$PPORRES[idx_param]) && length(idx_aff_params) > 0) {
+      current_value <- x$PPORRES[idx_param]
+      pretty_name <- translate_terms(parameter, "PKNCA", "PPTEST")
+      if (is_min_thr && current_value < min_thr) {
+        ret[idx_aff_params] <- sprintf("%s < %g", pretty_name, min_thr)
+      } else if (is_max_thr && current_value > max_thr) {
+        ret[idx_aff_params] <- sprintf("%s > %g", pretty_name, max_thr)
+      }
+    }
+    ret
+  }
+}
+
 # Helper function to validate if a value is a single numeric and check if it is defined
-validate_is_single_numeric <- function(value, name) {
+is_single_numeric <- function(value, name) {
   is_val <- any(!is.null(value) & !is.na(value) & !missing(value))
   if (is_val && (length(value) != 1 || !is.numeric(value))) {
     stop(sprintf("when defined %s must be a single numeric value", name))
   }
   is_val
-}
-
-# Updated validate_thresholds function to use validate_is_single_numeric
-validate_thresholds <- function(min_thr, max_thr) {
-  is_min_thr <- validate_is_single_numeric(min_thr, "min_thr")
-  is_max_thr <- validate_is_single_numeric(max_thr, "max_thr")
-
-  if (is_min_thr && is_max_thr && min_thr > max_thr) {
-    stop("if both defined min_thr must be less than max_thr")
-  }
-  list(
-    is_min_thr = is_min_thr,
-    is_max_thr = is_max_thr
-  )
 }
 
