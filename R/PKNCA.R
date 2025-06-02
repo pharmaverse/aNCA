436bccbc7dc714a4fb48546430d2a8030e0db5dc
Gero1999 <gerardojoro@gmail.com>
2025-06-02 14:18:58 +0200

    docs: added for ensure_column_unit_exists

--- R/PKNCA.R
+++ R/PKNCA.R
@@ -543,7 +543,20 @@ PKNCA_build_units_table <- function(o_conc, o_dose) { # nolint
            PPTESTCD, PPORRESU, PPSTRESU, conversion_factor)
 }
 
-# Ensures that specified unit columns exist in the PKNCA object, creating them if necessary.
+#' Ensure Unit Columns Exist in PKNCA Object
+#'
+#' Checks if specified unit columns exist in a PKNCA object (either PKNCAconc or PKNCAdose).
+#' If the columns do not exist, it creates them and assigns default values (NA or existing units).
+#'
+#' @param pknca_obj A PKNCA object (either PKNCAconc or PKNCAdose).
+#' @param unit_name A character vector of unit column names to ensure (concu, amountu, timeu...).
+#' @returns The updated PKNCA object with ensured unit columns.
+#'
+#' @details
+#' The function performs the following steps:
+#' 1. Checks if the specified unit columns exist in the PKNCA object.
+#' 2. If a column does not exist, it creates the column and assigns default values.
+#' 3. If not default values are provided, it assigns NA to the new column.
 ensure_column_unit_exists <- function(pknca_obj, unit_name) {
   for (unit in unit_name) {
     if (is.null(pknca_obj$columns[[unit]])) {
