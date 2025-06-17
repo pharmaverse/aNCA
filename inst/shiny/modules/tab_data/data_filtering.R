6bbbac538aaa357158d17dee9d22813cf461ac0f
Rabii Bouhestine <rabiibouhestine@outlook.com>
2025-06-17 09:11:36 +0000

    redesign data filtering

--- inst/shiny/modules/tab_data/data_filtering.R
+++ inst/shiny/modules/tab_data/data_filtering.R
@@ -15,100 +15,68 @@ data_filtering_ui <- function(id) {
 
   div(
     stepper_ui("Filtering"),
-    card(
+    div(
+      h3("Filtering"),
+      p("Any filters added here will be applied across the whole analysis."),
       div(
-        h3("Filters"),
-        p("
-          Click the 'Add Filters' button to add filters to your data.
-          Be sure to click 'Submit' in order to apply the changes.\n
-          Any filters added here will be applied across the whole analysis.
-        "),
+        class = "data-filtering-container",
         div(
-          class = "filters-buttons-container",
-          actionButton(ns("add_filter"), "Add Filter"),
-          input_task_button(ns("submit_filters"), "Submit Filters")
+          class = "filtered-table-container",
+          withSpinner(
+            reactableOutput(ns("filtered_data_display"))
+          )
         ),
-        div(id = ns("filters")), # container for filter widgets inserted in the server part
-        reactableOutput(ns("filtered_data_display"))
+        div(
+          class = "filters-container",
+          uiOutput(ns("filters"))
+        )
       )
-
     )
   )
 }
 
 data_filtering_server <- function(id, raw_adnca_data) {
   moduleServer(id, function(input, output, session) {
-    # Handle user-provided filters
-    filters <- reactiveValues()
+    ns <- session$ns
 
-    observeEvent(input$add_filter, {
-      # Create a unique ID for each filter
-      filter_id <- paste0("filter_", input$add_filter)
-
-      # Insert a new filter UI
-      insertUI(
-        selector = paste0("#", session$ns("filters")),
-        ui = input_filter_ui(session$ns(filter_id), colnames(raw_adnca_data()))
-      )
-
-      filters[[filter_id]] <- input_filter_server(filter_id)
+    # Render filters UI
+    output$filters <- renderUI({
+      req(raw_adnca_data())
+      df <- raw_adnca_data()
+      lapply(names(df), function(var) generate_filter_ui(var, df[[var]], ns))
     })
 
-    #' When filters change, show notification reminding the user about submitting
-    filter_reminder_notification <- reactiveVal(NULL)
-    observe({
-      applied_filters <- lapply(reactiveValuesToList(filters), \(x) x()) |>
-        purrr::keep(\(x) !is.null(x))
-      if (length(applied_filters) == 0) {
-        removeNotification(filter_reminder_notification())
-      } else {
-        showNotification(
-          "Remember to submit filters before continuing.",
-          id = session$ns("filter_submit_reminder"),
-          duration = 0,
-          closeButton = FALSE,
-          type = "message"
-        ) |> filter_reminder_notification()
-      }
-    })
-
+    # Reactive: filtered data
     filtered_data <- reactive({
-      # Extract filters from reactive values
-      applied_filters <- lapply(reactiveValuesToList(filters), \(x) x())
-
-      if (length(applied_filters) != 0) {
-        applied_filters |>
-          purrr::keep(\(x) !is.null(x)) |>
-          sapply(\(filt) str_glue("* {filt$column} {filt$condition} {filt$value}")) |>
-          paste0(collapse = "\n") %>%
-          paste0("Submitting the following filters:\n", .) %>%
-          log_info()
+      req(raw_adnca_data())
+      df <- raw_adnca_data()
+      for (var in names(df)) {
+        df <- apply_filter(df, var, input)
       }
+      df
+    })
 
-      removeNotification(filter_reminder_notification())
-
-      # Filter and return data
-      apply_filters(raw_adnca_data(), applied_filters)
-    }) |>
-      bindEvent(input$submit_filters, raw_adnca_data())
-
+    # Render filtered table
     output$filtered_data_display <- renderReactable({
       req(filtered_data())
+      wait_for_all_filter_inputs(raw_adnca_data(), input)
+
       reactable(
         filtered_data(),
+        rownames = FALSE,
         searchable = TRUE,
         sortable = TRUE,
         highlight = TRUE,
-        wrap = TRUE,
+        wrap = FALSE,
         resizable = TRUE,
         defaultPageSize = 25,
         showPageSizeOptions = TRUE,
-        striped = TRUE,
-        bordered = TRUE,
-        height = "98vh"
+        height = "70vh",
+        class = "reactable-table"
       )
     })
 
+    # Output
     filtered_data
   })
 }
