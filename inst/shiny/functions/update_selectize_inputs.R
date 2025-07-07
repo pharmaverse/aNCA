7b54784695a5c6670c710b902797feb76bfd34f3
Jana Spinner <spinnerjana@gmail.com>
2025-07-07 11:36:25 +0200

    lint

--- inst/shiny/functions/update_selectize_inputs.R
+++ inst/shiny/functions/update_selectize_inputs.R
@@ -68,12 +68,12 @@ update_selectize_inputs <- function(session, input_ids, column_names, manual_uni
       if ("DOSEAU" %in% column_names) {
         selected <- "DOSEAU"
       } else {
-        selected =
+        selected <-
           if (sub("select_", "", input_id) %in% column_names)
             sub("select_", "", input_id) else NULL
       }
     } else {
-      selected =
+      selected <-
         if (sub("select_", "", input_id) %in% column_names)
           sub("select_", "", input_id) else NULL
     }
