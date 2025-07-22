f96e3c2ce9d24df3d00d8be22d4c787557a7aa98
Kobana <kobanak@exbp.roche.com>
2025-07-22 17:13:35 +0200

    Feat: Correction for NULL plots removed"

--- R/g_pkcg.R
+++ R/g_pkcg.R
@@ -728,8 +728,6 @@ pkcg03 <- function(
   yvar = "AVAL",
   xvar_unit = "RRLTU",
   yvar_unit = "AVALU",
-  color_var = NULL,
-  color_var_label = NULL,
   xbreaks_var = "NFRLT",
   xbreaks_mindist = 0.5,
   xmin = NA,
@@ -763,34 +761,10 @@ pkcg03 <- function(
   ymax <- as.numeric(ymax)
 
   # Define summary settings
-  summary_settings <- list(
-    Mean_ci = list(mid = "mean", interval = "mean_ci", whiskers =
-                     c("mean_ci_lwr", "mean_ci_upr")),
-    Mean_sdi = list(mid = "mean", interval = "mean_sdi", whiskers =
-                      c("mean_sdi_lwr", "mean_sdi_upr")),
-    Mean_se = list(mid = "mean", interval = "mean_sei", whiskers =
-                     c("mean_sei_lwr", "mean_sei_upr")),
-    Median_ci = list(mid = "median", interval = "median_ci", whiskers =
-                       c("median_ci_lwr", "median_sci_upr"))
-  )
-  if (!summary_method %in% names(summary_settings)) {
-    stop("Invalid summary_method. Choose from: ", paste(names(summary_settings), collapse = ", "))
-  }
-  mid_value <- summary_settings[[summary_method]]$mid
-  interval_value <- summary_settings[[summary_method]]$interval
-  if (whiskers_lwr_upr == "Both") {
-    whiskers_value <- summary_settings[[summary_method]]$whiskers
-  } else if (whiskers_lwr_upr == "Upper") {
-    whiskers_value <- summary_settings[[summary_method]]$whiskers[2]
-  } else {
-    whiskers_value <- summary_settings[[summary_method]]$whiskers[1]
-  }
-  # Ensure color_var is interpreted as a factor
-  if (!is.null(color_var)) {
-    adpc[[color_var]] <- as.factor(adpc[[color_var]])
-  }
-
-  # save col labels, as further adpc tranformations cause them to be lost #
+  summary_settings <- define_summary_settings(summary_method, whiskers_lwr_upr)
+  mid_value <- summary_settings$mid_value
+  interval_value <- summary_settings$interval_value
+  whiskers_value <- summary_settings$whiskers_value
 
   adpc_grouped <- adpc %>%
     mutate(across(all_of(plotgroup_vars), as.character)) %>%
@@ -810,251 +784,285 @@ pkcg03 <- function(
 
     # Keep timepoints for which at least 50% of the values are not BLQ by timepoint/group
     keep_timepoint <- plot_data %>%
-    group_by(
-      .data[[mean_group_var]], .data[[xvar]]) %>%
-      summarise(
-        # Count number of samples for each timepoint by group
-        n_samples =  n_distinct(USUBJID),
-        # Count number of BLQ samples for each timepoint by group
-        n_blq = (sum(grepl("BLQ|LTR|<[1-9]|<PCLLOQ", AVALCAT1))),
-        # Compute BLQ ratio for each timepoint by group
-        n_blq_ratio = n_blq / n_samples,
-        .groups = "drop"
-      ) %>%
+      group_by(.data[[mean_group_var]], .data[[xvar]]) %>%
+      summarise(# Count number of samples for each timepoint by group
+                n_samples =  n_distinct(USUBJID),
+                # Count number of BLQ samples for each timepoint by group
+                n_blq = (sum(grepl("BLQ|LTR|<[1-9]|<PCLLOQ", AVALCAT1))),
+                # Compute BLQ ratio for each timepoint by group
+                n_blq_ratio = n_blq / n_samples,
+                .groups = "drop") %>%
       filter(n_blq_ratio <= 0.5, n_samples > 1) %>%
       select(.data[[mean_group_var]], .data[[xvar]])
+
     # Filter adpc to keep only timepoints of interest (group, timevar match keep_timepoint entries)
     plot_data <- plot_data %>% inner_join(keep_timepoint,
                                           by = c(mean_group_var, xvar))
-    method_label <- paste(
-      dplyr::case_when(
-        summary_method %in% c("Mean_se", "Mean_ci", "Mean_sdi") ~ "Mean",
-        summary_method == "Median_ci" ~ "Median"
-      ),
-      dplyr::case_when(
-        whiskers_lwr_upr == "Both"  ~ "±",
-        whiskers_lwr_upr == "Upper" ~ "+",
-        whiskers_lwr_upr == "Lower" ~ "-"
-      ),
-      dplyr::case_when(
-        summary_method == "Mean_se"  ~ "SE",
-        summary_method == "Mean_ci"  ~ "CI",
-        summary_method == "Mean_sdi" ~ "SD",
-        summary_method == "Median_ci" ~ "CI"
-      )
-    )
 
-    title <- paste0(
-      method_label, " ", generate_title_mean(plot_data, title, scale, studyid, mean_group_var)
-    )
-    subtitle <- generate_subtitle_mean(
-      plot_data, subtitle, plotgroup_vars, plotgroup_names
-    )
+    if (nrow(plot_data) > 0) {
 
-    title_text <- paste0(title, "<br>", "<sup>", subtitle, "</sup>")
-    title_margin <- (0.5 * length(unlist(strsplit(title_text, "\n|<br>"))))
 
-    #' magic numbers for footnote position and margin, work in app up to 4 lines
-    footnote <- {
-      if (is.null(footnote)) {
-        ""
-      } else {
-        parse_annotation(plot_data, footnote)
-      }
-    }
-    footnote_y <- 0.1 + (0.05 * length(unlist(strsplit(footnote, "\n|<br>"))))
-
-    plot <- tern::g_lineplot(
-      df = plot_data,
-      variables =  tern::control_lineplot_vars(
-        x = xvar,
-        y = yvar,
-        group_var = mean_group_var,
-        paramcd = "PARAM",
-        y_unit = yvar_unit,
-        subject_var = "USUBJID",
-      ),
-      alt_counts_df = plot_data,
-      mid = mid_value,
-      interval = interval_value,
-      whiskers = whiskers_value,
-      x_lab = parse_annotation(plot_data, xlab),
-      y_lab = parse_annotation(plot_data, ylab),
-      y_lab_add_paramcd = FALSE,
-      y_lab_add_unit = FALSE,
-      title = "Plot of Mean and 95% Confidence Limits by Visit",
-      subtitle = "xxx",
-      caption = NULL
-    )
-
-    # Provide limits and additional potential future aesthetic customizations
-    plot <- plot +
-      theme(
-        plot.title = element_text(family = "sans", size = 14, color = "black"),
-        plot.subtitle = element_text(family = "sans", size = 11, color = "black")
-      ) +
-      coord_cartesian(xlim = c(xmin, xmax), ylim = c(ymin, ymax))
-
-    # Ensure x breaks labels do not overlap graphically
-    plot <- plot +
-      scale_x_continuous(
-        guide = guide_axis(n.dodge = 1),
-        breaks = filter_breaks(
-          plot_data[[xbreaks_var]],
-          min_cm_distance = xbreaks_mindist,
-          plot = plot
+      method_label <- paste(
+        dplyr::case_when(
+          summary_method %in% c("Mean_se", "Mean_ci", "Mean_sdi") ~ "Mean",
+          summary_method == "Median_ci" ~ "Median"
         ),
-        labels = \(x) ifelse(x %% 1 == 0, as.character(as.integer(x)), as.character(x))
-      )
-
-    # Add color when specified
-    if (!is.null(color_var)) {
-      plot <- plot +
-        aes(color = !!sym(color_var)) +
-        theme(legend.position = "none")
-
-    # Add color legend only when neccessary
-      if (length(unique(adpc[[color_var]])) > 1) {
-
-    # Make sure the variable is interpreted as a factor
-        adpc[[color_var]] <- as.factor(adpc[[color_var]])
-
-    # Add to the plot the color_var and color_var_label
-        plot <- plot +
-          labs(color = if (!is.null(color_var_label)) color_var_label else color_var) +
-          theme(legend.position = "bottom")
-      }
-    }
-
-    aval_stat <- mid_value
-    if (scale == "LOG") {
-
-      log_data <- plot$data %>%
-        dplyr::mutate(
-          !!sym(aval_stat)  := ifelse(
-            !!sym(aval_stat) < 1e-3,
-          yes = 1e-3, no = !!sym(aval_stat)
-        ))
-      plot <- plot %+%  log_data
-      
-      if (!plotly) {
-        plot <- plot +
-          scale_y_continuous(
-          transform = "log10",
-          labels = \(x) ifelse(x == 1e-3, yes = 0, no = x)
-          )
-        }
-      }
-
-    if (scale == "SBS") {
-      if (!requireNamespace("ggh4x", quietly = FALSE))
-      stop(
-        "Side-by-side view requires `ggh4x` package, please install it with ",
-        "`install.packages('ggh4x')`"
-      )
-    if (!requireNamespace("scales", quietly = FALSE))
-      stop(
-        "Side-by-side view requires `scales` package, please install it with ",
-        "`install.packages('scales')`"
-      )
-
-      # Create SBS version of data and plot
-    sbs_data  <- bind_rows(
-      plot$data %>% dplyr::mutate(view = "Linear view"),
-      plot$data %>% dplyr::mutate(view = "Semilogarithmic view (Log10)")
-    ) %>%
-      dplyr::mutate(
-        !!sym(aval_stat) := ifelse(
-          !!sym(aval_stat) < 1e-3 & view == "Semilogarithmic view (Log10)",
-          yes = 1e-3, no = !!sym(aval_stat)
+        dplyr::case_when(
+          whiskers_lwr_upr == "Both"  ~ "±",
+          whiskers_lwr_upr == "Upper" ~ "+",
+          whiskers_lwr_upr == "Lower" ~ "-"
+        ),
+        dplyr::case_when(
+          summary_method == "Mean_se"  ~ "SE",
+          summary_method == "Mean_ci"  ~ "CI",
+          summary_method == "Mean_sdi" ~ "SD",
+          summary_method == "Median_ci" ~ "CI"
         )
       )
 
-          plot <- plot %+%  sbs_data +
-      facet_wrap(~ view, scales = "free_y") +
-      ggh4x::scale_y_facet(
-        view == "Semilogarithmic view (Log10)",
-        trans  = "log10",
-        labels = \(x) ifelse(x == 1e-3, yes = 0, no = x)
-      )
-  }
-    
-    if (plotly) {
-    suppressWarnings({
-      plotly_plot <- plot %>%
-        ggplotly(
-          tooltip = c("x", "y"),
-          dynamicTicks = if (scale != "SBS") TRUE else FALSE,
-          height = 500 + (footnote_y * 25) + title_margin * 50
-        ) %>%
-        layout(
-          # title and subtitle #
-          title = list(text = title_text),
-          annotations = list(
-            x = 0,
-            y = -footnote_y,
-            text = footnote,
-            showarrow = FALSE,
-            yref = "paper",
-            xref = "paper",
-            align = "left",
-            parse = TRUE
-          )
+      if (generate_title_mean(plot_data, title, scale, studyid, mean_group_var)[[2]] == 0) {
+        title <- paste0(
+          method_label, " ", generate_title_mean(plot_data, title, scale, studyid,
+                                                 mean_group_var)[[1]]
         )
+      } else if (generate_title_mean(plot_data, title, scale, studyid,
+                                     mean_group_var)[[2]] == 1) {
+        title <- generate_title_mean(plot_data, title, scale, studyid, mean_group_var)[[1]]
+      }
 
-      if (scale == "LOG") {
-        plotly_plot <- plotly_plot %>%
-            layout(yaxis = list(
-              type = "log",
-              autorange = TRUE,
-              tickformat = "~r"
-            ))
-        }
+      subtitle <- generate_subtitle_mean(
+        plot_data, subtitle, plotgroup_vars, plotgroup_names
+      )
 
-      plotly_plot
-      })
+      title_text <- paste0(title, "<br>", "<sup>", subtitle, "</sup>")
+      title_margin <- (0.5 * length(unlist(strsplit(title_text, "\n|<br>"))))
 
+      #' magic numbers for footnote position and margin, work in app up to 4 lines
+      footnote <- {
+        if (is.null(footnote)) {
+          ""
         } else {
-  plot <- plot +
-        labs(
-          title = title,
-          subtitle = subtitle,
-          caption = footnote
+          parse_annotation(plot_data, footnote)
+        }
+      }
+      footnote_y <- 0.1 + (0.05 * length(unlist(strsplit(footnote, "\n|<br>"))))
+
+
+      plot <- tern::g_lineplot(
+        df = plot_data,
+        variables =  tern::control_lineplot_vars(
+          x = xvar,
+          y = yvar,
+          group_var = mean_group_var,
+          paramcd = "PARAM",
+          y_unit = yvar_unit,
+          subject_var = "USUBJID",
+        ),
+        alt_counts_df = plot_data,
+        mid = mid_value,
+        interval = interval_value,
+        whiskers = whiskers_value,
+        x_lab = parse_annotation(plot_data, xlab),
+        y_lab = parse_annotation(plot_data, ylab),
+        y_lab_add_paramcd = FALSE,
+        y_lab_add_unit = FALSE,
+        title = "Plot of Mean and 95% Confidence Limits by Visit",
+        subtitle = "xxx",
+        caption = NULL
+      )
+
+
+
+      # Provide limits and additional potential future aesthetic customizations
+      plot <- plot +
+        theme(
+          plot.title = element_text(family = "sans", size = 14, color = "black"),
+          plot.subtitle = element_text(family = "sans", size = 11, color = "black")
+        ) +
+        coord_cartesian(xlim = c(xmin, xmax), ylim = c(ymin, ymax))
+
+      # Ensure x breaks labels do not overlap graphically
+      plot <- plot +
+        scale_x_continuous(
+          guide = guide_axis(n.dodge = 1),
+          breaks = filter_breaks(
+            plot_data[[xbreaks_var]],
+            min_cm_distance = xbreaks_mindist,
+            plot = plot
+          ),
+          labels = \(x) ifelse(x %% 1 == 0, as.character(as.integer(x)), as.character(x))
         )
+
+
+      aval_stat <- mid_value
+      if (scale == "LOG") {
+
+        log_data <- plot$data %>%
+          dplyr::mutate(!!sym(aval_stat)  := ifelse(
+            !!sym(aval_stat) < 1e-3,
+            yes = 1e-3, no = !!sym(aval_stat)
+          ))
+        plot <- plot %+%  log_data
+
+        if (!plotly) {
+          plot <- plot +
+            scale_y_continuous(
+              transform = "log10",
+              labels = \(x) ifelse(x == 1e-3, yes = 0, no = x)
+            )
+        }
+      }
+
+      if (scale == "SBS") {
+        if (!requireNamespace("ggh4x", quietly = FALSE))
+          stop(
+            "Side-by-side view requires `ggh4x` package, please install it with ",
+            "`install.packages('ggh4x')`"
+          )
+        if (!requireNamespace("scales", quietly = FALSE))
+          stop(
+            "Side-by-side view requires `scales` package, please install it with ",
+            "`install.packages('scales')`"
+          )
+
+        # Create SBS version of data and plot
+        sbs_data  <- bind_rows(plot$data %>% dplyr::mutate(view = "Linear view"),
+                               plot$data %>%
+                                 dplyr::mutate(view = "Semilogarithmic view (Log10)")) %>%
+
+          dplyr::mutate(
+            !!sym(aval_stat) := ifelse(
+              !!sym(aval_stat) < 1e-3 & view == "Semilogarithmic view (Log10)",
+              yes = 1e-3, no = !!sym(aval_stat)
+            )
+          )
+
+        plot <- plot %+%  sbs_data +
+          facet_wrap(~ view, scales = "free_y") +
+          ggh4x::scale_y_facet(view == "Semilogarithmic view (Log10)",
+            trans  = "log10",
+            labels = \(x) ifelse(x == 1e-3, yes = 0, no = x)
+          )
+      }
+
+      if (plotly) {
+        suppressWarnings({
+          plotly_plot <- plot %>%
+            ggplotly(
+              tooltip = c("x", "y"),
+              dynamicTicks = if (scale != "SBS") TRUE else FALSE,
+              height = 500 + (footnote_y * 25) + title_margin * 50
+            ) %>%
+            layout(
+              # title and subtitle #
+              title = list(text = title_text),
+              annotations = list(
+                x = 0,
+                y = -footnote_y,
+                text = footnote,
+                showarrow = FALSE,
+                yref = "paper",
+                xref = "paper",
+                align = "left",
+                parse = TRUE
+              )
+            )
+
+          if (scale == "LOG") {
+            plotly_plot <- plotly_plot %>%
+              layout(yaxis = list(
+                type = "log",
+                autorange = TRUE,
+                tickformat = "~r"
+              ))
+          }
+
+          plotly_plot
+        })
+
+      } else {
+        plot <- plot +
+          labs(
+            title = title,
+            subtitle = subtitle,
+            caption = footnote
+          )
+      }
+    } else {
+      # This block should be empty, or explicitly return NULL,
+      # so that NULL is returned when nrow(plot_data) == 0
+      NULL
     }
   })
-  plots  |>
+
+  final_plots <- plots  |>
     setNames(plot_ids)
- 
+
+  # Filter out NULL elements from the list of plots
+  Filter(Negate(is.null), final_plots)
+
 }
 
 generate_title_mean <- function(plot_data, title, scale, studyid, mean_group_var) {
   if (is.null(title)) {
-    paste0(
+    list(paste0(
       "Plot of PK Concentration-Time Profile ",
       dplyr::case_when(
         scale == "LIN" ~ "linear",
         scale == "LOG" ~ "logarithmic",
         TRUE ~ "linear and logarithmic"
       ),
-      " scale by ", parse_annotation(plot_data, paste0("!", mean_group_var)), ": ", 
+      " scale by ", parse_annotation(plot_data, paste0("!", mean_group_var)), ": ",
       unique(plot_data[[studyid]])
-    )
+    ), custom_title = 0)
   } else {
-    parse_annotation(plot_data, title)
+    list(parse_annotation(plot_data, title), custom = 1)
   }
 }
 
 # Helper Function for Subtitle Generation
 generate_subtitle_mean <- function(plot_data, subtitle, plotgroup_vars, plotgroup_names) {
   if (is.null(subtitle)) {
-    paste0(paste(
-      unlist(unname(plotgroup_names[plotgroup_vars])), ": ",
-      unique(plot_data[, plotgroup_vars]),
-      sep = "", collapse = ", "
-      )
+    paste0(paste(unlist(unname(plotgroup_names[plotgroup_vars])), ": ",
+                 unique(plot_data[, plotgroup_vars]),
+                 sep = "", collapse = ", ")
     )
   } else {
     parse_annotation(plot_data, subtitle)
   }
+}
+define_summary_settings <- function(summary_method, whiskers_lwr_upr) {
+  summary_settings <- list(
+    Mean_ci = list(mid = "mean",
+                   interval = "mean_ci",
+                   whiskers = c("mean_ci_lwr", "mean_ci_upr")), # Removed 'lower =' and 'upper ='
+    Mean_sdi = list(mid = "mean",
+                    interval = "mean_sdi",
+                    whiskers = c("mean_sdi_lwr", "mean_sdi_upr")),
+    Mean_se = list(mid = "mean", interval = "mean_sei",
+                   whiskers = c("mean_sei_lwr", "mean_sei_upr")),
+    Median_ci = list(mid = "median",
+                     interval = "median_ci",
+                     whiskers = c("median_ci_lwr", "median_ci_upr"))
+  )
+  if (!summary_method %in% names(summary_settings)) {
+    stop("Invalid summary_method. Choose from: ", paste(names(summary_settings), collapse = ", "))
+  }
+
+  mid_value <- summary_settings[[summary_method]]$mid
+  interval_value <- summary_settings[[summary_method]]$interval
+
+  whiskers_base <- summary_settings[[summary_method]]$whiskers
+
+  if (whiskers_lwr_upr == "Both") {
+    whiskers_value <- whiskers_base # Assign the full vector
+  } else if (whiskers_lwr_upr == "Upper") {
+    whiskers_value <- whiskers_base[2]
+  } else if (whiskers_lwr_upr == "Lower") {
+    whiskers_value <- whiskers_base[1]
+  } else {
+    stop("Invalid whiskers_lwr_upr. Choose from: 'Both', 'Upper', 'Lower'.")
+  }
+
+  list(mid_value = mid_value, interval_value = interval_value, whiskers_value = whiskers_value)
 }
