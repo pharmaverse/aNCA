89c4a035dbd03f423c9f03d6428e4c8def9201a5
Legras <legrasv@emea.roche.com>
2025-07-23 17:37:59 +0200

    lintr

--- R/export_cdisc.R
+++ R/export_cdisc.R
@@ -95,7 +95,6 @@ export_cdisc <- function(res_nca) {
     group_by(!!!syms(intersect(to_match_res_cols, names(.)))) %>%
     slice(1) %>%
     ungroup()
-  
   cdisc_info <- res_nca_req$result  %>%
     left_join(conc_info,
               by = intersect(names(.), names(conc_info)),
@@ -346,7 +345,6 @@ adjust_class_and_length <- function(df, metadata) {
       df[[var]] <- substr(as.character(df[[var]]), 0, var_specs$Length)
     } else if (var_specs$Type %in% c("Num", "integer", "float") &&
                  !endsWith(var, "DTM")) {
-      # df[[var]] <- round(as.numeric(df[[var]]), var_specs$Length)
     } else if (!var_specs$Type %in% c(
       "dateTime", "duration", "integer", "float", "Num"
     )) {
