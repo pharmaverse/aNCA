626faae540096d856ec0b773fa14c25cc017bf07
osenan <oriol@praenoscere.com>
2025-08-11 23:30:14 -0300

    feat: add first app load test and basic shinytest2 structure

---
+++ tests/testthat/test-e2e-runner.R
@@ -0,0 +1,8 @@
+test_that("End to End test pass", {
+  # based on here: https://rstudio.github.io/shinytest2/articles/use-package.html#applications-in-inst
+  skip_on_cran()
+
+  appdir <- system.file(package = "aNCA", "shiny")
+
+  test_app(appdir)
+})
