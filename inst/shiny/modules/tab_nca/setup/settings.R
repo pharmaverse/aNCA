bfed2c7eb36e948d27788b8fe5a96795bac41a7d
Gero1999 <gerardojoro@gmail.com>
2025-08-14 14:09:24 +0200

    server: integrate new selectInput

--- inst/shiny/modules/tab_nca/setup/settings.R
+++ inst/shiny/modules/tab_nca/setup/settings.R
@@ -81,7 +81,7 @@ settings_ui <- function(id) {
       accordion_panel(
         title = "Parameter Selection",
         virtualSelectInput(
-          inputId = "nca_parameters_selector",
+          inputId = ns("nca_parameters"),
           label = "Select:",
           choices = metadata_nca_parameters %>%
             filter(TYPE != "PKNCA-not-covered") %>%
@@ -92,9 +92,16 @@ settings_ui <- function(id) {
               description = PPTEST,
               alias = PPTEST
             ),
-          selected = metadata_nca_parameters %>%
-            filter(TYPE == "Standard") %>%
-            pull(PKNCA),
+          # TODO (Gerardo): This should be perhaps indicated in the metadata_nca_variables
+          # Depending on the type of study
+          selected = c(
+            "aucinf.obs", "aucinf.obs.dn", "auclast", "auclast.dn",
+            "cmax", "cmax.dn", "clast.obs", "clast.obs.dn",
+            "tlast", "tmax", "half.life", "cl.obs", "vss.obs", "vz.obs",
+            "mrt.last", "mrt.obs", "lambda.z", "lambda.z.n.points", "r.squared",
+            "adj.r.squared", "lambda.z.time.first", "aucpext.obs", "aucpext.pred",
+            "ae", "fe"
+          ),
           showSelectedOptionsFirst = TRUE,
           hasOptionDescription = TRUE,
           showValueAsTags = TRUE,
@@ -102,14 +109,6 @@ settings_ui <- function(id) {
           search = TRUE,
           multiple = TRUE,
           width = "100%"
-        ),
-        reactableOutput(ns("nca_parameters")),
-        card(
-          full_screen = FALSE,
-          style = "margin-top: 2em;",
-          card_header("Selected NCA Parameters"),
-          card_body(uiOutput(ns("nca_param_display"))
-          )
         )
       ),
       accordion_panel(
@@ -213,8 +212,7 @@ settings_server <- function(id, data, adnca_data, settings_override) {
         filter(TYPE != "PKNCA-not-covered") %>%
         pull("PKNCA")
 
-      updateReactable(
-        "nca_parameters",
+      updateVirtualSelect(
         selected = which(params_data %in% settings$parameter_selection)
       )
 
@@ -321,65 +319,6 @@ settings_server <- function(id, data, adnca_data, settings_override) {
       )
     })
 
-    DEFAULT_PARAMS <- c(
-      "aucinf.obs", "aucinf.obs.dn",
-      "auclast", "auclast.dn",
-      "cmax", "cmax.dn",
-      "clast.obs", "clast.obs.dn",
-      "tlast", "tmax",
-      "half.life", "cl.obs", "vss.obs", "vz.obs",
-      "mrt.last", "mrt.obs",
-      "lambda.z",
-      "lambda.z.n.points", "r.squared",
-      "adj.r.squared", "lambda.z.time.first",
-      "aucpext.obs", "aucpext.pred",
-      "ae", "fe"
-    )
-
-    output$nca_parameters <- renderReactable({
-      #remove parameters that are currently unavailable in PKNCA
-      params_data <- metadata_nca_parameters %>%
-        filter(TYPE != "PKNCA-not-covered")
-
-      default_row_indices <- which(params_data$PKNCA %in% DEFAULT_PARAMS)
-
-      reactable(
-        params_data %>%
-          select(TYPE, PPTESTCD, PPTEST, CAT),
-        groupBy = c("TYPE"),
-        pagination = FALSE,
-        filterable = TRUE,
-        compact = TRUE,
-        onClick = "select",
-        height = "49vh",
-        selection = "multiple",
-        defaultSelected = default_row_indices
-      )
-    })
-
-    nca_params <- reactive({
-      selected_rows <- getReactableState("nca_parameters", "selected")
-      if (is.null(selected_rows) || length(selected_rows) == 0) return(NULL)
-
-      params_data <- metadata_nca_parameters %>%
-        filter(TYPE != "PKNCA-not-covered")
-      selected_terms <- params_data[selected_rows, , drop = FALSE]
-
-      # Return PKNCA column names
-      selected_terms$PKNCA
-    })
-
-    output$nca_param_display <- renderUI({
-      req(nca_params())
-
-      div(
-        class = "nca-pill-grid",
-        lapply(nca_params(), function(param) {
-          tags$span(class = "nca-pill", param)
-        })
-      )
-    })
-
     # Reactive value to store the AUC data table
     auc_data <- reactiveVal(
       tibble(start_auc = rep(NA_real_, 2), end_auc = rep(NA_real_, 2))
@@ -431,13 +370,14 @@ settings_server <- function(id, data, adnca_data, settings_override) {
 
     settings <- reactive({
       req(input$select_analyte) # Wait on general settings UI to be loaded
+
       list(
         analyte = input$select_analyte,
         profile = input$select_profile,
         pcspec = input$select_pcspec,
         method = input$method,
         bioavailability = input$bioavailability,
-        parameter_selection = nca_params(),
+        parameter_selection = input$nca_parameters,
         data_imputation = list(
           impute_c0 = input$should_impute_c0
         ),
