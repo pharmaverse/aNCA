9eebdcf2edf6e49a5048c1a3ca950b0de60669bc
Gero1999 <gerardojoro@gmail.com>
2025-08-15 12:57:07 +0200

    add volpk parameter

---
+++ R/PKNCA_extra_parameters.R
@@ -0,0 +1,20 @@
+#' Calculate the total urine volume
+#'
+#' @param volume The volume (or mass) of the sample
+#' @return The sum of urine volumes for the interval
+#' @export
+pk.calc.volpk <- function(volume) {
+  sum(volume, na.rm = TRUE)
+}
+add.interval.col("volpk",
+                 FUN="pk.calc.volpk",
+                 values=c(FALSE, TRUE),
+                 unit_type="volume",
+                 pretty_name="Total Urine Volume",
+                 desc="The sum of urine volumes for the interval")
+PKNCA.set.summary(
+  name="volpk",
+  description="geometric mean and geometric coefficient of variation",
+  point=business.geomean,
+  spread=business.geocv
+)
