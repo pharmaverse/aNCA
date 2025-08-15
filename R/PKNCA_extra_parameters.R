4ca7d6e2f7de5bc8bc73c7e339f8abf45eeae714
Gero1999 <gerardojoro@gmail.com>
2025-08-15 13:28:33 +0200

    refactor: lintr

--- R/PKNCA_extra_parameters.R
+++ R/PKNCA_extra_parameters.R
@@ -3,18 +3,20 @@
 #' @param volume The volume (or mass) of the sample
 #' @return The sum of urine volumes for the interval
 #' @export
-pk.calc.volpk <- function(volume) {
+pk.calc.volpk <- function(volume) { #nolint
   sum(volume, na.rm = TRUE)
 }
-PKNCA::add.interval.col("volpk",
-                 FUN="pk.calc.volpk",
-                 values=c(FALSE, TRUE),
-                 unit_type="volume",
-                 pretty_name="Total Urine Volume",
-                 desc="The sum of urine volumes for the interval")
+PKNCA::add.interval.col(
+  "volpk",
+  FUN = "pk.calc.volpk",
+  values = c(FALSE, TRUE),
+  unit_type = "volume",
+  pretty_name = "Total Urine Volume",
+  desc = "The sum of urine volumes for the interval"
+)
 PKNCA::PKNCA.set.summary(
-  name="volpk",
-  description="geometric mean and geometric coefficient of variation",
-  point=PKNCA::business.geomean,
-  spread=PKNCA::business.geocv
+  name = "volpk",
+  description = "geometric mean and geometric coefficient of variation",
+  point = PKNCA::business.geomean,
+  spread = PKNCA::business.geocv
 )
