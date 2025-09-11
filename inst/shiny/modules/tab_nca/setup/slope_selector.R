1c1ad55e7bfafc9c7e4181fad1fa1372e5835238
Gero1999 <gerardojoro@gmail.com>
2025-09-11 11:17:31 +0200

    refactor: create a module page_and_searcher.R

--- inst/shiny/modules/tab_nca/setup/slope_selector.R
+++ inst/shiny/modules/tab_nca/setup/slope_selector.R
@@ -61,7 +61,6 @@ slope_selector_ui <- function(id) {
     # Widgets for manipulating plots display #
     fluidRow(
       class = "plot-widgets-container",
-
       div(
         class = "plot-widget-group",
         selectInput(
@@ -71,7 +70,6 @@ slope_selector_ui <- function(id) {
           selected = 1
         )
       ),
-
       div(
         class = "plot-widget-group",
         selectInput(
@@ -86,35 +84,8 @@ slope_selector_ui <- function(id) {
     # Plots display #
     uiOutput(ns("slope_plots_ui"), class = "slope-plots-container"),
     br(),
-    fluidRow(
-      class = "plot-widgets-container-2",
-      div(
-        class = "plot-widget-group",
-        actionButton(
-          ns("previous_page"),
-          "Previous Page",
-          class = "btn-page"
-        )
-      ),
-
-      div(
-        class = "plot-widget-group",
-        tags$span("Page "),
-        pickerInput(ns("select_page"), "", choices = c(),
-                    width = "100px"),
-        tags$span("of "),
-        uiOutput(ns("page_number"), inline = TRUE)
-      ),
-
-      div(
-        class = "plot-widget-group",
-        actionButton(
-          ns("next_page"),
-          "Next Page",
-          class = "btn-page"
-        )
-      )
-    ),
+    # Use the new pagination UI module
+    page_and_searcher_page_ui(ns("page_and_searcher")),
     br()
   )
 }
@@ -126,7 +97,7 @@ slope_selector_server <- function( # nolint
     log_trace("{id}: Attaching server")
 
     ns <- session$ns
-    
+
     pknca_data <- reactiveVal(NULL)
     plot_outputs <- reactiveVal(NULL)
     observeEvent(list(processed_pknca_data()), {
@@ -247,91 +218,27 @@ browser()
 
     # HACK: workaround to avoid plotly_click not being registered warning
     session$userData$plotlyShinyEventIDs <- "plotly_click-A"
-    current_page <- reactiveVal(1)
 
-    #' updating current page based on user input
-    observeEvent(input$next_page, {
-      current_page(current_page() + 1)
-      shinyjs::disable(selector = ".btn-page")
-    })
-    observeEvent(input$previous_page, {
-      if (current_page() == 1) return(NULL)
-      current_page(current_page() - 1)
-      shinyjs::disable(selector = ".btn-page")
-    })
-    observeEvent(input$select_page, current_page(as.numeric(input$select_page)))
-    observeEvent(list(input$plots_per_page, input$search_subject), current_page(1))
 
-    #' Updating plot outputUI, dictating which plots get displayed to the user.
-    #' Scans for any related reactives (page number, subject filter etc) and updates the plot output
-    #' UI to have only plotlyOutput elements for desired plots.
-    observeEvent(list(
-      plot_outputs(), input$plots_per_page, input$search_subject, current_page()
-    ), {
+    # --- Pagination and search logic using the new module ---
+    search_subject_r <- reactive({ input$search_subject })
+    plots_per_page_r <- reactive({ input$plots_per_page })
+
+    page_search <- page_and_searcher_server(
+      id = "page_and_searcher",
+      search_subject = search_subject_r,
+      plot_outputs = plot_outputs,
+      plots_per_page = plots_per_page_r,
+      ns_parent = ns
+    )
+
+    # Render only the plots requested by the user (using the module's outputs)
+    observe({
       req(plot_outputs())
-      log_trace("{id}: Updating displayed plots")
-
-      # Decide
-      # Make sure the search_subject input is not NULL
-      search_subject <- {
-        if (is.null(input$search_subject) || length(input$search_subject) == 0) {
-          subject_col <- pknca_data()$conc$columns$subject
-          unique(
-            pknca_data()$intervals %>%
-              filter(half.life) %>%
-              .[[subject_col]]
-          )
-        } else {
-          input$search_subject
-        }
-      }
-
-      has_plot_subject <- grepl(
-        paste0("USUBJID: ", "(", paste0(search_subject, collapse = ")|("), ")"),
-        names(plot_outputs())
-      )
-
-      # find which plots should be displayed based on page #
-      num_plots <- sum(has_plot_subject)
-      plots_per_page <- as.numeric(input$plots_per_page)
-      num_pages <- ceiling(num_plots / plots_per_page)
-
-      if (current_page() > num_pages) {
-        current_page(current_page() - 1)
-        return(NULL)
-      }
-
-      page_end <- current_page() * plots_per_page
-      page_start <- page_end - plots_per_page + 1
-      if (page_end > num_plots) page_end <- num_plots
-
-      # update page number display #
-      output$page_number <- renderUI(num_pages)
-
-      # Render only the plots requested by the user
       output$slope_plots_ui <- renderUI({
         shinyjs::enable(selector = ".btn-page")
-        plot_outputs()[has_plot_subject][page_start:page_end]
+        plot_outputs()[page_search$has_plot_subject()][page_search$page_start():page_search$page_end()]
       })
-
-      # update jump to page selector #
-      updatePickerInput(
-        session = session,
-        inputId = "select_page",
-        choices = 1:num_pages,
-        selected = current_page()
-      )
-
-      # update plot display #
-      shinyjs::toggleClass(
-        selector = ".slope-plots-container",
-        class = "multiple",
-        condition = plots_per_page != 1
-      )
-
-      # disable buttons if necessary #
-      shinyjs::toggleState(id = ns("previous_page"), condition = current_page() == 1)
-      shinyjs::toggleState(id = ns("next_page"), condition = current_page() == num_pages)
     })
 
     # Creates an initial version of the manual slope adjustments table with pknca_data
