725aea4c9fdcb8c98168053765eb17216d4e1a1c
Gero1999 <gerardojoro@gmail.com>
2025-09-29 16:46:19 +0200

    fix: make alternative str for pptx querto slides & enable html for revealjs

--- inst/shiny/functions/save_output.R
+++ inst/shiny/functions/save_output.R
@@ -62,7 +62,7 @@ format_to_xpt_compatible <- function(data) {
 #' @param df A data.frame to be shown as a table.
 #'
 #' @return Invisibly returns TRUE if the slide was appended.
-add_slide_2plots_table <- function(quarto_path, plot1, plot2, df) {
+add_slide_PlotTablePlot <- function(quarto_path, plot1, plot2, df, use_plotly = FALSE) {
   slide_content <- c(
     "\n---",
     "",
@@ -70,47 +70,91 @@ add_slide_2plots_table <- function(quarto_path, plot1, plot2, df) {
     "",
     "::: column",
     "```{r, echo=FALSE}",
-    plot1,
+    paste0(
+      plot1,
+      if (use_plotly) {" |> plotly::ggplotly()"} else {""}
+    ),
+    "flextable(", df, ")",
     "```",
     ":::",
     "",
     "::: column",
     "```{r, echo=FALSE}",
-    plot2,
+    paste0(
+      plot2,
+      if (use_plotly) {" |> plotly::ggplotly()"} else {""}
+    ),
     "```",
     ":::",
     "",
     ":::",
-    "",
-    "```{r, echo=FALSE}",
-    "flextable::flextable(",
-    df,
-    ")",
-    "```"
+    ""
   )
   write(slide_content, file = quarto_path, append = TRUE)
   invisible(TRUE)
 }
 
-create_dose_slides_quarto <- function(res_dose_slides, quarto_path, title, rds_path, template, extra_setup){
-  create_quarto_presentation(quarto_path, title, rds_path)
+add_slides_TablePlot_Plot <- function(quarto_path, plot1, plot2, df, use_plotly = FALSE) {
+  slide_content <- c(
+    "\n---",
+    "",
+    "::: columns",
+    "",
+    "::: column",
+    "```{r, echo=FALSE}",
+    "flextable(", df, ")",
+    "```",
+    ":::",
+    "",
+    "::: column",
+    "```{r, echo=FALSE}",
+    paste0(
+      plot2,
+      if (use_plotly) {" |> plotly::ggplotly()"} else {""}
+    ),
+    "```",
+    ":::",
+    "",
+    ":::",
+    "",
+    "\n---",
+    "```{r, echo=FALSE}",
+    paste0(
+      plot1,
+      if (use_plotly) {" |> plotly::ggplotly()"} else {""}
+    ),
+    "```",
+    ""
+  )
+  write(slide_content, file = quarto_path, append = TRUE)
+  invisible(TRUE)
+}
+
+create_dose_slides_quarto <- function(res_dose_slides, quarto_path, title, rda_path, slide_fun = "add_slide_PlotTablePlot", template, extra_setup, use_plotly = FALSE){
+  create_quarto_presentation(quarto_path, title, rda_path)
+  slide_fun <- get(slide_fun)
   for (i in seq_len(length(res_dose_slides))) {
-    add_slide_2plots_table(
+    slide_fun(
       quarto_path,
       plot1 = paste0("res_dose_slides[[", i, "]]$linplot"),
       plot2 = paste0("res_dose_slides[[", i, "]]$meanplot"),
-      df = paste0("res_dose_slides[[", i, "]]$statistics")
+      df = paste0("res_dose_slides[[", i, "]]$statistics"),
+      use_plotly = use_plotly
     )
   }
 }
-
-create_dose_slides_presentation <- function(res_dose_slides, path, title, rds_path, template, extra_setup, output_format = "pptx"){
-  quarto_path <- gsub("\\..*", ".qmd", path)
-  save(list = as.character(quote(res_dose_slides)), file = "saved_results_for_de.rds")
-  create_dose_slides_quarto(res_dose_slides, quarto_path = quarto_path, "tmp file", rds_path = "saved_results_for_de.rds")
+# create_dose_slides_presentation(res_dose_slides = res_dose_slides, path = "test_dose_slides.pptx", title = "Dose Escalation Slides (Unnamed project)", template = NULL)
+# create_dose_slides_presentation(res_dose_slides = res_dose_slides, path = "test_dose_slides.html", title = "Dose Escalation Slides (Unnamed project)", template = NULL)
+create_dose_slides_presentation <- function(res_dose_slides, path, title, template, extra_setup){
   output_format <- tools::file_ext(path)
+  quarto_path <- gsub(paste0("\\.", output_format), ".qmd", path)
+  if (output_format == "html") output_format <- "revealjs"
+  rda_obj_path <- paste0(dirname(quarto_path), "/all_outputs.rda")
+  use_plotly <- if (output_format == "pptx") FALSE else TRUE
+  slide_fun <- if (output_format == "pptx") "add_slides_TablePlot_Plot" else "add_slide_PlotTablePlot"
+  save(list = as.character(quote(res_dose_slides)), file = rda_obj_path)
+  create_dose_slides_quarto(res_dose_slides, quarto_path = quarto_path, title, slide_fun = slide_fun, rda_path = basename(rda_obj_path), use_plotly = use_plotly)
   quarto::quarto_render(input = quarto_path, output_format = output_format)
-  file.remove(quarto_path)
 }
 
 #' Create a New Quarto Presentation Document for Exporting Plots
@@ -121,11 +165,11 @@ create_dose_slides_presentation <- function(res_dose_slides, path, title, rds_pa
 #'
 #' @param quarto_path Path to the Quarto (.qmd) file to create.
 #' @param title Title for the presentation.
-#' @param rds_path Path to the RDS file to be loaded in the document.
+#' @param rda_path Path to the RDS file to be loaded in the document.
 #' @param template (Optional) Path to a Quarto template to use (default: NULL).
 #' @param extra_setup (Optional) Character vector of extra setup lines to include after YAML.
 #' @return Invisibly returns TRUE if the file was created.
-create_quarto_presentation <- function(quarto_path, title = "NCA Report", rds_path = NULL, template = NULL, extra_setup = NULL) {
+create_quarto_presentation <- function(quarto_path, title = "NCA Report", rda_path = NULL, template = NULL, extra_setup = NULL) {
   yaml_header <- c(
     "---",
     paste0("title: \"", title, "\""),
@@ -140,10 +184,11 @@ create_quarto_presentation <- function(quarto_path, title = "NCA Report", rds_pa
   )
   load_chunk <- c(
     "```{r setup, include=FALSE}",
-    if (!is.null(rds_path)) paste0("load(\"", rds_path, "\")") else "",
+    if (!is.null(rda_path)) paste0("load(\"", rda_path, "\")") else "",
     "library(plotly)",
     "library(knitr)",
     "library(kableExtra)",
+    "library(flextable)",
     "```",
     ""
   )
@@ -152,10 +197,9 @@ create_quarto_presentation <- function(quarto_path, title = "NCA Report", rds_pa
   invisible(TRUE)
 }
 
+linplot_meanplot_stats_by_group <- function(o_nca, group_by_vars, statistics = "Mean", facet_vars = "DOSEA", stats_parameters = c("CMAX", "TMAX", "VSS", "CLSTP")) {
 
-linplot_meanplot_stats_by_group <- function(o_nca, group_by_vars, statistics) {
-
-  groups <- unique(o_nca$data$conc$data[, group_by_vars])
+  groups <- unique(o_nca$data$intervals[, group_by_vars])
   output_list <- list()
   # Loop over each of the groups
   for (i in seq_len(nrow(groups))) {
@@ -169,7 +213,7 @@ linplot_meanplot_stats_by_group <- function(o_nca, group_by_vars, statistics) {
       selected_pcspec = d_conc_i[["PCSPEC"]],
       selected_usubjids = d_conc_i[["USUBJID"]],
       colorby_var = "USUBJID",
-      facet_by = NULL,
+      facet_by = facet_vars,
       time_scale = "Whole",
       yaxis_scale = "Log",
       show_threshold = FALSE,
@@ -184,7 +228,7 @@ linplot_meanplot_stats_by_group <- function(o_nca, group_by_vars, statistics) {
       selected_analytes = unique(d_conc_i[["ANALYTE"]]),
       selected_pcspecs = unique(d_conc_i[["PCSPEC"]]),
       selected_cycles = unique(d_conc_i[["NCA_PROFILE"]]),
-      id_variable = "DOSEA",
+      id_variable = facet_vars,
       groupby_var = group_by_vars,
       plot_ylog = FALSE,
       plot_sd_min = TRUE,
@@ -193,21 +237,14 @@ linplot_meanplot_stats_by_group <- function(o_nca, group_by_vars, statistics) {
     )
     stats_i <- calculate_summary_stats(
       data = merge(o_res_i, d_conc_i[, c(group_vars(o_nca), "DOSEA")]),
-      input_groups = "DOSEA"
+      input_groups = facet_vars
     ) %>%
       filter(
         Statistic %in% statistics
       ) %>%
       select(
-        any_of(
-          c(
-            "DOSEA",
-            "CLSTP[ng/mL]",
-            "VSSO[mL]",
-            "TMAX[h]",
-            "CMAX[ng/mL]"
-          )
-        )
+        any_of(c(facet_vars, "Statistic")),
+        dplyr::matches(paste0("^(", paste(stats_parameters, collapse = "|"), ")(\\[.*\\])?$"))
       ) %>%
       unique()
     output_list[[paste0("Group_", i)]] <- list(
@@ -218,15 +255,3 @@ linplot_meanplot_stats_by_group <- function(o_nca, group_by_vars, statistics) {
   }
   output_list
 }
-
-
-result <- list(
-  p1 = plotly::plotly_build(plot_ly(iris, x = iris$`Sepal.Length`, y = iris$`Sepal.Width`, type = "scatter")),
-  p2 = plotly::plotly_build(plot_ly(iris, y = iris$`Sepal.Length`, x = iris$`Sepal.Width`, type = "scatter", color = "red"))
-)
-saveRDS(result, "result.rds")
-save(list = c("result"), file = "results.rda")
-load("results.rda")
-
-create_quarto_presentation("myquarto.qmd", "NCA report", rds_path = "result.rda")
-add_slide_2plots_table("myquarto.qmd", plot1 = "result[['p1']]", plot2 = "result[['p2']]", "iris")
