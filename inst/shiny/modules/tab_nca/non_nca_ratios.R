6bbe0ed1256286c91ab6d0ff8d0eb16474fb69fd
Jana Spinner <spinnerjana@gmail.com>
2025-09-30 13:22:55 +0200

    feat: multiple ratio options for non nca ratios

--- inst/shiny/modules/tab_nca/non_nca_ratios.R
+++ inst/shiny/modules/tab_nca/non_nca_ratios.R
@@ -13,30 +13,25 @@ non_nca_ratio_ui <- function(id, title, select_label1, select_label2) {
       card_header(paste0(title, " Setup")),
       card_body(
         fluidRow(
-          selectInput(
-            ns("selected_spec1"),
-            select_label1,
-            choices = NULL,
-            multiple = TRUE
+          column(6,
+                 actionButton(ns("add_pair"), "Add Ratio Pair", icon = icon("plus"))
           ),
-          selectInput(
-            ns("selected_spec2"),
-            select_label2,
-            choices = NULL,
-            multiple = TRUE
-          ),
-          selectInput(
-            ns("summary_groups"),
-            "Summarise By:",
-            choices = NULL,
-            multiple = TRUE
+          column(6,
+                 selectInput(
+                   ns("summary_groups"),
+                   "Summarise By:",
+                   choices = NULL,
+                   multiple = TRUE
+                 )
           )
         ),
+        # Placeholder for dynamically added ratio pair selectors
+        div(id = ns("ratio_pairs_placeholder"))
       )
     ),
     card(
       height = "60vh",
-      card_header(paste0(title, " Results")),
+      card_header(paste0(title, "Results")),
       card_body(
         reactable_ui(ns("results"))
       )
@@ -54,6 +49,14 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
+    pair_counter <- reactiveVal(0)
+    selections <- reactiveValues()
+
+    spec_options <- reactive({
+      req(data())
+      unique(data()$conc$data$PCSPEC)
+    })
+
     id_groups <- reactive({
       req(data())
       data()$conc$columns$groups %>%
@@ -63,48 +66,103 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
           !is.null(col) && col != "PCSPEC" && length(unique(data()$conc$data[[col]])) > 1
         })
     })
-    # Update select inputs dynamically
+
+    ratio_groups <- reactive({
+      req(data())
+      c(grouping_vars(), id_groups(),
+        data()$dose$columns$dose, data()$dose$columns$time.nominal,
+        data()$dose$columns$route)
+    })
+
+    observeEvent(ratio_groups(), {
+      updateSelectInput(session, "summary_groups", choices = ratio_groups())
+    })
+
+    # Add the first pair automatically when data is loaded
     observeEvent(data(), {
-      spec_options <- unique(data()$conc$data$PCSPEC)
+      req(pair_counter() == 0)
+      # Manually trigger the event for adding a pair
+      input$add_pair
+    }, once = TRUE, ignoreInit = TRUE, priority = 1)
 
-      ratio_groups <- c(grouping_vars(), id_groups(),
-                        data()$dose$columns$dose, data()$dose$columns$time.nominal,
-                        data()$dose$columns$route)
+    # Add a new pair of selectInputs when the button is clicked
+    observeEvent(input$add_pair, {
+      count <- pair_counter() + 1
+      pair_counter(count)
+      
+      pair_id <- paste0("pair_", count)
+      spec1_id <- paste0("spec1_", count)
+      spec2_id <- paste0("spec2_", count)
+      remove_id <- paste0("remove_", count)
 
-      updateSelectInput(session, "selected_spec1", choices = spec_options)
-      updateSelectInput(session, "selected_spec2", choices = spec_options)
-      updateSelectInput(session, "summary_groups", choices = ratio_groups)
-    })
-
-    # Filter & prepare data for calculation
-    filtered_samples <- reactive({
-      req(input$selected_spec1, input$selected_spec2)
-      data()$conc$data %>%
-        filter(PCSPEC %in% c(input$selected_spec1, input$selected_spec2))
-    })
-
-    # Perform Calculation on Submit
-    results <- reactive({
-      req(filtered_samples())
-
-      spec1 <- input$selected_spec1
-      spec2 <- input$selected_spec2
-
-      ratio_groups <- c(grouping_vars(), id_groups(),
-                        data()$dose$columns$dose, data()$dose$columns$time.nominal,
-                        data()$dose$columns$route)
-
-      # Calculate ratios
-      multiple_matrix_ratios(
-        data = filtered_samples(),
-        matrix_col = "PCSPEC",
-        conc_col = data()$conc$columns$concentration,
-        units_col = "AVALU",
-        groups = ratio_groups,
-        spec1 = spec1,
-        spec2 = spec2
+      ui <- fluidRow(
+        id = ns(pair_id),
+        column(5,
+               selectInput(
+                 ns(paste0("spec1_", count)),
+                 "Numerator(s)",
+                 choices = spec_options(),
+                 multiple = TRUE
+               )
+        ),
+        column(5,
+               selectInput(
+                 ns(paste0("spec2_", count)),
+                 "Denominator(s)",
+                 choices = spec_options(),
+                 multiple = TRUE
+               )
+        ),
+        column(2,
+               actionButton(ns(paste0("remove_", count)), "Remove", icon = icon("trash-alt"))
+        )
       )
+
+      insertUI(
+        selector = paste0("#", ns("ratio_pairs_placeholder")),
+        where = "beforeEnd",
+        ui = ui
+      )
+      
+      observeEvent(input[[spec1_id]], { selections[[spec1_id]] <- input[[spec1_id]] }, ignoreNULL = FALSE)
+      observeEvent(input[[spec2_id]], { selections[[spec2_id]] <- input[[spec2_id]] }, ignoreNULL = FALSE)
+
+      # Observer to remove the UI for the specific pair
+      observeEvent(input[[remove_id]], {
+        removeUI(selector = paste0("#", ns(pair_id)))
+        selections[[spec1_id]] <- NULL
+        selections[[spec2_id]] <- NULL
+      }, once = TRUE, ignoreInit = TRUE, autoDestroy = TRUE)
+
     })
+    
+    results <- reactive({
+      req(pair_counter() > 0)
+      # Trigger this reactive when any selection changes
+      reactiveValuesToList(selections)
+      
+      all_ratios <- purrr::map_dfr(seq_len(pair_counter()), function(i) {
+        spec1 <- selections[[paste0("spec1_", i)]]
+        spec2 <- selections[[paste0("spec2_", i)]]
+        
+        if (length(spec1) > 0 && length(spec2) > 0) {
+          filtered_samples <- data()$conc$data %>% filter(PCSPEC %in% c(spec1, spec2))
+          multiple_matrix_ratios(
+            data = filtered_samples,
+            matrix_col = "PCSPEC",
+            conc_col = data()$conc$columns$concentration,
+            units_col = "AVALU",
+            groups = ratio_groups(),
+            spec1 = spec1,
+            spec2 = spec2
+          )
+        } else { NULL }
+      })
+      
+      if (nrow(all_ratios) == 0) return(NULL)
+      return(all_ratios)
+    }) %>% debounce(500) # Wait 500ms after user stops changing inputs
+    
 
     full_output <- reactive({
       req(results())
