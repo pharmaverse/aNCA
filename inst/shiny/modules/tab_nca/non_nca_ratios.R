8a090bc49fe60ba7c308b161dd910a3c49aba027
Jana Spinner <spinnerjana@gmail.com>
2025-09-30 14:40:26 +0200

    lint

--- inst/shiny/modules/tab_nca/non_nca_ratios.R
+++ inst/shiny/modules/tab_nca/non_nca_ratios.R
@@ -13,16 +13,19 @@ non_nca_ratio_ui <- function(id, title, select_label1, select_label2) {
       card_header(paste0(title, " Setup")),
       card_body(
         fluidRow(
-          column(6,
-                 actionButton(ns("add_pair"), "Add Ratio Pair", icon = icon("plus"), class = "add-pair-btn")
+          column(
+            6,
+            actionButton(ns("add_pair"), "Add Ratio Pair",
+                         icon = icon("plus"), class = "add-pair-btn")
           ),
-          column(6,
-                 selectInput(
-                   ns("summary_groups"),
-                   "Summarise By:",
-                   choices = NULL,
-                   multiple = TRUE
-                 )
+          column(
+            6,
+            selectInput(
+              ns("summary_groups"),
+              "Summarise By:",
+              choices = NULL,
+              multiple = TRUE
+            )
           )
         ),
         # Placeholder for dynamically added ratio pair selectors
@@ -89,7 +92,7 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
     observeEvent(input$add_pair, {
       count <- pair_counter() + 1
       pair_counter(count)
-      
+
       pair_id <- paste0("pair_", count)
       spec1_id <- paste0("spec1_", count)
       spec2_id <- paste0("spec2_", count)
@@ -100,16 +103,16 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
         id = ns(pair_id),
         selectInput(
           ns(paste0("spec1_", count)),
-           "Numerator(s)",
-           choices = spec_options(),
-           multiple = TRUE
-          ),
-          
+          "Numerator(s)",
+          choices = spec_options(),
+          multiple = TRUE
+        ),
+
         selectInput(
-         ns(paste0("spec2_", count)),
-         "Denominator(s)",
-         choices = spec_options(),
-         multiple = TRUE
+          ns(paste0("spec2_", count)),
+          "Denominator(s)",
+          choices = spec_options(),
+          multiple = TRUE
         ),
         div(
           class = "ratio-remove-btn-wrapper",
@@ -122,9 +125,13 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
         where = "beforeEnd",
         ui = ui
       )
-      
-      observeEvent(input[[spec1_id]], { selections[[spec1_id]] <- input[[spec1_id]] }, ignoreNULL = FALSE)
-      observeEvent(input[[spec2_id]], { selections[[spec2_id]] <- input[[spec2_id]] }, ignoreNULL = FALSE)
+
+      observeEvent(input[[spec1_id]], {
+        selections[[spec1_id]] <- input[[spec1_id]]
+      }, ignoreNULL = FALSE)
+      observeEvent(input[[spec2_id]], {
+        selections[[spec2_id]] <- input[[spec2_id]]
+      }, ignoreNULL = FALSE)
 
       # Observer to remove the UI for the specific pair
       observeEvent(input[[remove_id]], {
@@ -134,16 +141,16 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
       }, once = TRUE, ignoreInit = TRUE, autoDestroy = TRUE)
 
     })
-    
+
     results <- reactive({
       req(pair_counter() > 0)
       # Trigger this reactive when any selection changes
       reactiveValuesToList(selections)
-      
+
       all_ratios <- purrr::map_dfr(seq_len(pair_counter()), function(i) {
         spec1 <- selections[[paste0("spec1_", i)]]
         spec2 <- selections[[paste0("spec2_", i)]]
-        
+
         if (length(spec1) > 0 && length(spec2) > 0) {
           filtered_samples <- data()$conc$data %>% filter(PCSPEC %in% c(spec1, spec2))
           multiple_matrix_ratios(
@@ -155,13 +162,15 @@ non_nca_ratio_server <- function(id, data, grouping_vars) {
             spec1 = spec1,
             spec2 = spec2
           )
-        } else { NULL }
+        } else {
+          NULL
+        }
       })
-      
+
       if (nrow(all_ratios) == 0) return(NULL)
       return(all_ratios)
-    }) %>% debounce(500) # Wait 500ms after user stops changing inputs
-    
+    }) %>%
+      debounce(500) # Wait 500ms after user stops changing inputs
 
     full_output <- reactive({
       req(results())
