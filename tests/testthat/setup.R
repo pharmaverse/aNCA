000fb25b51bf3bf396e72c361296873016c8a484
Jana Spinner <spinnerjana@gmail.com>
2025-10-02 16:37:40 +0200

    lint

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -345,13 +345,13 @@ base::local({
   FIXTURE_PKNCA_DATA <<- suppressWarnings({
     PKNCA::PKNCAdata(
       data.conc = PKNCA::PKNCAconc(FIXTURE_CONC_DATA, AVAL ~ AFRLT | PCSPEC + USUBJID / PARAM,
-                                 concu = "AVALU", timeu = "RRLTU"),
+                                   concu = "AVALU", timeu = "RRLTU"),
       data.dose = PKNCA::PKNCAdose(FIXTURE_DOSE_DATA, DOSEA ~ AFRLT | USUBJID,
-                                 route = "ROUTE", duration = "ADOSEDUR"),
+                                   route = "ROUTE", duration = "ADOSEDUR"),
       units = units_table
-      )
+    )
   })
-  
+
   FIXTURE_PKNCA_DATA$intervals <<- FIXTURE_INTERVALS
 
   FIXTURE_PKNCA_DATA$options <<- list(keep_interval_cols = c("NCA_PROFILE",
