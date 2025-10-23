d25281c49816dc6d432b7969bd2b901dbe35a2d4
Gero1999 <gerardojoro@gmail.com>
2025-10-23 09:04:44 +0200

    test: adapt test to new plot ID

--- tests/testthat/test-utils-slope_selector.R
+++ tests/testthat/test-utils-slope_selector.R
@@ -237,14 +237,14 @@ describe("handle_interval_change", {
 describe("arrange_plots_by_groups", {
   it("orders plots by specified group columns", {
     named_list <- list(
-      "B: 2, A: 1" = "plot1",
-      "B: 1, A: 2" = "plot2",
-      "B: 1, A: 1" = "plot3"
+      "B=2_A=1" = "plot1",
+      "B=1_A=2" = "plot2",
+      "B=1_A=1" = "plot3"
     )
     ordered <- arrange_plots_by_groups(named_list, c("B", "A"))
-    expect_equal(names(ordered), c("B: 1, A: 1", "B: 1, A: 2", "B: 2, A: 1"))
+    expect_equal(names(ordered), c("B=1_A=1", "B=1_A=2", "B=2_A=1"))
     ordered2 <- arrange_plots_by_groups(named_list, c("A", "B"))
-    expect_equal(names(ordered2), c("B: 1, A: 1", "B: 2, A: 1", "B: 1, A: 2"))
+    expect_equal(names(ordered2), c("B=1_A=1", "B=2_A=1", "B=1_A=2"))
   })
 })
 
