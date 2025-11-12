7c251719e7909bddf64489aa868dca6d13f40cd5
Gero1999 <gerardojoro@gmail.com>
2025-11-12 13:50:29 +0100

    fix: typpos

--- data-raw/metadata_nca_variables.R
+++ data-raw/metadata_nca_variables.R
@@ -1,3 +1,3 @@
-# Load NCA parameters dataset
-metadata_nca_parameters <- read.csv("data-raw/metadata_nca_variables.csv")
+# Load NCA variables dataset
+metadata_nca_variables <- read.csv("data-raw/metadata_nca_variables.csv")
 usethis::use_data(metadata_nca_variables, overwrite = TRUE)
