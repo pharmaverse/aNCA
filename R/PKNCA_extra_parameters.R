924b017e42aa4275dcbe183bd14f370faddae9ba
Gero1999 <gerardojoro@gmail.com>
2025-12-01 13:56:17 +0100

    ignore lintr PKNCA_extra_parameters.R

--- R/PKNCA_extra_parameters.R
+++ R/PKNCA_extra_parameters.R
@@ -1,18 +1,20 @@
 
 
 ### Include Excretion Rate Parameters in the App
-add.interval.col <- function(name,
+add.interval.col <- function(name,  #nolint
                              FUN,
-                             values=c(FALSE, TRUE),
+                             values = c(FALSE, TRUE),
                              unit_type,
                              pretty_name,
-                             depends=NULL,
-                             desc="",
-                             sparse=FALSE,
-                             formalsmap=list(),
-                             datatype=c("interval",
-                                        "individual",
-                                        "population")) {
+                             depends = NULL,
+                             desc = "",
+                             sparse = FALSE,
+                             formalsmap = list(),
+                             datatype = c(
+                               "interval",
+                               "individual",
+                               "population"
+                             )) {
   # Check inputs
   if (!is.character(name)) {
     stop("name must be a character string")
@@ -29,25 +31,25 @@ add.interval.col <- function(name,
       stop("'depends' must be NULL or a character vector")
     }
   }
-  checkmate::assert_logical(sparse, any.missing=FALSE, len=1)
+  checkmate::assert_logical(sparse, any.missing = FALSE, len = 1)
   unit_type <-
     match.arg(
       unit_type,
-      choices=c(
+      choices = c(
         "unitless", "fraction", "%", "count",
         "time", "inverse_time",
         "amount", "amount_dose", "amount_time",
         "conc", "conc_dosenorm",
-        "dose", 
+        "dose",
         "volume",
         "auc", "aumc",
         "auc_dosenorm", "aumc_dosenorm",
         "clearance", "renal_clearance"
       )
     )
-  stopifnot("pretty_name must be a scalar"=length(pretty_name) == 1)
-  stopifnot("pretty_name must be a character"=is.character(pretty_name))
-  stopifnot("pretty_name must not be an empty string"=nchar(pretty_name) > 0)
+  stopifnot("pretty_name must be a scalar" = length(pretty_name) == 1)
+  stopifnot("pretty_name must be a character" = is.character(pretty_name))
+  stopifnot("pretty_name must not be an empty string" = nchar(pretty_name) > 0)
   datatype <- match.arg(datatype)
   if (!(datatype %in% "interval")) {
     stop("Only the 'interval' datatype is currently supported.")
@@ -59,11 +61,9 @@ add.interval.col <- function(name,
   }
   if (!is.list(formalsmap)) {
     stop("formalsmap must be a list")
-  } else if (length(formalsmap) > 0 &
-             is.null(names(formalsmap))) {
+  } else if (length(formalsmap) > 0 & is.null(names(formalsmap))) {
     stop("formalsmap must be a named list")
-  } else if (length(formalsmap) > 0 &
-             is.na(FUN)) {
+  } else if (length(formalsmap) > 0 & is.na(FUN)) {
     stop("formalsmap may not be given when FUN is NA.")
   } else if (!all(nchar(names(formalsmap)) > 0)) {
     stop("All formalsmap elements must be named")
@@ -71,10 +71,12 @@ add.interval.col <- function(name,
   # Ensure that the function exists
   if (!is.na(FUN) &&
       length(utils::getAnywhere(FUN)$objs) == 0) {
-    stop("The function named '", FUN, "' is not defined.  Please define the function before calling add.interval.col.")
+    stop(
+      "The function named '", FUN,
+      "' is not defined. Please define the function before calling add.interval.col."
+    )
   }
-  if (!is.na(FUN) &
-      length(formalsmap) > 0) {
+  if (!is.na(FUN) & length(formalsmap) > 0) {
     # Ensure that the formalsmap parameters are all in the list of
     # formal arguments to the function.
     if (!all(names(formalsmap) %in% names(formals(utils::getAnywhere(FUN)$objs[[1]])))) {
@@ -108,12 +110,12 @@ add.interval.col <- function(name,
 #' @return The midpoint collection time of the last measurable excretion rate, or NA/0 if not available
 #' @export
 pk.calc.ertlst <- function(conc, volume, time, duration.conc, check = TRUE) {
-  
+
   # Generate messages about missing concentrations/volumes
   message_all <- generate_missing_messages(conc, volume,
                                            name_a = "concentrations",
                                            name_b = "volumes")
-  
+
   if (all(is.na(conc))) {
     ret <- NA_real_
   } else if (all(conc %in% c(0, NA))) {
@@ -123,7 +125,7 @@ pk.calc.ertlst <- function(conc, volume, time, duration.conc, check = TRUE) {
     midtime <- time + duration.conc / 2
     ret <- max(midtime[!(conc %in% c(NA, 0))])
   }
-  
+
   if (length(message_all) != 0) {
     message <- paste(message_all, collapse = "; ")
     ret <- structure(ret, exclude = message)
@@ -155,19 +157,19 @@ PKNCA.set.summary(
 #' @return The maximum excretion rate, or NA if not available
 #' @export
 pk.calc.ermax <- function(conc, volume, time, duration.conc, check = TRUE) {
-  
+
   # Generate messages about missing concentrations/volumes
   message_all <- generate_missing_messages(conc, volume,
                                            name_a = "concentrations",
                                            name_b = "volumes")
-  
+
   if (length(conc) == 0 || all(is.na(conc))) {
     ret <- NA
   } else {
     er <- conc * volume / duration.conc
     ret <- max(er, na.rm=TRUE)
   }
-  
+
   if (length(message_all) != 0) {
     message <- paste(message_all, collapse = "; ")
     ret <- structure(ret, exclude = message)
@@ -198,13 +200,15 @@ PKNCA.set.summary(
 #' @param first.tmax If TRUE, return the first time of maximum excretion rate; otherwise, return the last
 #' @return The midpoint collection time of the maximum excretion rate, or NA if not available
 #' @export
-pk.calc.ertmax <- function(conc, volume, time, duration.conc, check = TRUE, first.tmax = NULL) {
-  
+pk.calc.ertmax <- function( #nolint
+    conc, volume, time, duration.conc, check = TRUE, first.tmax = NULL #nolint
+) {
+
   # Generate messages about missing concentrations/volumes
   message_all <- generate_missing_messages(conc, volume,
                                            name_a = "concentrations",
                                            name_b = "volumes")
-  
+
   if (length(conc) == 0 || all(conc %in% c(NA, 0))) {
     ret <- NA
   } else {
@@ -212,14 +216,14 @@ pk.calc.ertmax <- function(conc, volume, time, duration.conc, check = TRUE, firs
     ermax <- pk.calc.ermax(conc, volume, time, duration.conc, check = FALSE)
     midtime <- time + duration.conc / 2
     ret <- midtime[er %in% ermax]
-    
+
     if (first.tmax) {
       ret <- ret[1]
     } else {
       ret <- ret[length(ret)]
     }
   }
-  
+
   if (length(message_all) != 0) {
     message <- paste(message_all, collapse = "; ")
     ret <- structure(ret, exclude = message)
@@ -227,17 +231,19 @@ pk.calc.ertmax <- function(conc, volume, time, duration.conc, check = TRUE, firs
   ret
 }
 
-add.interval.col("ertmax",
-                 FUN="pk.calc.ertmax",
-                 unit_type="time",
-                 pretty_name="Tmax excretion rate",
-                 desc="The midpoint collection time of the maximum excretion rate (typically in urine or feces)")
+add.interval.col(
+  "ertmax",
+  FUN = "pk.calc.ertmax",
+  unit_type = "time",
+  pretty_name = "Tmax excretion rate",
+  desc = "The midpoint collection time of the maximum excretion rate (typically in urine or feces)"
+)
 
 PKNCA.set.summary(
-  name="ertmax",
-  description="median and range",
-  point=business.median,
-  spread=business.range
+  name = "ertmax",
+  description = "median and range",
+  point = business.median,
+  spread = business.range
 )
 
 
@@ -251,42 +257,42 @@ PKNCA.set.summary(
 generate_missing_messages <- function(a, b,
                                       name_a = deparse(substitute(a)),
                                       name_b = deparse(substitute(b))) {
-  
+
   mask_a <- is.na(a)
   mask_b <- is.na(b)
-  
+
   mask_both <- mask_a & mask_b
   mask_a_only <- mask_a & !mask_both
   mask_b_only <- mask_b & !mask_both
-  
+
   msg_both <- msg_a <- msg_b <- NA_character_
   n <- length(mask_a)
-  
+
   if (all(mask_both)) {
     msg_both <- sprintf("All %s and %s are missing", name_a, name_b)
   } else if (any(mask_both)) {
     msg_both <- sprintf("%g of %g %s and %s are missing", sum(mask_both), n, name_a, name_b)
   }
-  
+
   if (all(mask_a_only)) {
     msg_a <- sprintf("All %s are missing", name_a)
   } else if (any(mask_a_only)) {
     msg_a <- sprintf("%g of %g %s are missing", sum(mask_a_only), n, name_a)
   }
-  
+
   if (all(mask_b_only)) {
     msg_b <- sprintf("All %s are missing", name_b)
   } else if (any(mask_b_only)) {
     msg_b <- sprintf("%g of %g %s are missing", sum(mask_b_only), n, name_b)
   }
-  
+
   # Return non-NA messages
   stats::na.omit(c(msg_both, msg_a, msg_b))
 }
 
 
 
-###########################################################################################################
+####################################################################################
 
 #' Calculate the total urine volume
 #'
@@ -312,7 +318,7 @@ PKNCA::PKNCA.set.summary(
   spread = PKNCA::business.geocv
 )
 
-###########################################################################################################
+################################################################################
 
 
 # Define column interval for fraction excreted (aNCA: #669, PKNCA: #473)
@@ -325,4 +331,3 @@ PKNCA::add.interval.col(
   depends = "ae",
   desc = "The fraction of the dose excreted"
 )
-
