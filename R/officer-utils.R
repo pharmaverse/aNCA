d9feed4ca4811aaaf46f426a98697b00cdd15c50
Jana Spinner <spinnerjana@gmail.com>
2025-12-10 14:51:16 +0100

    nitpicks: rename a_par to title_formatted

--- R/officer-utils.R
+++ R/officer-utils.R
@@ -52,7 +52,7 @@ add_pptx_sl_table <- function(pptx, df, title = "",
                               subtitle = "",
                               footer = "Click here for individual results") {
 
-  a_par <- fpar(
+  title_formatted <- fpar(
     ftext(title),
     "\n",
     ftext(subtitle, prop = fp_text(font.size = 12)),
@@ -61,12 +61,11 @@ add_pptx_sl_table <- function(pptx, df, title = "",
 
   # Set flextable to autofit and center for better appearance
   ft <- flextable::flextable(df) %>%
-    flextable::autofit() %>%
-    flextable::set_table_properties(align = "center")
+    flextable::autofit()
 
   add_slide(pptx, layout = "Title Only") %>%
     ph_with(value = ft, location = "Table Placeholder 1") %>%
-    ph_with(value = a_par, location = "Title 1") %>%
+    ph_with(value = title_formatted, location = "Title 1") %>%
     ph_with(value = footer, location = "Footer Placeholder 3")
 }
 
