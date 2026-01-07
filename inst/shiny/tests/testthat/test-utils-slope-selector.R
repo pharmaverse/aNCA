4bb5b657d097f43e217fcb84cceec5bcbed9d710
Gero1999 <gerardojoro@gmail.com>
2026-01-07 09:24:21 +0100

    refactor: lintr

--- inst/shiny/tests/testthat/test-utils-slope-selector.R
+++ inst/shiny/tests/testthat/test-utils-slope-selector.R
@@ -18,9 +18,9 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "1:3"
     )
-    
+
     expect_equal(nrow(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)), 2)
-    
+
     # different USUBJID #
     NEW <- data.frame(
       TYPE = "Exclusion",
@@ -30,9 +30,9 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "1:3"
     )
-    
+
     expect_equal(nrow(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)), 2)
-    
+
     # different ATPTREF #
     NEW <- data.frame(
       TYPE = "Exclusion",
@@ -42,11 +42,10 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "1:3"
     )
-    
+
     expect_equal(nrow(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)), 2)
-    
   })
-  
+
   it("should remove overlapping points if no new points are detected", {
     NEW <- data.frame(
       TYPE = "Exclusion",
@@ -56,9 +55,9 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "4:5"
     )
-    
+
     expect_equal(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)$RANGE, "3,6")
-    
+
     NEW <- data.frame(
       TYPE = "Exclusion",
       USUBJID = 1,
@@ -67,11 +66,10 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "3:4"
     )
-    
+
     expect_equal(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)$RANGE, "5:6")
-    
   })
-  
+
   it("should add new points if partial overlap is detected", {
     NEW <- data.frame(
       TYPE = "Exclusion",
@@ -81,11 +79,10 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "4:9"
     )
-    
+
     expect_equal(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)$RANGE, "3:9")
-    
   })
-  
+
   it("should remove full row if full range of rule is removed", {
     NEW <- data.frame(
       TYPE = "Exclusion",
@@ -95,11 +92,10 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "3:6"
     )
-    
+
     expect_equal(nrow(check_slope_rule_overlap(EXISTING_FIXTURE, NEW)), 0)
-    
   })
-  
+
   it("should warn if more than one range for single subject, profile and rule type is detected", {
     EXISTING <- data.frame(
       TYPE = "Exclusion",
@@ -109,12 +105,12 @@ describe("check_slope_rule_overlap", {
       PCSPEC = 1,
       RANGE = "3:6"
     )
-    
+
     DUPLICATE <- EXISTING %>%
       mutate(
         RANGE = "4:7"
       )
-    
+
     expect_warning(
       check_slope_rule_overlap(rbind(EXISTING, DUPLICATE), DUPLICATE),
       "More than one range for single subject, profile and rule type detected."
@@ -123,36 +119,35 @@ describe("check_slope_rule_overlap", {
 })
 
 describe("detect_pknca_data_changes", {
-  
   old_data <- FIXTURE_PKNCA_DATA
-  
+
   it("detects change in data when there is no previous object", {
     new_data <- old_data
     res <- detect_pknca_data_changes(NULL, new_data)
     expect_true(res$in_data)
   })
-  
+
   it("detects no change when old and new are identical", {
     res <- detect_pknca_data_changes(old_data, old_data)
     expect_false(res$in_data)
     expect_false(res$in_hl_adj)
     expect_false(res$in_selected_intervals)
   })
-  
+
   it("detects change in data", {
     changed_data <- old_data
     changed_data$conc$data$VAL[1] <- 99
     res <- detect_pknca_data_changes(old_data, changed_data)
     expect_true(res$in_data)
   })
-  
+
   it("detects change in hl_adj", {
     changed_hl <- old_data
     changed_hl$conc$data$exclude_half.life[2] <- TRUE
     res <- detect_pknca_data_changes(old_data, changed_hl)
     expect_true(res$in_hl_adj)
   })
-  
+
   it("detects change in intervals", {
     changed_int <- old_data
     changed_int$intervals <- data.frame(ID = 1:4)
@@ -174,7 +169,7 @@ describe("handle_hl_adj_change", {
           exclude_half.life
         )
       )
-    
+
     # Create plots using the original and updated PKNCA data
     old_plots <- withCallingHandlers(
       get_halflife_plots(old_data)$plots,
@@ -184,11 +179,11 @@ describe("handle_hl_adj_change", {
       }
     )
     new_plots <- handle_hl_adj_change(new_data, old_data, old_plots)
-    
+
     # Check that the plots for other groups remain unchanged
     ix_unchanged_plots <- setdiff(seq_along(new_plots), 4)
     expect_equal(new_plots[ix_unchanged_plots], old_plots[ix_unchanged_plots])
-    
+
     # Define the expected differences in the original and updated plots
     old_plots_exp_details <- list(
       color = c("red", "red", "green", "green", "green"),
@@ -213,22 +208,22 @@ describe("handle_interval_change", {
   old_data <- FIXTURE_PKNCA_DATA
   old_data$intervals <- old_data$intervals[1:3, ]
   old_plots <- get_halflife_plots(old_data)$plots
-  
+
   it("removes plots when intervals are removed", {
     new_data <- FIXTURE_PKNCA_DATA
     new_data$intervals <- new_data$intervals[1, ]
     new_plots <- handle_interval_change(new_data, old_data, old_plots)
-    
+
     expect_equal(length(old_plots), 3)
     expect_equal(length(new_plots), 1)
     expect_equal(old_plots[names(new_plots)], new_plots)
   })
-  
+
   it("adds plots when intervals are added", {
     new_data <- FIXTURE_PKNCA_DATA
     new_data$intervals <- new_data$intervals[1:4, ]
     new_plots <- handle_interval_change(new_data, old_data, old_plots)
-    
+
     expect_equal(length(old_plots), 3)
     expect_equal(length(new_plots), 4)
     expect_equal(new_plots[names(old_plots)], old_plots)
@@ -253,10 +248,10 @@ describe("update_plots_with_pknca", {
   old_data <- FIXTURE_PKNCA_DATA
   old_data$intervals <- old_data$intervals[1:2, ]
   old_plots <- get_halflife_plots(old_data)$plots
-  
+
   new_data <- old_data
   new_data$conc$data$exclude_half.life <- TRUE
-  
+
   it("updates all plots when no intervals are specified", {
     updated_plots <- update_plots_with_pknca(new_data, old_plots, NULL)
     expect_equal(updated_plots[[1]]$x$data[[2]]$marker$symbol, rep("x", 5), ignore_attr = TRUE)
