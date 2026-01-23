6c034cc0d90a5167d903e6286017276616ee444a
Jana Spinner <spinnerjana@gmail.com>
2026-01-23 10:49:56 +0100

    docs and lint

--- R/apply_results_flags.R
+++ R/apply_results_flags.R
@@ -5,8 +5,10 @@
 #' parameters that were requested (based on intervals), applies threshold rules to the `Exclude`
 #' column, and determines the final `flagged` status (ACCEPTED, FLAGGED, or MISSING).
 #'
-#' @param data A data frame. The pivot-wider NCA results containing subject data from `PKNCAresults` object.
-#' @param nca_intervals A data frame. The intervals object from the PKNCA result (e.g., `res$data$intervals`).
+#' @param data A data frame. NCA results from the `pivot_wider_pknca_results` function,
+#' with additional grouping variables merged.
+#' @param nca_intervals A data frame. The intervals object from the
+#'  PKNCA result (e.g., `res$data$intervals`).
 #' @param group_cols A character vector. The column names used for grouping the data.
 #' @param flag_settings A named list of flag settings. Each element must contain
 #'   `is.checked` (logical) and `threshold` (numeric or character).
@@ -20,28 +22,28 @@
 #' @importFrom formatters var_labels
 #' @export
 apply_results_flags <- function(data, nca_intervals, group_cols, flag_settings) {
-  
+
   #Initialise flagged column
   flagged_data <- data %>%
-  mutate(
-    flagged = "NOT DONE"
-  )
+    mutate(
+      flagged = "NOT DONE"
+    )
 
   # Add flagging column in the pivoted results
   applied_flags <- purrr::keep(flag_settings, function(x) x$is.checked)
   flag_params <- names(applied_flags)
   flag_params_pknca <- translate_terms(flag_params, "PPTESTCD", "PKNCA")
-  
+
   flag_thr <- sapply(flag_settings, FUN =  function(x) x$threshold)
   flag_rule_msgs <- c(paste0(names(flag_settings), c(" < ", " < ", " > ", " > ", " < "), flag_thr))
-  
+
   valid_indices <- map_lgl(flag_params, function(p) {
     any(grepl(paste0("^", p, "(\\[|$)"), names(flagged_data)))
   })
-  
+
   flag_cols <- names(flagged_data)[formatters::var_labels(flagged_data)
-                                    %in% translate_terms(flag_params, "PPTESTCD", "PPTEST")]
-  
+                                   %in% translate_terms(flag_params, "PPTESTCD", "PPTEST")]
+
   if (length(flag_cols) > 0) {
     requested_flags <- nca_intervals %>%
       select(any_of(c(
@@ -51,19 +53,22 @@ apply_results_flags <- function(data, nca_intervals, group_cols, flag_settings) 
       group_by(across(any_of(group_cols))) %>%
       # Collapse duplicates: if any row is TRUE, the result is TRUE
       summarise(across(any_of(flag_params_pknca), any), .groups = "drop")
-    
+
     flagged_data <- flagged_data %>%
       left_join(requested_flags, by = intersect(names(.), names(requested_flags))) %>%
       rowwise() %>%
       mutate(
-        na_msg_vec = list(purrr::map2_chr(flag_cols, flag_params_pknca[valid_indices], function(d_col, f_col) {
-          if (!is.na(d_col) && is.na(get(d_col)) && isTRUE(get(f_col))) {
-            # Return "Param is NA"
-            paste(str_remove(d_col, "\\[.*\\]"), "is NA")
-          } else {
-            NA_character_
-          }
-        }) %>% na.omit()),
+        na_msg_vec = list(
+          purrr::map2_chr(flag_cols, flag_params_pknca[valid_indices],
+                          function(d_col, f_col) {
+                            if (!is.na(d_col) && is.na(get(d_col)) && isTRUE(get(f_col))) {
+                              # Return "Param is NA"
+                              paste(str_remove(d_col, "\\[.*\\]"), "is NA")
+                            } else {
+                              NA_character_
+                            }
+                          }) %>% na.omit()
+        ),
         na_msg = paste(na_msg_vec, collapse = "; "),
         # Update Exclude using case_when
         Exclude = case_when(
@@ -83,6 +88,6 @@ apply_results_flags <- function(data, nca_intervals, group_cols, flag_settings) 
       ungroup() %>%
       select(-all_of(c(flag_params_pknca, "na_msg_vec", "na_msg")))
   }
-  
+
   flagged_data
 }
