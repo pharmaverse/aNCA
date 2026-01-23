9a53376a58482d0cc408df12956c4b9aa2ba24f8
Gero1999 <gerardojoro@gmail.com>
2026-01-23 16:40:55 +0100

    create tests check_valid_pknca_data

--- tests/testthat/test-PKNCA.R
+++ tests/testthat/test-PKNCA.R
@@ -380,6 +380,36 @@ describe("select_level_grouping_cols", {
   })
 })
 
+describe("check_valid_pknca_data", {
+  pknca_data <- FIXTURE_PKNCA_DATA
+  
+  it("returns the input object if no issues are found", {
+    # Without exclusions for half-life
+    result <- check_valid_pknca_data(pknca_data)
+    expect_identical(result, pknca_data)
+  })
+  
+  # Make checks for half-life exclusions ----
+  pknca_data_with_excl <- pknca_data
+  excl_hl_col <- pknca_data_with_excl$conc$columns$exclude_half.life
+  pknca_data_with_excl$conc$data[1, excl_hl_col] <- TRUE
+  
+  it("does not throw an error if exclusions for half-life include a REASON value", {
+    pknca_data_with_excl$conc$data$REASON <- "Test reason"
+    expect_no_error(
+      check_valid_pknca_data(pknca_data_with_excl, exclusions_have_reasons = TRUE)
+    )
+  })
+  
+  it("throws an error if exclusions for half-life do not include a REASON value", {
+    pknca_data_with_excl$conc$data$REASON <- ""
+    expect_error(
+      check_valid_pknca_data(pknca_data_with_excl, exclusions_have_reasons = TRUE),
+      "No reason provided for the following half-life exclusions:"
+    )
+  })
+})
+
 # Tests for add_exclusion_reasons
 describe("add_exclusion_reasons", {
   it("adds a single exclusion reason to specified rows", {
