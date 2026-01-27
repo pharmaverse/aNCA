c73b6e3dff81a6c5865cb672672371079ed85b61
Gero1999 <gerardojoro@gmail.com>
2026-01-27 16:10:50 +0100

    fix: manual slopes not appearing in results

--- inst/shiny/modules/tab_nca.R
+++ inst/shiny/modules/tab_nca.R
@@ -98,7 +98,7 @@ tab_nca_server <- function(id, pknca_data, extra_group_vars, settings_override) 
     session$userData$ratio_table <- ratio_table
     session$userData$slope_rules <- slope_rules
 
-    reactable_server("manual_slopes", slope_rules$manual_slopes)
+    reactable_server(id = "manual_slopes", data =  reactive(slope_rules()$manual_slopes))
 
     # List all irrelevant warnings to suppres in the NCA calculation
     irrelevant_regex_warnings <- c(
