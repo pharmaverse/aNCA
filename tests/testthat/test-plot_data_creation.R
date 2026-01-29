9ea3ca302497c370f0ec21ee91097353b4799718
Gero1999 <gerardojoro@gmail.com>
2026-01-29 17:24:05 +0100

    simplify test reading removing filtering_list where not needed

--- tests/testthat/test-plot_data_creation.R
+++ tests/testthat/test-plot_data_creation.R
@@ -9,81 +9,54 @@ d_data3 <- d_data %>%
 pknca_data$conc$data <- bind_rows(d_data, d_data2, d_data3)
 conc_data <- pknca_data$conc$data
 
-filtering_list <- list(
-  USUBJID = unique(pknca_data$conc$data$USUBJID)[1],
-  PARAM = unique(pknca_data$conc$data$PARAM)[1],
-  PCSPEC = unique(pknca_data$conc$data$PCSPEC)[1],
-  ATPTREF = unique(pknca_data$conc$data$ATPTREF)[1]
-)
-
 analytes <- unique(d_data$PARAM)
 pcspecs <- unique(d_data$PCSPEC)
 subjects <- unique(d_data$USUBJID)
 
 describe("process_data_individual functions correctly", {
+  conc_col <- pknca_data$conc$columns$concentration
+  default_filter <- list(
+    USUBJID = pknca_data$USUBJID[1],
+    PARAM = pknca_data$PARAM[1],
+    PCSPEC = pknca_data$PCSPEC[1],
+    ATPTREF = pknca_data$ATPTREF[1]
+  )
 
   it("returns a dataframe with default settings", {
-    res <- process_data_individual(
-      pknca_data = pknca_data
-    )
-    # expect data frame output
+    res <- process_data_individual(pknca_data = pknca_data)
     expect_true(is.data.frame(res))
   })
 
   it("handles missing columns gracefully", {
-    conc_col <- pknca_data$conc$columns$concentration
     data_no_conc_col <- pknca_data
     data_no_conc_col$conc$data <- select(conc_data, -all_of(conc_col))
     expect_error(
-      process_data_individual(
-        pknca_data = data_no_conc_col
-      ),
+      process_data_individual(pknca_data = data_no_conc_col),
       "object 'AVAL' not found"
     )
   })
 
-  it("filters out EVID != 0 and NA concentration", {
-    conc_col <- pknca_data$conc$columns$concentration
-    conc_data_with_na_evid <- conc_data
-    conc_data_with_na_evid$EVID <- c(0, rep(1, nrow(conc_data) - 1))
-    conc_data_with_na_evid[[conc_col]][c(2,3)] <- NA
+  it("filters out NA concentration", {
+    conc_data_with_na <- conc_data
+    conc_data_with_na$EVID <- c(0, rep(1, nrow(conc_data) - 1))
+    conc_data_with_na[[conc_col]][c(2,3)] <- NA
     pknca_data_with_na_evid <- pknca_data
-    pknca_data_with_na_evid$conc$data <- conc_data_with_na_evid
-
-    p <- process_data_individual(
-      pknca_data = pknca_data_with_na_evid,
-      filtering_list = list(
-        USUBJID = pknca_data$USUBJID[1],
-        PARAM = pknca_data$PARAM[1],
-        PCSPEC = pknca_data$PCSPEC[1]
-      )
-    )
-    expect_true(all(p$EVID == 0))
+    pknca_data_with_na_evid$conc$data <- conc_data_with_na
+    p <- process_data_individual(pknca_data = pknca_data_with_na_evid)
     expect_true(all(!is.na(p[[conc_col]])))
   })
 
   it("filters data according to filtering_list", {
     p <- process_data_individual(
       pknca_data = pknca_data,
-      filtering_list = list(
-        ATPTREF = pknca_data$ATPTREF[1]
-      )
+      filtering_list = list(ATPTREF = pknca_data$ATPTREF[1])
     )
     expect_true(all(p$ATPTREF == 1))
   })
 
   it("handles predose duplication if filtering_list is used", {
-    p <- process_data_individual(
-      data = pknca_data,
-      filtering_list = list(
-        USUBJID = pknca_data$USUBJID[1],
-        PARAM = pknca_data$PARAM[1],
-        PCSPEC = pknca_data$PCSPEC[1],
-        ATPTREF = pknca_data$ATPTREF[1]
-      )
-    )
-    predose_record_in_plot <- p %>%
-      filter(NFRLT == 168)
+    p <- process_data_individual(data = pknca_data, filtering_list = default_filter)
+    predose_record_in_plot <- p %>% filter(NFRLT == 168)
     expect_true(nrow(predose_record_in_plot) == 1)
     expect_true(predose_record_in_plot$ATPTREF == pknca_data$ATPTREF[1])
   })
@@ -93,7 +66,6 @@ describe("process_data_individual functions correctly", {
     aval_zeros$AVAL[c(1,2)] <- c(0, 0)
     pknca_aval_zeros <- pknca_data
     pknca_aval_zeros$conc$data <- aval_zeros
-
     p <- process_data_individual(
       pknca_data = pknca_aval_zeros,
       ylog_scale = TRUE
@@ -103,14 +75,13 @@ describe("process_data_individual functions correctly", {
 })
 
 describe("process_data_mean functions correctly", {
+  filter_param_pcspec <- list(PARAM = pknca_data$PARAM[1], PCSPEC = pknca_data$PCSPEC[1])
+  filter_all <- list(PARAM = conc_data$PARAM[1], PCSPEC = conc_data$PCSPEC[1], ATPTREF = conc_data$ATPTREF[1])
 
   it("returns a dataframe with default settings", {
     p <- process_data_mean(
       pknca_data = pknca_data,
-      filtering_list = list(
-        PARAM = pknca_data$PARAM[1],
-        PCSPEC = pknca_data$PCSPEC[1]
-      ),
+      filtering_list = filter_param_pcspec,
       extra_grouping_vars = "ATPTREF"
     )
     expect_true("Mean" %in% names(p))
@@ -124,10 +95,7 @@ describe("process_data_mean functions correctly", {
     expect_error(
       process_data_mean(
         pknca_data = pknca_incomplete,
-        filtering_list = list(
-          PARAM = pknca_data$PARAM[1],
-          PCSPEC = pknca_data$PCSPEC[1]
-        ),
+        filtering_list = filter_param_pcspec,
         extra_grouping_vars = "ATPTREF"
       ),
       "object 'AVAL' not found"
@@ -137,11 +105,7 @@ describe("process_data_mean functions correctly", {
   it("filters data if filtering_list is used", {
     p <- process_data_mean(
       pknca_data = pknca_data,
-      filtering_list = list(
-        PARAM = conc_data$PARAM[1],
-        PCSPEC = conc_data$PCSPEC[1],
-        ATPTREF = conc_data$ATPTREF[1]
-      ),
+      filtering_list = filter_all,
       extra_grouping_vars = c("ATPTREF", "PARAM", "PCSPEC")
     )
     expect_true(all(p$ATPTREF == conc_data$ATPTREF[1]))
@@ -156,7 +120,7 @@ describe("process_data_mean functions correctly", {
         PARAM = c(conc_data$PARAM[1], conc_data$PARAM[2]),
         PCSPEC = conc_data$PCSPEC[1]
       ),
-      extra_grouping_vars = c("DOSETRT", "PCSPEC", "PARAM"),
+      extra_grouping_vars = c("DOSETRT", "PCSPEC", "PARAM")
     )
     expect_true("DOSETRT" %in% names(p))
     expect_true("PCSPEC" %in% names(p))
@@ -164,8 +128,7 @@ describe("process_data_mean functions correctly", {
   })
 
   it("filters non-positive Mean for log scale", {
-    zero_data <- conc_data %>%
-      mutate(AVAL = ifelse(USUBJID %in% unique(USUBJID)[1:2], 0, AVAL))
+    zero_data <- conc_data %>% mutate(AVAL = ifelse(USUBJID %in% unique(USUBJID)[1:2], 0, AVAL))
     pknca_zero_data <- pknca_data
     pknca_zero_data$conc$data <- zero_data
     p <- process_data_mean(
@@ -198,12 +161,7 @@ describe("exploration_individualplot: Individual Plot Mode", {
     p <- exploration_individualplot(
       pknca_data = pknca_data,
       color_by = "PARAM",
-      facet_by = "PARAM",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1],
-        USUBJID = subjects[1]
-      )
+      facet_by = "PARAM"
     )
     expect_s3_class(p$facet, "FacetWrap")
   })
@@ -222,12 +180,7 @@ describe("exploration_individualplot: Individual Plot Mode", {
     p <- exploration_individualplot(
       pknca_data = pknca_data,
       color_by = "PARAM",
-      threshold_value = 0.1,
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1],
-        USUBJID = subjects[1]
-      )
+      threshold_value = 0.1
     )
     layer_classes <- sapply(p$layers, function(x) class(x$geom)[1])
     expect_true("GeomHline" %in% layer_classes)
@@ -238,12 +191,7 @@ describe("exploration_individualplot: Individual Plot Mode", {
     p <- exploration_individualplot(
       pknca_data = pknca_data,
       color_by = "PARAM",
-      palette = "viridis",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1],
-        USUBJID = subjects[1]
-      )
+      palette = "viridis"
     )
     p_build <- ggplot_build(p)
     plot_colors <- unique(p_build$data[[1]]$colour)
@@ -255,12 +203,7 @@ describe("exploration_individualplot: Individual Plot Mode", {
     empty_data$conc$data <- empty_data$conc$data[0, ]
     p <- exploration_individualplot(
       pknca_data = empty_data,
-      color_by = "PARAM",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1],
-        USUBJID = subjects[1]
-      )
+      color_by = "PARAM"
     )
     expect_s3_class(p, "ggplot")
     expect_equal(p$labels$title, "Error")
@@ -272,13 +215,7 @@ describe("exploration_individualplot: Individual Plot Mode", {
     p <- exploration_individualplot(
       pknca_data = pknca_data,
       color_by = "PARAM",
-      show_dose = TRUE,
-      facet_by = "PARAM",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1],
-        USUBJID = subjects[1]
-      )
+      show_dose = TRUE
     )
     layer_classes <- sapply(p$layers, function(x) class(x$geom)[1])
     expect_true("GeomVline" %in% layer_classes)
@@ -314,11 +251,7 @@ describe("exploration_meanplot: Mean Plot Mode", {
   it("returns a ggplot object with mean labels", {
     p <- exploration_meanplot(
       pknca_data = pknca_data,
-      color_by = "PARAM",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1]
-      )
+      color_by = "PARAM"
     )
     expect_s3_class(p, "ggplot")
     expect_true(grepl("Mean", p$labels$title))
@@ -332,11 +265,7 @@ describe("exploration_meanplot: Mean Plot Mode", {
       pknca_data = pknca_data,
       color_by = "PARAM",
       sd_min = TRUE,
-      sd_max = TRUE,
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1]
-      )
+      sd_max = TRUE
     )
     p_both_build <- ggplot_build(p_both)
     layer_classes <- sapply(p_both$layers, function(x) class(x$geom)[1])
@@ -347,11 +276,7 @@ describe("exploration_meanplot: Mean Plot Mode", {
     p <- exploration_meanplot(
       pknca_data = pknca_data,
       color_by = "PARAM",
-      ci = TRUE,
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1]
-      )
+      ci = TRUE
     )
     layer_classes <- sapply(p$layers, function(x) class(x$geom)[1])
     expect_true("GeomRibbon" %in% layer_classes)
@@ -364,11 +289,7 @@ describe("exploration_meanplot: Mean Plot Mode", {
       color_by = "PARAM",
       sd_min = TRUE,
       sd_max = TRUE,
-      ci = TRUE,
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1]
-      )
+      ci = TRUE
     )
     layer_classes <- sapply(p$layers, function(x) class(x$geom)[1])
     expect_true("GeomErrorbar" %in% layer_classes)
@@ -380,11 +301,7 @@ describe("exploration_meanplot: Mean Plot Mode", {
       pknca_data = pknca_data,
       color_by = "PARAM",
       show_dose = TRUE,
-      facet_by = "PARAM",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1]
-      )
+      facet_by = "PARAM"
     )
     layer_classes <- sapply(p$layers, function(x) class(x$geom)[1])
     expect_true("GeomVline" %in% layer_classes)
@@ -398,11 +315,7 @@ describe("exploration_meanplot: Mean Plot Mode", {
     empty_data$conc$data <- empty_data$conc$data[0, ]
     p <- exploration_meanplot(
       pknca_data = empty_data,
-      color_by = "PARAM",
-      filtering_list = list(
-        PARAM = analytes[1],
-        PCSPEC = pcspecs[1]
-      )
+      color_by = "PARAM"
     )
     expect_s3_class(p, "ggplot")
     expect_equal(p$labels$title, "Error")
