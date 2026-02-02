bdc505a6610f68b068015f190b3e9fe3feb8f874
Gero1999 <gerardojoro@gmail.com>
2026-02-02 14:05:30 +0100

    add test for read_setts, case of settings$parameters$types_df

--- tests/testthat/test-readers.R
+++ tests/testthat/test-readers.R
@@ -120,8 +120,13 @@ describe("read_settings", {
   })
 
   it("converts settings elements into data frames", {
+    # Partial AUC definitions
     res <- read_settings(path)
     expect_s3_class(res$settings$partial_aucs, "data.frame")
+    
+    # Parameter selections by study type
+    expect_s3_class(res$settings$parameters$types_df, "data.frame")
+    expect_equal(nrow(res$settings$parameters$types_df), 3)
   })
 
   it("handles empty or null units/general_exclusions gracefully", {
