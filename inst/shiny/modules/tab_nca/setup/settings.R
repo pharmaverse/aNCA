92a5e00554362aafbaa06c02458a97808ec62091
Jana Spinner <spinnerjana@gmail.com>
2026-02-17 18:06:13 +0100

    refactor: move check for settings analyte and check for other settings for setup widget updates

--- inst/shiny/modules/tab_nca/setup/settings.R
+++ inst/shiny/modules/tab_nca/setup/settings.R
@@ -140,8 +140,8 @@ settings_server <- function(id, data, adnca_data, settings_override) {
       selected <- choices
       not_compatible <- c()
 
-      # if settings (specifically analyte) exist, update analyte picker input and check compatibility
-      if (!is.null(settings) && !is.null(settings$analyte)) {
+      # if settings exist, update analyte picker input and check compatibility
+      if (!is.null(settings)) {
 
         if (all(settings$analyte %in% choices)) {
           selected <- settings$analyte
@@ -160,16 +160,19 @@ settings_server <- function(id, data, adnca_data, settings_override) {
         # Additional settings (PCSPEC and ATPTREF handled later)
         updateSelectInput(session, inputId = "method", selected = settings$method)
 
-        if (!is.null(settings$bioavailability))
+        if (!is.null(settings$bioavailability)) {
           updateSelectInput(session, inputId = "bioavailability",
                             selected = settings$bioavailability)
+        }
 
         # Data imputation #
         update_switch("should_impute_c0", value = settings$data_imputation$impute_c0)
 
         # Partial AUCs #
-        auc_data(settings$partial_aucs)
-        refresh_reactable(refresh_reactable() + 1)
+        if(!is.null(settings$partial_aucs)) {
+          auc_data(settings$partial_aucs)
+          refresh_reactable(refresh_reactable() + 1)
+        }
 
         # Flags #
         .update_rule_input(
@@ -208,7 +211,7 @@ settings_server <- function(id, data, adnca_data, settings_override) {
         )
       }
 
-      if (length(not_compatible) > 0) {
+      if (!is.null(settings$analyte) && length(not_compatible) > 0) {
         msg <- paste0(paste0(not_compatible, collapse = ", "),
                       " settings not found in data. Reverting to defaults.")
         log_warn(msg)
