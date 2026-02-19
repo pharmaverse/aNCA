eeef7ef32341c740d8f5ca6b741e9e18ed8a2353
Gero1999 <gerardojoro@gmail.com>
2026-02-19 09:10:35 +0100

    specify NA strings in adnca_example loaded

--- data-raw/adnca_example.R
+++ data-raw/adnca_example.R
@@ -1,3 +1,3 @@
 # Load NCA parameters dataset
-adnca_example <- read.csv("data-raw/metadata_nca_parameters.csv")
+adnca_example <- read.csv("data-raw/metadata_nca_parameters.csv", na.strings = c("", "NA"))
 usethis::use_data(adnca_example, overwrite = TRUE)
