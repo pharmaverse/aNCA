b892f4235d273b2340e075a5e3d882b6b109e72d
Valentin Legras <val35131@gmail.com>
2026-02-19 10:03:00 +0000

    Select all available color_by vars for mean plots, add COHORT/PART/PERIOD to grouping
    - Mean plot default color_by now selects all available variables from the
      priority list, not just the first one
    - Add COHORT, PART, PERIOD to Grouping_Variables mapping_alternatives so
      they are auto-mapped when present in the dataset
    
    Co-authored-by: Ona <no-reply@ona.com>

--- inst/shiny/modules/tab_data/data_mapping.R
+++ inst/shiny/modules/tab_data/data_mapping.R
@@ -13,7 +13,7 @@ NON_STD_MAPPING_INFO <- data.frame(
   ),
   mapping_section = c("Supplemental Variables", "Sample Variables"),
   mapping_alternatives = c(
-    "TRTA, TRTAN, ACTARM, TRT01A, TRT01P, RACE, SEX, GROUP, STRAIN, DOSFRM, NOMDOSE, DOSEP",
+    "TRTA, TRTAN, ACTARM, TRT01A, TRT01P, RACE, SEX, GROUP, STRAIN, DOSFRM, NOMDOSE, DOSEP, COHORT, PART, PERIOD",
     ""
   ),
   is_multiple_choice = c(TRUE, TRUE),
--- inst/shiny/modules/tab_explore/plot_sidebar.R
+++ inst/shiny/modules/tab_explore/plot_sidebar.R
@@ -192,15 +192,15 @@ plot_sidebar_server <- function(id, pknca_data, grouping_vars) {
 
       is_individual <- "usubjid" %in% names(input)
 
-      # Default color_by: USUBJID for individual plots, first available
-      # grouping variable from the priority list for mean plots
+      # Default color_by: USUBJID for individual plots, all available
+      # variables from the priority list for mean plots
       default_color <- if (is_individual) {
         subject_col
       } else {
         color_priority <- c("DOSETRT", "PCSPEC", "DOSEA", "DOSEU", "TRT01A",
                             "GROUP", "ACTARM", "COHORT", "PERIOD", "PART")
         available_color <- intersect(color_priority, full_grouping_vars)
-        if (length(available_color) > 0) available_color[1] else dose_col
+        if (length(available_color) > 0) available_color else dose_col
       }
 
       updatePickerInput(
