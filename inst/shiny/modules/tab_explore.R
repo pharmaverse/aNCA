be4736ad695c6fafe3840c02f27912b7c6193e02
Valentin Legras <val35131@gmail.com>
2026-02-19 13:25:38 +0000

    Revert sidebar width changes
    Co-authored-by: Ona <no-reply@ona.com>

--- inst/shiny/modules/tab_explore.R
+++ inst/shiny/modules/tab_explore.R
@@ -5,7 +5,6 @@
 tab_explore_ui <- function(id) {
   ns <- NS(id)
 
-  div(class = "exploration-plots",
   navset_card_pill(
     id = "visuals",
     height = "90vh",
@@ -30,7 +29,6 @@ tab_explore_ui <- function(id) {
       "PK/Dose QC Plot",
       pk_dose_qc_plot_ui(ns("pk_dose_qc_plot"))
     )
-  )
   )
 }
 
