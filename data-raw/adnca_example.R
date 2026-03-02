06820b3005ea886c0489390034a88e7f2afe2cc2
Gerardo J. Rodríguez <68994823+Gero1999@users.noreply.github.com>
2026-03-02 15:58:44 +0100

    Update data-raw/adnca_example.R
    Co-authored-by: Valentin Legras <93440714+Shaakon35@users.noreply.github.com>

--- data-raw/adnca_example.R
+++ data-raw/adnca_example.R
@@ -1,3 +1,3 @@
-# Load NCA parameters dataset
+# Build adnca_example dataset from CSV
 adnca_example <- read.csv("data-raw/adnca_example.csv", na.strings = c("", "NA"))
 usethis::use_data(adnca_example, overwrite = TRUE)
