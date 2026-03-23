10a8b48ab01f6f602e44f60224c3950c013c682c
Gero1999 <gerardojoro@gmail.com>
2026-03-23 14:38:33 +0100

    include a FORMULATION col in adnca_example

--- data-raw/adnca_example.R
+++ data-raw/adnca_example.R
@@ -1,3 +1,4 @@
 # Build adnca_example dataset from CSV
-adnca_example <- read.csv("data-raw/adnca_example.csv", na.strings = c("", "NA"))
+adnca_example <- read.csv("data-raw/adnca_example.csv", na.strings = c("", "NA")) %>%
+  mutate(FORMULATION = paste0(ROUTE, " OF ", DOSETRT))
 usethis::use_data(adnca_example, overwrite = TRUE)
