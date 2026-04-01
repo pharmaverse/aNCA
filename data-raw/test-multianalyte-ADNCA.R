b03c208212a726a294d05c7d33b380e27918ac65
Gero1999 <gerardojoro@gmail.com>
2026-04-01 14:54:39 +0200

    correct order of actions in mutate

--- data-raw/test-multianalyte-ADNCA.R
+++ data-raw/test-multianalyte-ADNCA.R
@@ -3,7 +3,7 @@ multian_test <- read.csv("data-raw/adnca_example.csv", na.strings = c("", "NA"))
   mutate(DOSFRM = ifelse(ROUTE == "ORAL", "TABLET", "INJECTION, SOLUTION")) %>%
   # Provide different units for the metabolite
   mutate(
-    AVALU = ifelse(PARAM == "Metab-DrugA" & AVALU == "ug/mL", "mg/mL", AVALU),
-    AVAL = ifelse(PARAM == "Metab-DrugA" & AVALU == "ug/mL", AVAL / 1000, AVAL)
+    AVAL = ifelse(PARAM == "Metab-DrugA" & AVALU == "ug/mL", AVAL / 1000, AVAL),
+    AVALU = ifelse(PARAM == "Metab-DrugA" & AVALU == "ug/mL", "mg/mL", AVALU)
   )
 write.csv(multian_test, "tests/testthat/data/test-multianalyte-ADNCA.csv", row.names = FALSE)
