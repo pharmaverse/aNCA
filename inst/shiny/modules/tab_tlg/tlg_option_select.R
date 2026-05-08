0c587e7397c513958ba685ec59592879cfb341b0
Hoang, Hanson {GCCF~SOUTH SAN FRANCISCO} <hoanghansonhh@gmail.com>
2026-05-08 12:02:57 -0700

    fix: typo infering to inferring in TLG option modules

--- inst/shiny/modules/tab_tlg/tlg_option_select.R
+++ inst/shiny/modules/tab_tlg/tlg_option_select.R
@@ -1,7 +1,7 @@
 #' Function generating an input widget for TLG option.
 #' @param id      id of the input widget
 #' @param opt_def definition of the option, as specified in the `yaml` file
-#' @param data    data object used for parsing labels, strings, infering placeholder values or
+#' @param data    data object used for parsing labels, strings, inferring placeholder values or
 #'                choices etc.
 tlg_option_select_ui <- function(id, opt_def, data) {
   ns <- NS(id)
@@ -42,7 +42,7 @@ tlg_option_select_ui <- function(id, opt_def, data) {
 #' Function generating an input widget server for TLG option.
 #' @param id            id of the input widget
 #' @param opt_def       definition of the option, as specified in the `yaml` file
-#' @param data          data object used for parsing labels, strings, infering placeholder values or
+#' @param data          data object used for parsing labels, strings, inferring placeholder values or
 #'                      choices etc.
 #' @param reset_trigger a reactive expression on which the module will restore its returned value
 #'                      to the default one.
