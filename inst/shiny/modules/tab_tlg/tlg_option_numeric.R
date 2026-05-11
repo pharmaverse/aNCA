bc92d3288f28d51aabca78717644ff02dc490a00
Hoang, Hanson {GCCF~SOUTH SAN FRANCISCO} <hoanghansonhh@gmail.com>
2026-05-11 10:23:21 -0700

    style: fix line length in TLG option modules

--- inst/shiny/modules/tab_tlg/tlg_option_numeric.R
+++ inst/shiny/modules/tab_tlg/tlg_option_numeric.R
@@ -18,8 +18,8 @@ tlg_option_numeric_ui <- function(id, opt_def, data) {
 #' Function generating an input widget server for TLG option.
 #' @param id            id of the input widget
 #' @param opt_def       definition of the option, as specified in the `yaml` file
-#' @param data          data object used for parsing labels, strings, inferring placeholder values or
-#'                      choices etc.
+#' @param data          data object used for parsing labels, strings, inferring placeholder
+#'                      values or choices etc.
 #' @param reset_trigger a reactive expression on which the module will restore its returned value
 #'                      to the default one.
 #' @returns a reactive with the input value
